b75b11ddc4a0051ccd57e3782a436e2f33b56301
Sean Maden <metamaden@users.noreply.github.com>
2022-10-03 09:32:02 -0700

    Update z_methods.R
    handles get_mean_ratio2
    uses zsource_type to check cell type variable in zsource
    checks zsource class for get_mean_ratio2

--- source/z_methods.R
+++ source/z_methods.R
@@ -8,14 +8,14 @@
 zsource_transform <- function(zsource, transformv){
   # transform zsource dataset
   #
-  #
+  # zsource: matrix of data used to calculate z (e.g. counts matrix)
   #
 }
 
 zsource_type <- function(zsource, varname = "cellType_broad_hc"){
   # get the types (e.g. cell types.columns in z)
   #
-  #
+  # zsource: matrix of data used to calculate z (e.g. counts matrix)
   #
   typev <- NA
   if(is(zsource, "SingleCellExperiment")){
@@ -27,14 +27,35 @@ zsource_type <- function(zsource, varname = "cellType_broad_hc"){
   return(typev)
 }
 
-zsource_marker <- function(zsource, markerv, method = "mean_ratio"){
+zsource_markerdata <- function(zsource, varname = "cellType_broad_hc", 
+                           method = "mean_ratio", mr.assay = "logcounts"){
   # get the gene markers (e.g. features/rows in z)
   #
+  # note:
+  #   wraps several marker gene functions
   #
+  # zsource: matrix of data used to calculate z (e.g. counts matrix)
+  # varname : name of variable in zsource containing types
+  # mr.assay : assay_name argument for get_mean_ratio2()
   #
-  markerv <- NA
-  if(method == "mean_ratio"){require(DeconvoBuddies)}
+  marker.data <- NA
   if(is(zsource, "SingleCellExperiment")){zsource <- counts(zsource)}
-  
+  message("checking varname exists in zsource")
+  typev <- zsource_type(zsource = zsource, varname = varname)
+  if(method == "mean_ratio"){
+    if(!is(zsource, "SingleCellExperiment")){
+      stop("method mean_ratio requires that zsource is a SingleCellExperiment")}
+    message("using method mean_ratio")
+    require(DeconvoBuddies)
+    marker.data <- DeconvoBuddies::get_mean_ratio2(zsource, 
+                                                   cellType_col = varname, 
+                                                   assay_name = mr.assay,
+                                                   add_symbol = TRUE)
+  } else if(method == "findMarkers"){
+    message("using method findMarkers")
+    require(scran)
+    # findMarkers code goes here
+  }
+  return(marker.data)
 }
 
