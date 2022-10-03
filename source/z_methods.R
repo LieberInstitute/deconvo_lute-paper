8896caf4cd788fd6daca73ca24b85bb60f80971b
Sean Maden <metamaden@users.noreply.github.com>
2022-10-03 11:18:30 -0700

    Update z_methods.R
    tidies up get_z_experiment()
    organizes variable names and summary methods
    adds status messages

--- source/z_methods.R
+++ source/z_methods.R
@@ -71,52 +71,107 @@ zsource_markerdata <- function(zsource, type.varname = "cellType_broad_hc",
   return(markerdata)
 }
 
-get_z_experiment <- function(ngenes.byk = NA, type.summary = "mean",
-                             zsource = NA, type.varname = "cellType_broad_hc", 
-                             method = "mean_ratio", mr.assay = "logcounts", 
-                             markerdata = NA, type = NA, transform = NA){
-  # calculate z for a deconvolution experiment
+get_top_markers <- function(markerdata, typev, ngenes.byk, 
+                            method.markers = "mean_ratio"){
+  # get top markers from markerdata
   #
-  # zsource: matrix of data used to calculate z (e.g. counts matrix)
-  # type.varname : name of variable in zsource containing types
-  # method : name of method to identify marker genes from zsource.
-  # mr.assay : assay_name argument for get_mean_ratio2()
-  # marker.data : data containing the features of interest and the values by 
-  #   type (e.g. cell type) in zsource.
-  # ngenes.byk: top genes to select by type. If NA, retains all marker genes 
-  #   provided upstream.
-  # type.summary: function to use to summarize data by type.
-  # 
-  require(dplyr)
-  if(is.na(markerdata)){
-    markerdata <- zsource_markerdata(zsource, type.varname, method, mr.assay)
-  }
-  # get types
-  typev <- unique(zsource[[type.varname]])
-  # get z
-  # get marker data for top genes
+  # markerdata:
+  # typev:
+  # ngenes.byk: top markers to return for each cell type
+  # method.markers: method used to generate markerdata.
+  #
+  # returns:
+  # top markers as vector
+  #
   ma <- markerdata
-  top.markers <- lapply(typev, function(ki){
+  ma.top <- do.call(rbind, lapply(typev, function(ki){
     markerv <- NA
-    if(method == "mean_ratio"){
+    if(method.markers == "mean_ratio"){
       # note: parses output from DeconvoBuddies::get_mean_ratio2()
       colnames(ma)[2] <- "celltype.target"
       mi <- ma %>% filter(celltype.target == ki) %>% arrange(rank_ratio)
-      if(ngenes.byk < nrow(mi)){markerv <- markerv[seq(ngenes.byk)]}
+      if(nrow(mi)>0 & ngenes.byk < nrow(mi)){mi <- mi[seq(ngenes.byk),]}
     }
-    message("keeping ",length(markerv)," features for type ",ki)
-    markerv})
-    
-    
-    
-    # get k summaries
-    if(type.summary == "mean"){
-      ki.summary <- rowMeans(markerdati[,ki.summary.cols])
-      rownames(ki.summary) <- ki.markers
-    }
-    ki.summary
-  })))
-  # summarize top genes
-  md.byk <- do.call(cbind, lapply(seq(length(markerdata)), function(ii)))
+    message("keeping ",nrow(mi)," features for type ",ki)
+    mi}))
+  return(ma.top)
+}
+
+get_z_experiment <- function(zsource, 
+                             markerdata = NA,
+                             method.markers = "mean_ratio", 
+                             mr.assay = "logcounts",
+                             ngenes.byk = 20, 
+                             type.varname = "cellType_broad_hc", 
+                             summary.varname = "donor",
+                             k.summary.method = "mean",
+                             z.summary.method = "mean",
+                             return.all = FALSE){
+  # calculate z for a deconvolution experiment
+  #
+  # zsource: data (of type SingleCellExperiment) used to calculate z (e.g. counts matrix)
+  # markerdata : data containing the features of interest and the values by 
+  #   type (e.g. cell type) in zsource.
+  # method.markers : name of method to identify marker genes from zsource.
+  # mr.assay : assay_name argument for get_mean_ratio2()
+  # type.varname : name of variable in zsource coldata, containing type labels
+  # summary.varname : name of variable in zsource coldata, containing labels on
+  #   which to summarize the zsource counts.
+  # k.summary.method : name of method for summarizing zsource counts on variable
+  #   summary.varname and k types.
+  # z.summary.method : name of method for summarizing within the k types for z.
+  # return.all: whether to return all intermediate outputs. If FALSE, returns 
+  #   only the final z table.
+  #
+  # return:
+  #   data for the z tables, with option to include intermediate outputs (see 
+  #   argument return.all)
+  # 
+  require(dplyr)
+  if(!is(zsource, "SingleCellExperiment")){
+    stop("zsource should be of type SingleCellExperiment")}
+  if(is.na(markerdata)){
+    markerdata <- zsource_markerdata(zsource = zsource, 
+                                     type.varname = type.varname, 
+                                     method = method.markers, 
+                                     mr.assay = mr.assay)
+  }
+  # get variable levels
+  typev <- unique(zsource[[type.varname]])
+  summaryv <- unique(zsource[[summary.varname]])
+  message("filtering source data on top markers")
+  ma.top <- get_top_markers(ma = markerdata, typev = typev, 
+                            ngenes.byk = ngenes.byk, 
+                            method = method.markers)
+  zsf <- zsource[rownames(zsource) %in% ma.top$gene,]
+  # summarize counts by type and by category (e.g. donors)
+  message("summarizing counts by type and ",summary.varname, "...")
+  zs <- do.call(cbind, lapply(summaryv, function(ii){
+    si <- do.call(cbind, lapply(typev, function(jj){
+      zsf.filt <- zsf[[type.varname]] == jj & 
+        zsf[[summary.varname]] == ii
+      dati <- NA
+      if(k.summary.method == "mean"){
+        dati <- rowMeans(counts(zsf[,zsf.filt]))}
+      dati
+    }))
+    colnames(si) <- paste0(ii, ";", typev);si
+  }))
+  # get z by summarizing type by category (e.g. donors)
+  message("summarizing counts by type for final z table...")
+  cnv.zs <- colnames(zs)
+  z <- t(apply(zs, 1, function(ri){
+    unlist(lapply(typev, function(ki){
+      datf <- ri[grepl(ki, names(ri))]; dati <- NA
+      if(z.summary.method == "mean"){
+        dati <- mean(datf, na.rm = T)}
+      dati
+    }))
+  }))
+  colnames(z) <- typev; lr <- z
+  if(return.all){
+    lr <- list(top.marker.data = ma.top, z.summary.filt = zs, z.final = z)
+  }
+  return(lr)
 }
 
