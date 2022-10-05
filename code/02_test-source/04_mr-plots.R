51ca7c512a42db1f315b090d5e46a2a535ae5cf3
Sean Maden <metamaden@users.noreply.github.com>
2022-10-05 08:40:35 -0700

    adds mk composite plot
    adds mk composite plot inc.
    . violinplots
    . jitter
    . boxplots
    updates mr-plot.R

--- code/02_test-source/04_mr-plots.R
+++ code/02_test-source/04_mr-plots.R
@@ -3,6 +3,7 @@
 # Plot mean ratios (MRs) and mean ratio summaries.
 
 library(ggplot2)
+library(gridExtra)
 
 #------
 # paths
@@ -33,19 +34,40 @@ ggjt1 <- ggplot(dfpi, aes(x = celltype.target, y = ratio, color = celltype.targe
 ggbp1 <- ggplot(dfpi, aes(x = celltype.target, y = ratio, fill = celltype.target)) + 
   theme_bw() + geom_boxplot() + ylab("MR_k") + xlab("cell_type") +
   theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+
 # plots for composite figures
 ggvp2 <- ggplot(dfpi, aes(x = celltype.target, y = ratio, fill = celltype.target)) + 
   theme_bw() + geom_violin(draw_quantiles = 0.5) + ylab("MR_k") + xlab("cell_type") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+  theme(axis.text.x = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_blank()) +
+  ggtitle("Violin plots")
 ggjt2 <- ggplot(dfpi, aes(x = celltype.target, y = ratio, color = celltype.target)) + 
   theme_bw() + ylab("MR_k") + xlab("cell_type") + geom_jitter(size = 1, alpha = 0.5) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+  theme(axis.text.x = element_blank(), legend.position = "none",
+        axis.title.x = element_blank(), axis.title.y = element_blank()) +
+  ggtitle("Jitter points")
 ggbp2 <- ggplot(dfpi, aes(x = celltype.target, y = ratio, fill = celltype.target)) + 
   theme_bw() + geom_boxplot() + ylab("MR_k") + xlab("cell_type") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1), legend.position = "none")
+  theme(axis.text.x = element_text(angle = 45, hjust = 1), 
+        axis.title.x = element_blank(), axis.title.y = element_blank(),
+        legend.position = "none") +
+  ggtitle("Boxplots")
 
 # save individual plots
 
+# save composite plot
+lm <- matrix(c(1,1,2,2,3,3,3),ncol=1) # adds extra space for xaxis labels
+
+plot.comp.fname <- "mr-k_composite-vp-jt-bp.png"
+plot.fpath <- file.path(save.dpath, plot.comp.fname)
+
+png(plot.fpath, width = 4, height = 6, units = 'in', res = 400)
+
+grid.arrange(ggvp2, ggjt2, ggbp2, ncol = 1, layout_matrix = lm, 
+             left = paste0(paste0(rep(" ", 20),collapse=""), "MR_k"), 
+             bottom = "cell_type")
+
+dev.off()
 
 # save composite plot
 
