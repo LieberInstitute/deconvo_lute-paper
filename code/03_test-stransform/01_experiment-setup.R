21b239d2024cc32dcd9e24e068e18dc3f7f81043
Sean Maden <metamaden@users.noreply.github.com>
2022-10-05 16:33:34 -0700

    Update 01_experiment-setup.R

--- code/03_test-stransform/01_experiment-setup.R
+++ code/03_test-stransform/01_experiment-setup.R
@@ -19,13 +19,45 @@ save.dpath <- file.path(proj.dpath, "outputs/03_test-stransform")
 lz.fname <- "lz-mr_expt-stransform_dlpfc-ro1.rda"
 script.fnamev <- c("z_methods.R", "z_transform")
 
+sce.fpath <- "DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata"
+
+#-------
+# params
+#-------
+celltype.varname <- "cellType_broad_hc"
+
 #-----
 # load
 #-----
 # source key functions
-for(scripti in script.fnamev){
-  source(file.path(source.dpath, scripti))
-}
+for(scripti in script.fnamev){source(file.path(source.dpath, scripti))}
+
+# single cell experiment
+sce <- get(load(sce.fpath))
+
+#-------------------------------------------------
+# make new cell type -- make types per tregs paper
+#-------------------------------------------------
+varv <- sce[[celltype.varname]]
+table(varv)
+#  Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib
+#   3979      2157      1601     32051      1940     24809     11067
+
+new.ct <- rep("other", ncol(sce))
+net.ct[which(sce[[celltype.varname]])]
+
+
+#-----------------------------------------------------
+# make lz -- list containing z table and other outputs
+#-----------------------------------------------------
+
+# get detailed zexpt data
+lz <- get_z_experiment(sce, method.markers = "mean_ratio", 
+                       mr.assay = "logcounts", ngenes.byk = 25, 
+                       type.varname = "cellType_broad_hc", 
+                       summary.varname = "BrNum", k.summary.method = "mean",
+                       z.summary.method = "mean", return.all = TRUE,
+                       save.dpath = save.dpath)
 
 
 
