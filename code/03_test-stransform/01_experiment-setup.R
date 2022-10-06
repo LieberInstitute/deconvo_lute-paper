599c894c20e08bd043f0394c30faa1296716603c
Sean Maden <metamaden@users.noreply.github.com>
2022-10-06 06:41:29 -0700

    Update 01_experiment-setup.R
    organize experiment script
    consolidate script subheaders

--- code/03_test-stransform/01_experiment-setup.R
+++ code/03_test-stransform/01_experiment-setup.R
@@ -23,12 +23,6 @@ script.fnamev <- c("z_methods.R", "z_transform.R", "make_example_data.R")
 
 sce.fpath <- "DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata"
 
-#-------
-# params
-#-------
-celltype.varname <- "cellType_broad_hc"
-celltype.treg.varname <- "celltype.treg"
-
 #-----
 # load
 #-----
@@ -37,21 +31,6 @@ for(scripti in script.fnamev){source(file.path(source.dpath, scripti))}
 
 # single cell experiment
 sce <- get(load(sce.fpath))
-
-#-------------------------------------------------
-# make new cell type -- make types per tregs paper
-#-------------------------------------------------
-varv <- as.character(sce[[celltype.varname]])
-varv[!varv %in% c("Excit", "Inhib", "Oligo")] <- "other"
-sce[[celltype.treg.varname]] <- varv
-
-table(varv)
-#  Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib
-#   3979      2157      1601     32051      1940     24809     11067
-
-table(sce[[celltype.treg.varname]])
-# Excit Inhib Oligo other
-# 24809 11067 32051  9677
 
 #------------------------------------
 # show how it works with example data
@@ -76,18 +55,47 @@ head(z)
 # randomized s transform
 ltrans <- list(s_rescale = list(meanv = seq(4), sdv = rep(2,4)))
 ldecon2 <- ldecon_example(k.value = 4, ltransform = ltrans)
-
+head(ldecon2$z_rescale)
+#          k_1        k_2       k_3       k_4
+# [1,]  0.000000 12.1385130  0.000000  0.000000
+# [2,]  5.507968 41.1391628 28.127941 14.267247
+# [3,] 37.536944  0.4782795  8.768361  2.898522
+# [4,]  0.000000  1.7252446 20.799447 10.393403
+# [5,]  5.241753  0.1964092 34.663691 12.713812
+# [6,]  0.480829 14.9222000  0.000000 11.180610
 
 #-----------------------
 # conduct new experiment
 #-----------------------
-# get detailed zexpt data
-lz <- get_z_experiment(sce, method.markers = "mean_ratio", 
-                       mr.assay = "logcounts", ngenes.byk = 25, 
-                       type.varname = "cellType_broad_hc", 
-                       summary.varname = "BrNum", k.summary.method = "mean",
-                       z.summary.method = "mean", return.all = TRUE,
+# params
+celltype.varname <- "cellType_broad_hc"
+celltype.treg.varname <- "celltype.treg"
+
+# make new cell type -- make types per tregs paper
+varv <- as.character(sce[[celltype.varname]])
+varv[!varv %in% c("Excit", "Inhib", "Oligo")] <- "other"
+sce[[celltype.treg.varname]] <- varv
+
+# summarize cell type data
+table(varv)
+#  Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib
+#   3979      2157      1601     32051      1940     24809     11067
+table(sce[[celltype.treg.varname]])
+# Excit Inhib Oligo other
+# 24809 11067 32051  9677
+
+# get new lz for k=4
+lz <- get_z_experiment(sce, ngenes.byk = 25, summary.varname = "BrNum", 
+                       return.all = TRUE, type.varname = celltype.treg.varname,
                        save.dpath = save.dpath)
+# inspect z.final
+head(lz[["z.final"]])
+
+# get z transformations
+# z static
+lz[["zs1"]] <- s_rescale(lz[["z.final"]], factorv = c(2,2,6,8))
+# z randomized
+lz[["zs2"]] <- s_rescale(lz[["z.final"]], meanv = c(), sdv = c())
 
 
 
