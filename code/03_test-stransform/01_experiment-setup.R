18189da2ff2f0c534e045dfeece2431e0249356e
Sean Maden <metamaden@users.noreply.github.com>
2022-10-06 08:25:48 -0700

    Update 01_experiment-setup.R
    cleans up script

--- code/03_test-stransform/01_experiment-setup.R
+++ code/03_test-stransform/01_experiment-setup.R
@@ -22,8 +22,8 @@ lz.fname <- "lz-mr_expt-stransform_dlpfc-ro1.rda"
 
 sce.fpath <- "DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata"
 
-script.fnamev <- c("z_methods.R", "z_transform.R", 
-                   "make_example_data.R", "z_figures.R")
+script.fnamev <- c("z_methods.R", "z_transform.R", "make_example_data.R", 
+                   "z_figures.R", "y_methods.R")
 
 #-----
 # load
@@ -112,68 +112,7 @@ lz[["zs1"]] <- s_rescale(lz[["z.final"]], factorv = meanv)
 # z randomized
 lz[["zs2"]] <- s_rescale(lz[["z.final"]], meanv = meanv, sdv = sdv)
 
-
-#---------------------------
-# make new pseudobulk series
-#---------------------------
-ctvarname <- "celltype"
-sce[["celltype"]] <- ifelse(sce[["cellType"]]=="Oligo", "Oligo", 
-                            ifelse(sce[["cellType"]]=="Astro", "Astro",
-                                   ifelse(sce[["cellType"]]=="OPC", "OPC", "other")))
-table(sce[["celltype"]])
-# Astro Oligo   OPC other 
-# 782  5455   572  4393
-
-
-
-
-get_lpb <- function(scef, datv = NA, nj = NA, ctvarname = "celltype.treg", 
-                    seed.num = 2){
-  # get list of pseudobulked counts tables
-  #
-  # datv : vector of cell type scale data. Length should be equal to nj*nk. 
-  #   Values here correspond to the mpb design matrix. If NA, this is randomized 
-  #   and variable nj is used.
-  # scef : SingleCellExperiment, ideally filtered on some marker genes or z features.
-  # nj : number of pseudobulked samples to simulate. This is only used if datv==NA.
-  # 
-  set.seed(seed.num)
-  # parse types
-  kvarv <- scef[[ctvarname]]; klabv <- unique(kvarv); nk <- length(klabv)
-  # parse pb data -- get datv using either arg datv,nj
-  if(is.na(datv)){
-    if(is.na(nj)){
-      stop("provide either datv or nj.")
-    } else{
-      datv <- rep(sample(1e3, nk), nj)
-    }
-  }
-  ncol <- length(datv)/nk
-  # get pseudobulk design matrix
-  mpb <- matrix(datv, ncol=ncol) %>% apply(2, function(ci){ci/sum(ci)})
-  rownames(mpb) <- klabv; colnames(mpb) <- paste0("j_", seq(ncol(mpb)))
-  scalev <- sample(1000:10000, ncol(mpb)) # sample scale factors (total counts)
-  ct <- counts(scef) # set up counts for sampling
-  ctlabv <- colnames(ct) <- paste0(kvarv, "_", seq(ncol(ct)))
-  # get list of pseudobulked counts tables
-  lpb <- lapply(seq(ncol(mpb)), function(ji){
-    # get sample-specific info
-    scalej <- scalev[ji] # sample scale factor
-    cellv.ij <- mpb[,ji]*scalej # vector of total cell counts
-    # get randomized counts data
-    ct.pb.j <- do.call(cbind, lapply(klabv, function(ki){
-      num.cells.ij <- cellv.ij[ki] # num cells to sample for this type
-      cnvf <- ctlabv[grepl(ki, gsub("_.*", "", ctlabv))]
-      cnvf.index.ij <- cnvf[sample(seq(length(cnvf)), 
-                                   num.cells.ij, replace = T)]
-      return(ct[,cnvf.index.ij])
-    }))
-    return(ct.pb.j)
-  })
-  names(lpb) <- colnames(mpb); lpb[["mpb"]] <- mpb
-  return(lpb)
-}
-
+# get pseudobulked tables
 markerv <- rownames(lz$z.final)
 datv <- c(1,2,3,3,2,3,5,2,4,2,1,1)
 lpb <- get_lpb(datv, scef = sce[markerv,])
