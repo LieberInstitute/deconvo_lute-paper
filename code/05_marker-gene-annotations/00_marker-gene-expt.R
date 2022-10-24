bbf5aa32e55b29c7f3b22eb17283900e8aac9188
Sean Maden <metamaden@users.noreply.github.com>
2022-10-24 10:13:32 -0700

    adds markers for k=7
    adds marker data for k = 7

--- code/05_marker-gene-annotations/00_marker-gene-expt.R
+++ code/05_marker-gene-annotations/00_marker-gene-expt.R
@@ -20,11 +20,20 @@ celltype.varname <- "cellType_broad_hc"
 #----------
 proj.dpath <- "deconvo_method-paper"
 
+# path to full singlecellexperiment
+sce.fpath <- file.path(here(), 
+                       "DLPFC_snRNAseq/processed-data/sce",
+                       "sce_DLPFC.Rdata")
+
+sef.fpath <- file.path(proj.dpath, "outputs/05_marker-gene-annotations/",
+                       "sef_mr-markers-k7-from-sce_dlpfc-ro1.rda")
+
+markers.fpath <- file.path(proj.dpath, "outputs/05_marker-gene-annotations/",
+                           "mr-markers-output_k7-ctbroadhc_from-sce_dlpfc-ro1.rda")
+
 #-----
 # load
 #-----
-# path to full singlecellexperiment
-sce.fpath <- file.path(here(), "DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata")
 sce <- get(load(sce.fpath)) # get full singlecellexperiment
 
 #---------------------
@@ -39,51 +48,31 @@ table(sce[[celltype.varname]])
 # get markers
 #------------
 # get marker genes
-markers <- get_mean_ratio2(sce, 
-                           cellType_col = celltype.varname,
-                           assay_name = "logcounts",
-                           add_symbol = TRUE)
+markers <- get_mean_ratio2(sce, cellType_col = celltype.varname,
+                           assay_name = "logcounts", add_symbol = TRUE)
 # save results
-save(markers.withdrop, file = markers.withdrop.fpath)
+save(markers, file = markers.fpath)
 
-#------------------------
-# get markers --- no drop
-#------------------------
-# filter drop category
-exclude.drop <- !sce[[celltype.varname]]=="drop"
-scef <- sce[,exclude.drop]
+#------------------------------------------
+# get sef -- subset top 100 markers by type
+#------------------------------------------
+table(markers$cellType.target) # num. markers returned by cell type
+# Astro EndoMural     Micro     Oligo       OPC     Excit     Inhib
+#   595       719       473       288      1333      4639      3676
 
-# get marker genes
-markers.nodrop <- get_mean_ratio2(scef, cellType_col = celltype.varname, 
-                                  assay_name = "logcounts", add_symbol = TRUE)
+# get top 100 markers by gene
+ctv <- unique(markers$cellType.target)
+ma <- as.data.frame(markers, stringsAsFactors = F)
+ma[,2] <- as.character(ma[,2])
+markerv <- unique(unlist(lapply(ctv, function(ki){
+  mi <- as.data.frame(ma[ma$cellType.target == ki,])
+  mi <- mi[order(mi$rank_ratio),]; mi[seq(100),1]
+})))
 
-# save results
-save(markers.nodrop, file = markers.nodrop.fpath)
-
-#----------------------
-# marker gene summaries
-#----------------------
-lv <- list(markers.withdrop, markers.nodrop)
-# number of unique marker genes
-length(unique(markers.withdrop$gene)) # 8845
-length(unique(markers.nodrop$gene)) # 5789
-
-# top repeated marker genes
-lapply(lv, function(ii){
-  dt <- as.data.frame(table(ii$gene))
-  dt <- dt[rev(order(dt[,2])),]
-  print(head(dt))})
-#        Var1 Freq
-# 8757 ZNF638    8
-# 8491 ZBTB20    8
-# 8416   WWOX    8
-# 7986   TTC3    8
-# 7814 TNRC6B    8
-# 7813 TNRC6A    8
-#        Var1 Freq
-# 5746 ZNF638    7
-# 5616 ZBTB20    7
-# 5562   WWOX    7
-# 5274   TTC3    7
-# 5163 TNRC6B    7
-# 5162 TNRC6A    7
+# get counts for unique markers
+scef <- sce[markerv,]
+sef <- SummarizedExperiment(assays = list(counts = as.matrix(counts(scef))),
+                            colData = colData(scef), 
+                            rowData = rowData(scef))
+# save sef
+save(sef, file = sef.fpath)
