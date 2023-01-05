a96113705da16f15799bb21b7a8d80fce6b56ffe
Sean Maden <metamaden@users.noreply.github.com>
2023-01-05 13:33:02 -0800

    adds ggpt, ggpt facet plots

--- code/07_cell-size-estimates/01-01_analyze-merged-cellsizes_halo-sce.R
+++ code/07_cell-size-estimates/01-01_analyze-merged-cellsizes_halo-sce.R
@@ -15,6 +15,9 @@ read.dpath <- out.dpath <- file.path("deconvo_method-paper", "outputs",
                                      "07_cell-size-estimates")
 dfm <- get(load(file.path(read.dpath, read.fname)))
 
+# save filename stem
+save.fnstem <- "halo-scecounts"
+
 #------------
 # format data
 #------------
@@ -45,16 +48,35 @@ dfp <- do.call(rbind, lapply(ctv, function(cti){
   dfp$sce.mean.across.samples <- mfv[grepl("^sce\\..*", names(mfv))]
   return(dfp)
 }))
-colnames(dfp)[1] <- "mean.across.samples"
+colnames(dfp)[1] <- "halo.mean.across.samples"
 
 # get ggplot
+# make scatterplot
 ggpt <- ggplot(dfp, aes(x = sce.mean.across.samples, 
-                        y = mean.across.samples,
+                        y = halo.mean.across.samples,
+                        color = celltype, shape = halo.var)) + 
+  geom_point(size = 5, alpha = 0.5) +
+  geom_abline(intercept = 0, slope = 1, col = "black") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("SCE") + ylab("HALO") + ggtitle("Mean across samples")
+# save scatterplot
+pdf.fname <- paste0("ggpt-cellsize_", save.fnstem, ".pdf")
+pdf.fpath <- file.path(read.dpath, pdf.fname)
+ggsave(pdf.fpath, ggpt, width = 5, height = 4, 
+       device = "pdf", units = "in", dpi = 400)
+
+# make facet plot
+ggpt <- ggplot(dfp, aes(x = sce.mean.across.samples, 
+                        y = halo.mean.across.samples,
                         color = celltype)) + geom_point() +
   geom_abline(intercept = 0, slope = 1, col = "black") +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
-ggpt+facet_wrap(~halo.var)
-
-
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  xlab("SCE") + ylab("HALO") + ggtitle("Mean across samples")
+ggf <- ggpt + facet_wrap(~halo.var)
+# save facet plot
+pdf.fname <- paste0("ggptfacet-cellsize_", save.fnstem, ".pdf")
+pdf.fpath <- file.path(read.dpath, pdf.fname)
+ggsave(pdf.fpath, ggf, width = 6.5, height = 3.5, 
+       device = "pdf", units = "in", dpi = 400)
 
 
