5b04200fc0f31aff2ef4b3d2926fd0338fdba92b
Sean Maden <metamaden@users.noreply.github.com>
2023-01-09 08:33:15 -0800

    Create 02_marker-visualizations.R

---
+++ code/07_cell-size-estimates/02_marker-visualizations.R
@@ -0,0 +1,48 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+# Visualize k=2 markers
+#
+#
+
+#----------
+# load data 
+#----------
+# se marker data, k2
+sef.fname <- "sef_mr-markers_k2_20-per-k_dlpfc-ro1.rda"
+sef.dpath <- file.path("deconvo_method-paper", "outputs", 
+                       "05_marker-gene-annotations")
+sef <- get(load(file.path(sef.dpath, sef.fname)))
+
+#------------------------------------
+# visualizations -- marker expression
+#------------------------------------
+# heatmap of marker logcounts
+Heatmap(assays(setf)$logcounts)
+
+# tile plot
+lct <- assays(setf)$logcounts
+lct$marker <- rownames(lct)
+dfp <- rbind(data.frame(mean = lct$Neuron, marker = lct$marker),
+             data.frame(mean = lct$`Non-neuron`, marker = lct$marker))
+dfp$celltype <- c(rep("Neuron", nrow(lct)), rep("Non-neuron", nrow(lct)))
+dfp$mean <- round(dfp$mean, 2)
+ggplot(dfp, aes(x = celltype, y = marker, label = mean, fill = mean)) + 
+  geom_tile() + geom_text() + theme_bw()
+
+# violin plot
+ggplot(dfp, aes(x = celltype, y = mean, fill = celltype)) + 
+  geom_violin(draw_quantiles = 0.5) + theme_bw()
+
+# violin with scatter
+ggplot(dfp, aes(x = celltype, y = mean, fill = celltype)) + 
+  geom_violin(draw_quantiles = 0.5) + theme_bw() +
+  geom_jitter(alpha = 0.5, width = 0.2, size = 5)
+
+# scatterplot
+ggplot(lct, aes(x = Neuron, y = `Non-neuron`, label = marker)) + 
+  geom_point(alpha = 0.5, size = 5) + 
+  geom_label_repel(box.padding   = 0.35, point.padding = 0.8, 
+                   segment.color = 'grey50') + 
+  theme_bw() + geom_abline(intercept = 0, slope = 1, col = "red")
