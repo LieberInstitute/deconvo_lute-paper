0b464f29e515ee765c2c82ab33513b13227b80f6
Sean Maden <metamaden@users.noreply.github.com>
2023-01-17 14:05:18 -0800

    remove ktotal arg

--- code/08_lute-simulations/00_cell-size-bias-adj.R
+++ code/08_lute-simulations/00_cell-size-bias-adj.R
@@ -11,7 +11,11 @@ sapply(libv, library, character.only = TRUE)
 #----------------
 # run simulations
 #----------------
-lexpt <- donor_marker_biasexpt(offsetv = c(1, 10), P = c(0.25, 0.75),
+# define offset series
+offsetv <- c(seq(0.1, 2.1, 0.1), seq(18, 20, 0.1))
+
+
+lexpt <- donor_marker_biasexpt(offsetv = offsetv, P = c(0.25, 0.75),
                                donor.adj.method = 'var_denom',
-                               gindexv = c(1, 2), ndonor = 10, ktotal = 2,
+                               gindexv = c(1, 2), ndonor = 10,
                                seed.num = 0, verbose = FALSE)
