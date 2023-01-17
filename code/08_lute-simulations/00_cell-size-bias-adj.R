a89a13369c239049b1bb50d24c81845e3334f98f
Sean Maden <metamaden@users.noreply.github.com>
2023-01-17 15:16:55 -0800

    Update 00_cell-size-bias-adj.R
    adds examples for sd_denom
    adds examples for sctransform

--- code/08_lute-simulations/00_cell-size-bias-adj.R
+++ code/08_lute-simulations/00_cell-size-bias-adj.R
@@ -8,14 +8,40 @@
 libv <- "lute"
 sapply(libv, library, character.only = TRUE)
 
-#----------------
-# run simulations
-#----------------
+#--------------------
+# test using sd_denom
+#--------------------
 # define offset series
-offsetv <- c(seq(0.1, 2.1, 0.1), seq(18, 20, 0.1))
+set.seed(0)
+pdiffv <- seq(-2e-2, 2e-2, 2e-3)
+ddiffv <- rnorm(n = 21, mean = 1e-2, sd = 1e-3)
+
+lseries <- lapply(seq(pdiffv), function(ii){
+  p1true <- 0.75 + pdiffv[ii]; p2true <- 1-p1true
+  donor_marker_biasexpt(offsetv = offsetv[ii], 
+                        P = c(p1true, p2true),
+                        donor.adj.method = "sd_denom",
+                        bounds_thresh = 50)
+})
+
+dfr <- do.call(rbind, lapply(lseries, function(ii){ii$dfres}))
+
+# scatterplots of results
+dfp <- dfr
+dfp$celltype <- ifelse(dfp$type.index==1, "neuron", "non-neuron")
+dfp$offset.type <- ifelse(dfp$offset < 10 , "low", "high")
+
+ggpt <- ggplot(dfp, aes(x = prop.true, y = prop.pred, 
+                color = celltype, shape = celltype)) + theme_bw() + 
+  geom_point(alpha = 0.5) + 
+  geom_abline(slope = 1, intercept = 0, color = "black") +
+  theme()
+
+ggpt + facet_wrap(~prop.type*offset.type)
+
+#-----------------------------------------
+# test using sctransform::correct_counts()
+#-----------------------------------------
+install.packages("sctransform")
 
 
-lexpt <- donor_marker_biasexpt(offsetv = offsetv, P = c(0.25, 0.75),
-                               donor.adj.method = 'var_denom',
-                               gindexv = c(1, 2), ndonor = 10,
-                               seed.num = 0, verbose = FALSE)
