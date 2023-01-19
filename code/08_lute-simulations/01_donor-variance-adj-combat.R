5bff1094ad9c05cb2b25b96b4253777187cafd5c
Sean Maden <metamaden@users.noreply.github.com>
2023-01-19 10:29:53 -0800

    Update 01_donor-variance-adj-combat.R

--- code/08_lute-simulations/01_donor-variance-adj-combat.R
+++ code/08_lute-simulations/01_donor-variance-adj-combat.R
@@ -11,25 +11,7 @@ sapply(libv, library, character.only = TRUE)
 # new save dest
 save.dpath <- file.path("deconvo_method-paper", "outputs", "08_lute-simulations")
 
-#------------------------------------------
-# simulate low and high variance donor data
-#------------------------------------------
-# show impact of increasing donor offset
-ndonor <- 10
-gindexv <- c(1, 2)
-offset.low <- 1
-offset.high <- 100
-
-# get pca results
-ldat <- list()
-
-# simulate donor data
-# low variances
-title.str <- paste0("Low donor variance (offset = ",1,")\n")
-ldat[["low"]] <- rand_donor_marker_table(ndonor = ndonor, gindexv = gindexv, 
-                                         sd.offset.pos = offset.low, 
-                                         sd.offset.neg = offset.low)
-# high variances
-ldat[["high"]] <- rand_donor_marker_table(ndonor = ndonor, gindexv = gindexv, 
-                                          sd.offset.pos = offset.high, 
-                                          sd.offset.neg = offset.high)
+#----------------------
+# get bias expt results
+#----------------------
+lb <- donor_marker_biasexpt(donor.adj.method = "combat")
