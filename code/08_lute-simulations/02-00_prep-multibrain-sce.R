28557bffb0e569a6173ca48939e4e443a9de3e64
Sean Maden <metamaden@users.noreply.github.com>
2023-01-24 11:06:53 -0800

    adds k2 markers, adds sce mrb

--- code/08_lute-simulations/02-00_prep-multibrain-sce.R
+++ code/08_lute-simulations/02-00_prep-multibrain-sce.R
@@ -13,3 +13,23 @@ save.dpath <- file.path(proj.dname, "outputs", code.dname)
 source.dpath <- file.path(proj.dname, "source")
 script.fname <- "helperfun_tran-et-al.R"
 source(file.path(source.dpath, script.fname))
+
+# get data
+sce.fname <- "sce-mrb_dlpfc.rda"
+sce.fpath <- file.path(save.dpath, sce.fname)
+sce <- get_sce_mrb()
+sce <- prep_sce()
+save(sce, file = sce.fpath)
+
+# check attributes
+names(assays(sce))
+# [1] "counts"    "logcounts"
+table(sce[["k2"]])
+# neuron  other 
+# 3968   7234
+
+# get markers
+mr <- get_mean_ratio2(sce, "k2", "logcounts")
+mr.fname <- "markers-k2_db-mr2_sce-dlpfc-mrb.rda"
+mr.fpath <- file.path(save.dpath, mr.fname)
+save(mr, file = mr.fpath)
--- source/helperfun_tran-et-al.R
+++ source/helperfun_tran-et-al.R
@@ -2,66 +2,67 @@
 #
 # Author: Sean Maden
 #
-# Helper functions for working with Tran et al 2021 multi-region brain snRNAseq
-# datasets.
+# Helper functions for working with Multi-region Brain (a.k.a. mrb) datasets 
+# published in Tran et al 2021.
 #
 #
 
-get_sce_tran <- function(loc.type = "dlpfc"){
+get_sce_mrb <- function(check.fpath = ".", loc.type = "dlpfc", download = T,
+                         http.addr = paste0("https://libd-snrnaseq-pilot",
+                                            ".s3.us-east-2.amazonaws.com/")){
+  # get_sce_tran
+  #
+  # check for sce, and optionally download if not found.
+  #
   loc.str <- toupper(loc.type)
-  addr <- paste0("https://libd-snrnaseq-pilot.s3.us-east-2.amazonaws.com/SCE_",
-                 loc.str,"-n3_tran-etal.rda")
-  fname <- unlist(strsplit(addr, "/"))
-  fname <- fname[grepl("\\.rda$", fname)][1]
-  if(!fname %in% list.files()){
-    message("downloading SCE rda dataset...")
-    utils::download.file(url = addr, destfile = fname)
-  } else{message("found local SCE rda dataset")}
-  message("loading SCE dataset...")
-  return(get(load(fname)))
+  sce.fname <- paste0("SCE_", loc.str,"-n3_tran-etal.rda")
+  sce.fpath <- file.path(check.fpath, sce.fname)
+  if(file.exists(sce.fpath)){
+    message("found local SCE rda dataset")
+    sce <- get(load(sce.fpath))
+  } else{
+    if(download){
+      message("downloading sce...")
+      addr <- paste0(http.addr, sce.fname)
+      utils::download.file(url = addr, destfile = sce.fpath)
+      sce <- get(load(sce.fpath))
+    } else{
+      message("sce not found at: ", sce.fpath)
+      return(NULL)
+    }
+  }
+  return(sce)
 }
 
-srcparam_tran <- function(){
-  # source the key parameters for Tran et al 2021
-  sce.celltype.varname <- "cellType"
-  sce.donor.varname <- "donor"
-  sce.k2.varname <- "k2"
-  lparam <- list(varnames = list(celltype = sce.celltype.varname,
-                                 donor = sce.donor.varname,
-                                 klevels = list(k2 = sce.k2.varname)))
-  return(lparam)
+parse_k_brain <- function(sce, kvar = 2, celltypevar = "cellType"){
+  cd <- colData(sce); ctvar <- cd[,celltypevar]
+  kvar <- paste0("k", kvar)
+  if(kvar %in% colnames(cd)){
+    message("kvar ",kvar, " already in coldata.")
+    return(sce)
+  } else if(kvar == 2){
+    newkvar <- ifelse(grepl("Excit|Inhib", ctvar), "neuron", "non-neuron")
+    cd[,kvar] <- newkvar; colData(sce) <- cd
+    return(sce)
+  } else{
+    stop("Error, invalid kvar.")
+  }
+  return(NULL)
 }
 
-prepare_sce_tran <- function(expr.rescale.types = c("logcounts"), 
-                             rescale.expr = FALSE, do.k.series = FALSE){
-  #
-  #
+prep_sce <- function(sce.fname = "SCE_DLPFC-n3_tran-etal.rda", 
+                             rescale.assays = "logcounts", kvar = 2,
+                             celltypevar = "cellType"){
+  # prepare_sce_tran
+  # 
   # key steps to prepare downloaded sce object for analysis
   #
-  
-  lparam <- srcparam_tran()
-  
-  if(rescale.expr){
-    if("logcounts" %in% expr.rescale.types){
-      message("")
-      if(!"logcounts" %in% names(assays(sce))){
-        sce <- scuttle::logNormCounts(sce)
-        save(sce, file = fname)
-      }
+  if("logcounts" %in% rescale.assays){
+    if(!"logcounts" %in% names(assays(sce))){
+      message("Getting logcounts...")
+      sce <- scuttle::logNormCounts(sce)
     }
   }
-  
-  
-  
-  # make k variables from cell type variable
-  if(do.k.series){
-    kvarname <- "k2"
-    if(!kvarname %in% colnames(colData(sce))){
-      sce[[kvarname]] <- ifelse(grepl("Excit|Inhib", sce[[ctvarname]]), 
-                                "neuron", "other")
-      save(sce, file = fname)
-    }
-  }
-  
+  sce <- parse_k_brain(sce, kvar, celltypevar)
   return(sce)
 }
