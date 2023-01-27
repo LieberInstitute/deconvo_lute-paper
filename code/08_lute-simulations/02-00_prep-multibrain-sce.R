859889d6635683b32d549b6bccc8698425d1da7c
Sean Maden <metamaden@users.noreply.github.com>
2023-01-26 16:14:52 -0800

    update mrb sce analysis
    updates .R script
    . gets top 20 markers by type
    . saves scef, filtered sce object
    updates rmd
    . adds many plots
    . adds glmGamPoi calls to get dist coeffs

--- code/08_lute-simulations/02-00_prep-multibrain-sce.R
+++ code/08_lute-simulations/02-00_prep-multibrain-sce.R
@@ -36,15 +36,33 @@ table(sce[["k2"]])
 #-------------------
 # get marker objects
 #-------------------
-
 # get markers of cell types
 mr <- get_mean_ratio2(sce, "k2", "logcounts")
 mr.fname <- "markers-k2_db-mr2_sce-dlpfc-mrb.rda"
 mr.fpath <- file.path(save.dpath, mr.fname)
 save(mr, file = mr.fpath)
 
+# get top markers -- 20 genes/type
+mr.fname <- "markers-k2_db-mr2-t20_sce-dlpfc-mrb.rda"
+mr.fpath <- file.path(save.dpath, mr.fname)
+type.variable <- "k2"
+ngenes.per.k <- 20
+typev <- unique(sce[["k2"]])
+ma.top <- do.call(rbind, lapply(typev, function(typei){
+  mr %>% filter(cellType.target == typei) %>% arrange(rank_ratio) %>%
+    top_n(n = ngenes.per.k)
+}))
+# get scef
+scef <- sce[ma.top$gene,]
+scef.fname <- "scef-mrb_gmr2-k2-t20_dlpfc.rda"
+save(scef, file = file.path(save.dpath, scef.fname))
+
 # get markers of donors
 mrd <- get_mean_ratio2(sce, "donor", "logcounts")
 mrd.fname <- "markers-donor_db-mr2_sce-dlpfc-mrb.rda"
 mrd.fpath <- file.path(save.dpath, mrd.fname)
 save(mrd, file = mrd.fpath)
+
+
+
+
