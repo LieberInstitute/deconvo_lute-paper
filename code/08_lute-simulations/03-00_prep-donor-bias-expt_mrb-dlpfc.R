7c258056e44c980cef9324e4055428f5a042e38a
Sean Maden <metamaden@users.noreply.github.com>
2023-02-01 12:18:00 -0800

    Update 03-00_prep-donor-bias-expt_mrb-dlpfc.R
    adds downsampling
    adds combat adjustments

--- code/08_lute-simulations/03-00_prep-donor-bias-expt_mrb-dlpfc.R
+++ code/08_lute-simulations/03-00_prep-donor-bias-expt_mrb-dlpfc.R
@@ -30,18 +30,58 @@ sce <- get(load(sce.fpath))
 #-------------------
 # get top 20 markers
 #-------------------
+nmarker <- 20
+typev <- unique(dfm$cellType.target)
+dfmt <- do.call(rbind, lapply(typev, function(typei){
+  dfm %>% filter(cellType.target == typei) %>% 
+    arrange(rank_ratio) %>%
+    top_n(n = nmarker)
+}))
+
+#------------------
+# format assay data
+#------------------
+for(ii in names(assays(sce))){
+  assays(sce)[[ii]] <- as.matrix(assays(sce)[[ii]])}
 
 #---------------------
 # perform downsampling
 #---------------------
+assays(sce)[["counts_ds"]] <- mexpr_downsample(assays(sce)[["counts"]])
+assays(sce)[["logcounts_ds"]] <- mexpr_downsample(assays(sce)[["logcounts"]])
 
 # save
+sce.fname <- "sce_ds-assays_mrb-dlpfc.rda"
+save(sce, file = file.path(save.dpath, sce.fname))
 
 #---------------
 # perform combat
 #---------------
+for(ai in names(assays(sce))){
+  message("Working on assay: ", ai)
+  ai <- "counts_ds"
+  mi <- assays(sce)[[ai]]
+  cnv <- colnames(mexpr)
+  pheno <- data.frame(donor = sce[["donor"]], type = sce[["k2"]])
+  mod <- model.matrix(~type, data = pheno)
+  batch <- pheno$donor
+  mi.adj <- ComBat(dat = mi, batch = batch, mod = mod, 
+             par.prior = TRUE, prior.plots = FALSE)
+  assays(sce)[[paste0(ai, "_combat")]] <- mi.adj
+}
 
 # save
+sce.fname <- "sce_combat-ds-assays_mrb-dlpfc.rda"
+save(sce, file = file.path(save.dpath, sce.fname))
+
+#-------------------
+# analysis functions
+#------------------
+get_anova_df <- function(mi, type = "complex"){
+  lmi <- lm()
+  avi <- anova(lmi)
+  dfa <- as.data.frame(avi)
+}
 
 #-----------------------------
 # get neg. binom. coefficients
