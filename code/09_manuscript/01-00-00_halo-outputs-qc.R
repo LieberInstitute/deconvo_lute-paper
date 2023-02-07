794274dadda86aca91a9aabf12b51e9670afb76d
Sean Maden <metamaden@users.noreply.github.com>
2023-02-07 14:03:03 -0800

    adds halo anova results
    adds transformations
    adds fev barplots
    adds density plots

--- code/09_manuscript/01-00-00_halo-outputs-qc.R
+++ code/09_manuscript/01-00-00_halo-outputs-qc.R
@@ -5,15 +5,101 @@
 #
 #
 
+libv <- c("ggplot2", "gridExtra")
+
+#-------------
+# manage paths
+#-------------
 # get save dpath
 code.dname <- "09_manuscript"
 proj.dname <- "deconvo_method-paper"
 save.dpath <- file.path(proj.dname, "outputs", code.dname)
 
+#---------------
 # load halo data
+#---------------
 fname <- "halo_all.Rdata"
 path <- file.path("Human_DLPFC_Deconvolution", "processed-data", "03_HALO", fname)
 dfh <- get(load(path))
+
+#--------------------
+# get transformations
+#--------------------
+# nuclear area
+# do log10 transformation
+dfh$nuc.area.log10 <- log10(dfh$Nucleus_Area)
+
+# akt3 
+# do quantile transform by subject
+samplev <- unique(dfh$Sample)
+datv <- unlist(lapply(samplev, function(si){
+  message("working on sample: ", si)
+  aktv <- dfh[dfh$Sample==si, ]$AKT3_Copies
+  qv <- quantile(aktv, seq(0, 1, 1e-3))
+  qlabv <- as.numeric(gsub("%", "", names(qv)))
+  qv2 <- qv[1:(length(qv)-1)]
+  newv <- rep("NA", length(aktv))
+  for(ii in seq(length(qv2))){
+    which.aktv <- aktv >= qv[ii] & aktv < qv[ii+1]
+    newv[which.aktv] <- qlabv[ii+1]
+  }
+  return(newv)
+}))
+datv <- as.numeric(datv)
+datv[is.na(datv)] <- median(datv, na.rm = T) # replace NAs
+plot(density(datv))
+
+dfh$akt3.copies.qscale <- as.numeric(datv)
+
+
+# qnorm method 2
+# library(preprocessCore)
+
+dfh$akt3.qnorm2 <- as.numeric(normalize.quantiles(as.matrix(dfh$AKT3_Copies, ncol = 1)))
+quantile_normalisation <- function(df){
+  df_rank <- apply(df,2,rank,ties.method="min")
+  df_sorted <- data.frame(apply(df, 2, sort))
+  df_mean <- apply(df_sorted, 1, mean)
+  
+  index_to_mean <- function(my_index, my_mean){
+    return(my_mean[my_index])
+  }
+  
+  df_final <- apply(df_rank, 2, index_to_mean, my_mean=df_mean)
+  rownames(df_final) <- rownames(df)
+  return(df_final)
+}
+
+#-------------------
+# plot distributions
+#-------------------
+dfp1 <- data.frame(untransformed = dfh$Nucleus_Area,
+                  transformed = dfh$nuc.area.log10,
+                  variable = "Nucleus_Area")
+dfp2 <- data.frame(untransformed = dfh$AKT3_Copies,
+                   transformed = dfh$akt3.copies.qscale,
+                   variable = "AKT3_Copies")
+dfp <- rbind(dfp1, dfp2)
+for(i in seq(2)){dfp[,i] <- as.numeric(dfp[,i])}
+
+p1 <- ggplot(dfp, aes(x = untransformed)) + 
+  geom_density() + theme_bw() +
+  ggtitle("Untransformed") + 
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_blank()) +
+  facet_wrap(~variable)
+p2 <- ggplot(dfp, aes(x = transformed)) + 
+  geom_density() + theme_bw() +
+  ggtitle("Transformed") + 
+  theme(axis.title.y = element_blank(),
+        axis.title.x = element_blank()) +
+  facet_wrap(~variable)
+
+fname <- "ggdens-composite_cellsize-halo_ro1-dlpfc.jpg"
+jpeg(file.path(save.dpath, fname), width = 5, height = 3.5,
+     units = "in", res = 400)
+grid.arrange(p1, p2, nrow = 2, left = "Density", bottom = "Value")
+dev.off()
 
 #----------------------
 # check key assumptions
@@ -57,7 +143,7 @@ anova(mod, lm)
 # anova analysis
 #---------------
 # nucleus area
-dep.var <- "Nucleus_Area"
+dep.var <- "nuc.area.log10"
 # basic model
 model.str <- paste0(dep.var, " ~ cell_type + Slide + SAMPLE_ID")
 avi1 <- eval(parse(text = paste0("aov(", model.str, ", data = dfh)")))
@@ -84,32 +170,51 @@ avi4 <- eval(parse(text = paste0("aov(", model.str, ", data = dfh)")))
 #----------------------------
 library(ggplot2)
 
+title.str <- "Nucleus area (log10-transformed)"
+avi <- avi2
 dfp <- as.data.frame(summary(avi)[[1]])
 dfp <- data.frame(variable = gsub(" ", "", rownames(dfp)), ssq = dfp[,2])
 dfp$perc.var <- 100*dfp$ssq/sum(dfp$ssq)
+dfp$label <- paste0(round(dfp$perc.var, 1), "%")
 
 level.order <- rev(order(dfp$perc.var))
 dfp$variable <- factor(dfp$variable, levels = dfp$variable[level.order])
 
-ggplot(dfp, aes(x = variable, y = perc.var, fill = variable)) +
+ggbar <- ggplot(dfp, aes(x = variable, y = perc.var, fill = variable)) +
   geom_bar(stat = "identity") + theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
-  ylab("Total percent variance")
+  ylab("Total percent variance") + ggtitle(title.str) +
+  geom_text(aes(x = variable, y = perc.var, label = label)) +
+  theme(legend.position = "none")
+fname <- "ggbar-perc-var_nalog10-complex_ro1-dlpfc.jpg"
+jpeg(file.path(save.dpath, fname), width = 3.5, height = 2.5, 
+     units = "in", res = 400)
+ggbar; dev.off()
+
+ggbar <- ggplot(dfp, aes(x = variable, y = perc.var, fill = variable)) +
+  geom_bar(stat = "identity") + theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.position = "none") +
+  ylab("Percent variance\n(log10-scaled)") + 
+  scale_y_log10() + ggtitle(title.str) +
+  geom_text(aes(x = variable, y = perc.var, label = label))
+fname <- "ggbar-perc-var-log10_nalog10-complex_ro1-dlpfc.jpg"
+jpeg(file.path(save.dpath, fname), width = 3.5, height = 2.5, 
+     units = "in", res = 400)
+ggbar; dev.off()
 
 dfpe <- dfp[!dfp$variable=="Residuals",]
 dfpe$perc.var <- 100*dfpe$ssq/sum(dfpe$ssq)
-dfpe$label <- paste0(round(dfpe$perc.var, 2), "%")
-ggplot(dfpe, aes(x = variable, y = perc.var, fill = variable)) +
+dfpe$label <- paste0(round(dfpe$perc.var, 1), "%")
+ggbar <- ggplot(dfpe, aes(x = variable, y = perc.var, fill = variable)) +
   geom_bar(stat = "identity") + theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1),
         legend.position = "none") +
-  ylab("Total explained variance") +
+  ylab("Total explained variance") + ggtitle(title.str) +
   geom_text(aes(x = variable, y = perc.var, label = label))
-
-ggplot(dfpe, aes(x = variable, y = perc.var)) +
-  geom_bar(stat = "identity", position = "stack") + theme_bw() +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1),
-        legend.position = "none") +
-  ylab("Total explained variance")
+fname <- "ggbar-perc-expl-var_nalog10-complex_ro1-dlpfc.jpg"
+jpeg(file.path(save.dpath, fname), width = 3.5, height = 2.5, 
+     units = "in", res = 400)
+ggbar; dev.off()
 
 
