97980a10d61ddf280acbf827410cdd8602a87302
Sean Maden <metamaden@users.noreply.github.com>
2023-02-13 16:11:44 -0800

    Update 01_donor-bias-meanvar_ro1-dlpfc.R
    adds mean matrix, by type;donor
    adds var matrix, by type;donor
    adds mean-var plots

--- code/10_donor-bias-simulations-continued/01_donor-bias-meanvar_ro1-dlpfc.R
+++ code/10_donor-bias-simulations-continued/01_donor-bias-meanvar_ro1-dlpfc.R
@@ -5,7 +5,8 @@
 #
 #
 
-libv <- c("SingleCellExperiment", "limma", "SummarizedExperiment", "scuttle")
+libv <- c("SingleCellExperiment", "limma", "SummarizedExperiment", "scuttle",
+          "ggplot2")
 sapply(libv, library, character.only = TRUE)
 
 #---------------
@@ -32,9 +33,57 @@ scei <- get(load(sce.fpath))
 #-----------------------------
 # means and variances by donor
 #-----------------------------
-groupvar <- paste0(scei[["k2"]], ";", scei[["Sample"]])
-table(groupvar)
+#groupvar <- paste0(scei[["k2"]], ";", scei[["Sample"]])
+#table(groupvar)
+#scei[["groupvar"]] <- groupvar
+#sce.var <- aggregateAcrossCells(scei, ids = scei[["groupvar"]], statistics = "var")
 
-scei[["groupvar"]] <- groupvar
-sce.var <- aggregateAcrossCells(scei, ids = "groupvar", statistics = "var")
+assayname <- "counts_adj"
+varname <- "groupvar"
+scei[[varname]] <- paste0(scei[["k2"]], ";", scei[["Sample"]])
+unique.groups <- unique(scei[[varname]])
+sce.var <- do.call(cbind, lapply(unique.groups, function(groupi){
+  message("working on group level ", groupi)
+  filt <- scei[[varname]] == groupi
+  rowVars(assays(scei[,filt])[[assayname]])
+}))
+
+sce.mean <- do.call(cbind, lapply(unique.groups, function(groupi){
+  message("working on group level ", groupi)
+  filt <- scei[[varname]] == groupi
+  rowMeans(assays(scei[,filt])[[assayname]])
+}))
+
+colnames(sce.mean) <- colnames(sce.var) <- unique.groups
+
+# save
+# variances
+fname <- paste0("matrix-donor-type_variances_",markeri, "_", handle.str,".rda")
+save(sce.var, file = file.path(save.dpath, fname))
+# means
+fname <- paste0("matrix-donor-type_means_",markeri, "_", handle.str,".rda")
+save(sce.mean, file = file.path(save.dpath, fname))
+
+# plots
+genev <- rownames(sce.mean)
+groupv <- colnames(sce.mean)
+dfp <- do.call(rbind, lapply(seq(ncol(sce.mean)), function(ii){
+  dfpi <- data.frame(gene = genev, mean = sce.mean[,ii], var = sce.var[,ii])
+  dfpi$group <- groupv[ii]; return(dfpi)
+}))
+dfp$celltype <- gsub(";.*", "", dfp$group)
+dfp$donor <- gsub(".*;", "", dfp$group)
+
+ggsm <- ggplot(dfp, aes(x = mean, y = var, color = donor, group = donor)) +
+  geom_smooth() + facet_wrap(~celltype) + theme_bw() +
+  geom_abline(intercept = 0, slope = 1, color = "black") +
+  scale_x_log10() + scale_y_log10()
+
+# save jpg
+save.dpath <- "."
+fname <- "ggsmooth-composite_k2-types-by-donor_ro1-dlpfc.jpg"
+jpeg(file = file.path(save.dpath, fname), width = 8, height = 4, 
+     units = "in", res = 400)
+print(ggsm); dev.off()
+
 
