30c5f1e14d562940c7ff320676f6589f09958ad3
Sean Maden <metamaden@users.noreply.github.com>
2023-02-17 08:46:44 -0800

    Create 03-01_cell-type-scale-factors_corr-test.R

---
+++ code/09_manuscript/03-01_cell-type-scale-factors_corr-test.R
@@ -0,0 +1,49 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+# Correlation tests between cell type scale factors.
+#
+#
+
+# manage paths
+code.dname <- "09_manuscript"
+proj.dname <- "deconvo_method-paper"
+save.dpath <- file.path(proj.dname, "outputs", code.dname)
+
+#-------------------------
+# get marker scale factors
+#-------------------------
+handle.str <- "ro1-dlpfc"
+marker.typev <- c("k2", "k3", "k4")
+
+lscef <- lapply(marker.typev, function(markeri){
+  message("loading the data...")
+  sce.fname <- paste0("sce_marker-adj-",markeri,"_",
+                      handle.str,".rda")
+  sce.fpath <- file.path(save.dpath, sce.fname)
+  scei <- get(load(sce.fpath))
+  
+  # get cell size scale factors
+  type.vector <- scei[[markeri]]
+  unique.types <- unique(typev)
+  # get tall table of total mrna by type
+  df.tc <- do.call(rbind, lapply(unique.types, function(typei){
+    ctf <- counts(scei[,scei[[markeri]]==typei])
+    dfi <- data.frame(total.count = colSums(ctf))
+    dfi$type <- typei; return(dfi)
+  }))
+  # get tall table of total expressed genes by type
+  df.eg <- do.call(rbind, lapply(unique.types, function(typei){
+    ctf <- counts(scei[,scei[[markeri]]==typei])
+    m.eg <- apply(ctf,2,function(ci){length(ci[ci>0])})
+    dfi <- data.frame(total.expr.genes = as.numeric(unlist(m.eg)))
+    dfi$type <- typei; return(dfi)
+  }))
+  
+  # get marker expr
+  mr <- metadata(sce)[["markers"]][["top"]] # top markers
+  scef <- scei[mr$gene,]
+  return(scef)
+})
+names(lscef) <- marker.typev
