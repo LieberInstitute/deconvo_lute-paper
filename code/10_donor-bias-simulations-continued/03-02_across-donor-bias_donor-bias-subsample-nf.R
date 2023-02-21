8ed2c4cc032f200fd4008bcda3b29bf404337e52
Sean Maden <metamaden@users.noreply.github.com>
2023-02-21 14:00:37 -0800

    saves new workflow table
    defines workflow table for inter-sample tests
    saves new workflow table

--- code/10_donor-bias-simulations-continued/03-02_across-donor-bias_donor-bias-subsample-nf.R
+++ code/10_donor-bias-simulations-continued/03-02_across-donor-bias_donor-bias-subsample-nf.R
@@ -32,26 +32,29 @@ sample.variable <- "Sample"
 assay.name <- "counts_adj"
 
 # set number of iterations
-num.iter.intra <- 10
-num.iter.inter <- 10
-
+num.iter <- 10
 # set fract cells per iter
 fract.cells.iter <- 20
-
+# number of samples to select per iteration
+num.sample.iter <- 3
+# set the cell size factors for pseudobulk
+S <- c("glial" = 3, "neuron" = 10)
 # filepaths
 rnf.data.fpath <- file.path("r-nf_deconvolution_donor-bias", "data")
 
-# intra data filepaths
-# sce intra
-sce.intra.fname <- "sce_largest-donor.rda"
-sce.intra.fpath <- file.path(save.dpath, rnf.data.fpath, sce.intra.fname)
-# lindex intra
+# save paths
+sce.fname <- "sce_all-samples.rda"
+sce.fpath <- file.path(save.dpath, rnf.data.fpath, sce.fname)
+# lindex
 lindex.fname <- "lindex_inter.rda"
 lindex.fpath <- file.path(save.dpath, rnf.data.fpath, lindex.fname)
+# ypb
+ypb.fname <- "ypb_inter.rda"
+ypb.fpath <- file.path(save.dpath, rnf.data.fpath, ypb.fname)
 
 # workflow table
-wt.fname <- "workflow-table_intra.csv"
-wt.intra.fpath <- file.path(save.dpath, rnf.data.fpath, wt.fname)
+wt.fname <- "workflow-table_inter-sample.csv"
+wt.fpath <- file.path(save.dpath, rnf.data.fpath, wt.fname)
 
 #----------
 # load data
@@ -71,17 +74,18 @@ dft <- as.data.frame(table(sce[[celltype.variable]],
 num.cells.glial <- round(min(dft[dft[,1]=="glial",3])*0.3, 0)
 num.cells.neuron <- round(min(dft[dft[,1]=="neuron",3])*0.3, 0)
 # get random cell indices as a list
-num.donor.iter <- 3
 cd <- colData(sce)
 unique.samples <- unique(cd[,sample.variable])
 unique.types <- unique(cd[,celltype.variable])
 unique.types <- unique.types[order(unique.types)]
-S <- c("glial" = 3, "neuron" = 10)
+# order s cell size factors
 S <- S[order(match(names(S), unique.types))]
 num.cellsv <- c("glial" = num.cells.glial, "neuron" = num.cells.neuron)
-lindex <- lapply(seq(num.iter.inter), function(ii){
+lindex <- lapply(seq(num.iter), function(ii){
   set.seed(ii)
-  random.samples <-  sample(unique.samples, num.donor.iter)
+  
+  # get filtered sce data as scef
+  random.samples <-  sample(unique.samples, num.sample.iter)
   filt <- cd[,sample.variable] %in% random.samples
   cdf <- cd[filt,]
   vindex <- which(colnames(sce) %in% unlist(lapply(unique.types, function(typei){
@@ -94,59 +98,47 @@ lindex <- lapply(seq(num.iter.inter), function(ii){
   tp.prop <- tp[,2]; names(tp.prop) <- tp[,1]
   tp.prop <- tp.prop/sum(tp[,2])
   
-  # get pseudobulk matrix
-  P <- tp.prop
-  # get signature matrix
-  Z <- do.call(cbind, lapply(unique.types, function(typei){
-    rowMeans(assays(scef)[[assay.name]])
-  }))
-  ZS <- sweep(Z, 2, S, "*")
-  ypb <- t(t(P) %*% t(ZS))
-  
   # return results
   list(vindex = vindex, tp = tp.prop, ypb = ypb, samples = random.samples)
 })
 # save indices
 save(lindex, file = lindex.fpath)
 
-# true proportions
-dft <- as.data.frame(table(scef[[celltype.variable]]))
-tp <- dft[,2]/sum(dft[,2])
-names(tp) <- dft[,1]
-save(tp, file = tp.intra.fpath)
-
-# pseudobulk
-# true proportions 
-P <- tp
-# get signature matrix
-unique.types <- unique(scef[[celltype.variable]])
+# get pseudobulk data
+tp <- as.data.frame(table(sce[[celltype.variable]]))
+tp.prop <- tp[,2]; names(tp.prop) <- tp[,1]
+P <- tp.prop/sum(tp.prop)
 Z <- do.call(cbind, lapply(unique.types, function(typei){
   rowMeans(assays(scef)[[assay.name]])
 }))
-S <- c(10, 3)
 ZS <- sweep(Z, 2, S, "*")
 ypb <- t(t(P) %*% t(ZS))
-# save 
-save(ypb, file = ypb.intra.fpath)
+# save pseudobulk
+save(ypb, file = ypb.fpath)
 
 #---------------------
 # write workflow table
 #---------------------
-wt <- data.frame(iterations_index = seq(num.iter.intra))
-wt$method <- "nnls"
-wt$sample_id <- largest.donor.id
-wt$celltype_variable <- celltype.variable
-wt$assay_name <- assay.name
-# manage filepaths
-cnamev <- c("sce_filepath", "true_proportions_filepath", 
-            "bulk_filepath", "index_matrix_filepath")
-fpathv <- c(file.path("data", sce.intra.fname),
-            file.path("data", tp.intra.fname),
-            file.path("data", ypb.intra.fname),
-            file.path("data", mi.intra.fname))
-for(ii in seq(length(cnamev))){wt[,cnamev[ii]] <- paste0('"$launchDir/', fpathv[ii], '"')}
+methodv <- c("nnls", "music")
+wt <- do.call(rbind, lapply(methodv, function(methodi){
+  wti <- data.frame(iterations_index = seq(num.iter))
+  wti$method <- methodi
+  wti$sample_id <- unlist(lapply(lindex, function(li){
+    paste0(li$samples, collapse = ";")}))
+  wti$celltype_variable <- celltype.variable
+  wti$assay_name <- assay.name
+  # manage filepaths
+  cnamev <- c("sce_filepath", "bulk_filepath", "list_index_fpath")
+  fpathv <- c(file.path("data", sce.fname),
+              file.path("data", ypb.fname),
+              file.path("data", lindex.fname))
+  for(ii in seq(length(cnamev))){
+    wti[,cnamev[ii]] <- paste0('"$launchDir/', fpathv[ii], '"')}
+  wti
+}))
+
 # save
-write.csv(wt, file = wt.intra.fpath, row.names = F)
+write.csv(wt, file = wt.fpath, row.names = F)
 
 
 
