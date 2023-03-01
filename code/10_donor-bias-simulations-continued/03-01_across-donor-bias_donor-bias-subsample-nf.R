3e7be969b5c8e056b2322b17f2a31000e5704277
Sean Maden <metamaden@users.noreply.github.com>
2023-03-01 14:05:49 -0800

    adds inter-sample bias results, plots
    adds results of 1k iterations, 4 methods for inter-sample bias
    adds plots of subsample experiment results

--- code/10_donor-bias-simulations-continued/03-01_across-donor-bias_donor-bias-subsample-nf.R
+++ code/10_donor-bias-simulations-continued/03-01_across-donor-bias_donor-bias-subsample-nf.R
@@ -106,4 +106,105 @@ rt.fpath <- file.path(save.dpath, rnf.dname, "data", rt.fname)
 rt <- read.csv(rt.fpath)
 
 
+library(ggplot2)
+library(gridExtra)
+library(GGally)
 
+# plot pairs
+method.varname <- "deconvolution_method"
+# varname <- "prop.pred.type1"
+# filter duplicate entries
+rtf <- rt
+rtf$label <- paste0(rtf$iterations_index, ";", rtf$deconvolution_method)
+rtf <- rtf[!duplicated(rtf$label),]
+methodv <- unique(rtf[,method.varname])
+# iterate on plot variables
+varv <- c("prop.pred.type1", "prop.pred.type2", "bias.type1", "bias.type2", "rmse.types")
+lgg <- lapply(varv, function(varname){
+  dfp <- do.call(cbind, lapply(methodv, function(methodi){
+    filt <- rtf[,method.varname]==methodi; rti <- rtf[filt,]
+    rti <- rti[order(rti$iterations_index),]; rti[,varname]
+  }))
+  dfp <- as.data.frame(dfp); colnames(dfp) <- methodv
+  ggpairs(dfp) + ggtitle(varname)
+})
+names(lgg) <- varv
+
+lgg$prop.pred.type1
+lgg$prop.pred.type2
+lgg$bias.type1
+lgg$bias.type2
+lgg$rmse.types
+
+# violin plots
+#
+lgg <- lapply(varv, function(varname){
+  dfp <- rtf; dfp$value <- dfp[,varname]
+  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
+    geom_violin(draw_quantiles = 0.5) + ggtitle(varname)
+})
+jpeg("ggvp-comp_inter-sample-bias.jpg", width = 5, height = 4,
+     units = "in", res = 400)
+grid.arrange(lgg[[1]], lgg[[2]], lgg[[3]], lgg[[4]], lgg[[5]],
+             layout_matrix = matrix(c(1,2,3,4,5), nrow = 2))
+dev.off()
+# bias plots
+plot1 <- lgg[[3]] + theme_bw() +
+  theme(axis.text.x = element_blank(),
+                          axis.title.x = element_blank()) +
+  geom_hline(yintercept = 0, alpha = 0.2)
+plot2 <- lgg[[4]] + theme_bw() +
+  theme(axis.title.x = element_blank(),
+                          axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_hline(yintercept = 0, alpha = 0.2)
+jpeg("ggvp-comp-bias_inter-sample-bias.jpg", 
+     width = 6, height = 5, units = "in", res = 400)
+grid.arrange(plot1, plot2, nrow = 2,
+             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
+             bottom = "Deconvolution method")
+dev.off()
+
+# jitter plots
+lgg <- lapply(varv, function(varname){
+  dfp <- rtf; dfp$value <- dfp[,varname]
+  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
+    geom_jitter(alpha = 0.5) + ggtitle(varname) +
+    geom_boxplot(alpha = 0, color = "cyan", size = 1)
+})
+# bias plots
+plot1 <- lgg[[3]] + theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank()) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+plot2 <- lgg[[4]] + theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+jpeg("ggjitterbox-comp-bias_inter-sample-bias.jpg", 
+     width = 6, height = 5, units = "in", res = 400)
+grid.arrange(plot1, plot2, nrow = 2,
+             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
+             bottom = "Deconvolution method")
+dev.off()
+
+# scatter plot bias
+# no facet
+ggpt <- ggplot(rtf, aes(x = bias.type1, y = bias.type2, 
+                group = deconvolution_method, 
+                color = deconvolution_method)) +
+  geom_point(alpha = 0.2) + geom_abline(slope = 1, intercept = 0) +
+  theme_bw() + 
+  geom_hline(yintercept = 0, alpha = 0.5, color = "gray") +
+  geom_vline(xintercept = 0, alpha = 0.5, color = "gray")
+# facet
+ggpt <- ggplot(rtf, aes(x = bias.type1, y = bias.type2)) +
+  geom_point(alpha = 0.2) + geom_abline(slope = 1, intercept = 0) +
+  theme_bw() + 
+  geom_hline(yintercept = 0, alpha = 0.5, color = "gray") +
+  geom_vline(xintercept = 0, alpha = 0.5, color = "gray")
+ggpt <- ggpt + facet_wrap(~deconvolution_method)
+jpeg('ggpt-facet-method_inter-sample-bias.jpg', 
+     width = 6, height = 6, 
+     units = "in", res = 400)
+ggpt
+dev.off()
