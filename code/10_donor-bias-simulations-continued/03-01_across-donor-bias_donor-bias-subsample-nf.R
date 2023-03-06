43ce718abbdaf8f44bb07df2afc025908fbd6a92
Sean Maden <metamaden@users.noreply.github.com>
2023-03-06 08:13:21 -0800

    organize scripts
    moves main plots up
    moves dependency calls to top

--- code/10_donor-bias-simulations-continued/03-01_across-donor-bias_donor-bias-subsample-nf.R
+++ code/10_donor-bias-simulations-continued/03-01_across-donor-bias_donor-bias-subsample-nf.R
@@ -9,7 +9,7 @@
 #
 
 libv <- c("lute", "SummarizedExperiment", "SingleCellExperiment", 
-          "ggplot2", "gridExtra")
+          "ggplot2", "gridExtra", "GGally")
 sapply(libv, library, character.only = TRUE)
 
 #------------------
@@ -122,21 +122,41 @@ rt.fname <- lfv[grepl(results.filt, lfv)]
 rt.fpath <- file.path(save.dpath, rnf.dname, "data", rt.fname)
 rt <- read.csv(rt.fpath)
 
-
-library(ggplot2)
-library(gridExtra)
-library(GGally)
-
-# plot pairs
+# prep results table
+varv <- c("prop.pred.type1", "prop.pred.type2", "bias.type1", "bias.type2", "rmse.types")
 method.varname <- "deconvolution_method"
-# varname <- "prop.pred.type1"
 # filter duplicate entries
 rtf <- rt
 rtf$label <- paste0(rtf$iterations_index, ";", rtf$deconvolution_method)
 rtf <- rtf[!duplicated(rtf$label),]
 methodv <- unique(rtf[,method.varname])
+
+# jitter plots
+lgg <- lapply(varv, function(varname){
+  dfp <- rtf; dfp$value <- dfp[,varname]
+  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
+    geom_jitter(alpha = 0.5) + ggtitle(varname) +
+    geom_boxplot(alpha = 0, color = "cyan", size = 1)
+})
+# bias plots
+plot1 <- lgg[[3]] + theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank()) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+plot2 <- lgg[[4]] + theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+jpeg("ggjitterbox-comp-bias_inter-sample-bias.jpg", 
+     width = 6, height = 5, units = "in", res = 400)
+grid.arrange(plot1, plot2, nrow = 2,
+             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
+             bottom = "Deconvolution method")
+dev.off()
+
+# plot pairs
+# varname <- "prop.pred.type1"
 # iterate on plot variables
-varv <- c("prop.pred.type1", "prop.pred.type2", "bias.type1", "bias.type2", "rmse.types")
 lgg <- lapply(varv, function(varname){
   dfp <- do.call(cbind, lapply(methodv, function(methodi){
     filt <- rtf[,method.varname]==methodi; rti <- rtf[filt,]
@@ -175,29 +195,6 @@ plot2 <- lgg[[4]] + theme_bw() +
                           axis.text.x = element_text(angle = 45, hjust = 1)) +
   geom_hline(yintercept = 0, alpha = 0.2)
 jpeg("ggvp-comp-bias_inter-sample-bias.jpg", 
-     width = 6, height = 5, units = "in", res = 400)
-grid.arrange(plot1, plot2, nrow = 2,
-             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
-             bottom = "Deconvolution method")
-dev.off()
-
-# jitter plots
-lgg <- lapply(varv, function(varname){
-  dfp <- rtf; dfp$value <- dfp[,varname]
-  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
-    geom_jitter(alpha = 0.5) + ggtitle(varname) +
-    geom_boxplot(alpha = 0, color = "cyan", size = 1)
-})
-# bias plots
-plot1 <- lgg[[3]] + theme_bw() +
-  theme(axis.text.x = element_blank(),
-        axis.title.x = element_blank()) +
-  geom_hline(yintercept = 0, alpha = 1, color = "red")
-plot2 <- lgg[[4]] + theme_bw() +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1)) +
-  geom_hline(yintercept = 0, alpha = 1, color = "red")
-jpeg("ggjitterbox-comp-bias_inter-sample-bias.jpg", 
      width = 6, height = 5, units = "in", res = 400)
 grid.arrange(plot1, plot2, nrow = 2,
              layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
--- code/10_donor-bias-simulations-continued/03-02_within-donor-bias_donor-bias-subsample-nf.R
+++ code/10_donor-bias-simulations-continued/03-02_within-donor-bias_donor-bias-subsample-nf.R
@@ -135,16 +135,43 @@ library(ggplot2)
 library(gridExtra)
 library(GGally)
 
-# plot pairs
 method.varname <- "deconvolution_method"
-# varname <- "prop.pred.type1"
-# filter duplicate entries
+# iterate on plot variables
+varv <- c("prop.pred.type1", "prop.pred.type2", "bias.type1", 
+          "bias.type2", "rmse.types")
+
+# jitter plots
+# filter duplicated data
 rtf <- rt
 rtf$label <- paste0(rtf$iterations_index, ";", rtf$deconvolution_method)
 rtf <- rtf[!duplicated(rtf$label),]
 methodv <- unique(rtf[,method.varname])
-# iterate on plot variables
-varv <- c("prop.pred.type1", "prop.pred.type2", "bias.type1", "bias.type2", "rmse.types")
+# get plots list
+lgg <- lapply(varv, function(varname){
+  dfp <- rtf; dfp$value <- dfp[,varname]
+  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
+    geom_jitter(alpha = 0.5) + ggtitle(varname) +
+    geom_boxplot(alpha = 0, color = "cyan", size = 1)
+})
+# bias plots
+plot1 <- lgg[[3]] + theme_bw() +
+  theme(axis.text.x = element_blank(),
+        axis.title.x = element_blank()) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+plot2 <- lgg[[4]] + theme_bw() +
+  theme(axis.title.x = element_blank(),
+        axis.text.x = element_text(angle = 45, hjust = 1)) +
+  geom_hline(yintercept = 0, alpha = 1, color = "red")
+jpeg("ggjitterbox-comp-bias_intra-sample-bias.jpg", 
+     width = 6, height = 5, units = "in", res = 400)
+grid.arrange(plot1, plot2, nrow = 2,
+             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
+             bottom = "Deconvolution method")
+dev.off()
+
+# plot pairs
+# varname <- "prop.pred.type1"
+# filter duplicate entries
 lgg <- lapply(varv, function(varname){
   dfp <- do.call(cbind, lapply(methodv, function(methodi){
     filt <- rtf[,method.varname]==methodi; rti <- rtf[filt,]
@@ -178,29 +205,6 @@ plot2 <- lgg[[4]] + theme_bw() +
         axis.text.x = element_text(angle = 45, hjust = 1)) +
   geom_hline(yintercept = 0, alpha = 0.2)
 jpeg("ggvp-comp-bias_intra-sample-bias.jpg", 
-     width = 6, height = 5, units = "in", res = 400)
-grid.arrange(plot1, plot2, nrow = 2,
-             layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
-             bottom = "Deconvolution method")
-dev.off()
-
-# jitter plots
-lgg <- lapply(varv, function(varname){
-  dfp <- rtf; dfp$value <- dfp[,varname]
-  ggplot(dfp, aes(x = deconvolution_method, y = value)) +
-    geom_jitter(alpha = 0.5) + ggtitle(varname) +
-    geom_boxplot(alpha = 0, color = "cyan", size = 1)
-})
-# bias plots
-plot1 <- lgg[[3]] + theme_bw() +
-  theme(axis.text.x = element_blank(),
-        axis.title.x = element_blank()) +
-  geom_hline(yintercept = 0, alpha = 1, color = "red")
-plot2 <- lgg[[4]] + theme_bw() +
-  theme(axis.title.x = element_blank(),
-        axis.text.x = element_text(angle = 45, hjust = 1)) +
-  geom_hline(yintercept = 0, alpha = 1, color = "red")
-jpeg("ggjitterbox-comp-bias_intra-sample-bias.jpg", 
      width = 6, height = 5, units = "in", res = 400)
 grid.arrange(plot1, plot2, nrow = 2,
              layout_matrix = matrix(c(1,1,1,2,2,2,2), ncol = 1),
