491bcc5c98f2b96b6ae95ff247eeef5a3a457d76
Sean Maden <metamaden@users.noreply.github.com>
2023-03-08 14:30:19 -0800

    Update 01-01_check-bulk-qc.R
    functionalize jitter plots, data summaries

--- code/11_bulk-expression-analysis/01-01_check-bulk-qc.R
+++ code/11_bulk-expression-analysis/01-01_check-bulk-qc.R
@@ -23,33 +23,78 @@ rse <- get(load(file.path(path, rse.filename)))
 save.path <- file.path("deconvo_method-paper", "outputs", 
                        "11_bulk-expression-analysis")
 
+#-----------------
+# helper functions
+#-----------------
+group_jitter <- function(variable.name, cd, counts,
+                         ggtitle.string = "Total counts by group",
+                         type = "total.counts"){
+  # get plot data
+  group.vector <- unique(cd[,variable.name])
+  if(type == "total.counts"){
+    value.string <- "Total counts"
+    dfp <- do.call(rbind, lapply(group.vector, function(gi){
+      filter <- cd[,variable.name]==gi
+      dfi <- data.frame(value = colSums(counts[,filter]))
+      dfi$group <- gi; return(dfi)
+    }))
+  } else if(type == "number.zero"){
+    value.string <- "Num. zero"
+    dfp <- do.call(rbind, lapply(group.vector, function(gi){
+      filter <- cd[,variable.name]==gi
+      cf <- counts[,filter]
+      
+      num.zero <- apply(cf,2,function(ci){length(ci[ci==0])})
+      
+      dfi <- data.frame(value = colSums(counts[,filter]))
+      dfi$group <- gi; return(dfi)
+    }))
+  } else{
+    stop("Error, unrecognized type argument.")
+  }
+  
+  
+  # order group as factor
+  labels.vector <- unique(dfp$group)
+  levels.vector <- unlist(sapply(labels.vector, function(li){
+    median(dfp[dfp[,2]==li,1])}))
+  dfp$group <- factor(dfp$group, 
+                      levels = labels.vector[order(levels.vector)])
+  
+  # make new plot object
+  new.plot <- ggplot(dfp, aes(x = group, y = total.counts)) + theme_bw() +
+    geom_jitter() + geom_boxplot(color = "cyan", alpha = 0) +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+    ggtitle(ggtitle.string) + xlab(value.string)
+  return(list(dfp = dfp, new.plot = new.plot))
+}
+
+
 #---------------------------
 # total expression summaries
 #---------------------------
 # params
 assay.name <- "counts"
 batch.variable <- "batch.id"
+condition.variable <- "expt_condition"
 # rse data
 cd <- colData(rse)
-cd[,batch.variable] <- paste0(cd$BrNum,"_",cd$location)
 counts <- assays(rse)[[assay.name]]
+# get new cd variables
+cd[,batch.variable] <- paste0(cd$BrNum,"_",cd$location)
+cd[,condition.variable] <- paste0(cd$library_prep,"_",cd$library_type)
 
 # total expression by sample
-dfp <- data.frame(total.counts = colSums(counts))
-dfp$sample <- cd[,batch.variable]
-labels.vector <- unique(dfp$sample)
-levels.vector <- unlist(sapply(labels.vector, function(li){median(dfp[dfp[,2]==li,1])}))
-dfp$sample <- factor(dfp$sample, levels = labels.vector[order(levels.vector)])
-ggplot(dfp, aes(x = sample, y = total.counts)) + theme_bw() +
-  geom_jitter() + geom_boxplot(color = "cyan", alpha = 0) +
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+ljitter1 <- group_jitter(batch.variable, cd, counts, "Batch ID")
 
 # total expression by library prep
+ljitter2 <- group_jitter("library_prep", cd, counts, "Batch ID")
 
 # total expression by condition
+ljitter3 <- group_jitter("library_type", cd, counts, "Batch ID")
 
 # total expression by condition, library prep
-
+ljitter4 <- group_jitter(condition.variable, cd, counts, "Batch ID")
 
 #-----------------------------------
 # missing/unexpressed gene summaries
