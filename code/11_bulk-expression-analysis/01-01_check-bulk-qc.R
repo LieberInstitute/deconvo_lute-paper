7fe432f61a37e9b1b01e4bf22fd902d920b15b6b
Sean Maden <metamaden@users.noreply.github.com>
2023-03-08 14:05:47 -0800

    Update 01-01_check-bulk-qc.R
    begins adding total counts summaries by batch variables

--- code/11_bulk-expression-analysis/01-01_check-bulk-qc.R
+++ code/11_bulk-expression-analysis/01-01_check-bulk-qc.R
@@ -4,7 +4,7 @@
 #
 #
 
-libv <- c("SummarizedExperiment")
+libv <- c("SummarizedExperiment", "ggplot2")
 sapply(libv, library, character.only = T)
 
 #----------
@@ -26,6 +26,30 @@ save.path <- file.path("deconvo_method-paper", "outputs",
 #---------------------------
 # total expression summaries
 #---------------------------
+# params
+assay.name <- "counts"
+batch.variable <- "batch.id"
+# rse data
+cd <- colData(rse)
+cd[,batch.variable] <- paste0(cd$BrNum,"_",cd$location)
+counts <- assays(rse)[[assay.name]]
+
+# total expression by sample
+dfp <- data.frame(total.counts = colSums(counts))
+dfp$sample <- cd[,batch.variable]
+labels.vector <- unique(dfp$sample)
+levels.vector <- unlist(sapply(labels.vector, function(li){median(dfp[dfp[,2]==li,1])}))
+dfp$sample <- factor(dfp$sample, levels = labels.vector[order(levels.vector)])
+ggplot(dfp, aes(x = sample, y = total.counts)) + theme_bw() +
+  geom_jitter() + geom_boxplot(color = "cyan", alpha = 0) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# total expression by library prep
+
+# total expression by condition
+
+# total expression by condition, library prep
+
 
 #-----------------------------------
 # missing/unexpressed gene summaries
