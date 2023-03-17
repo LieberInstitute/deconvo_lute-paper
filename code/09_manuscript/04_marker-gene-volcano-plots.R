ac738ba02c4770bc407ebb41f9eabc07c0895007
Sean Maden <metamaden@users.noreply.github.com>
2023-03-17 14:28:03 -0700

    Update 04_marker-gene-volcano-plots.R

--- code/09_manuscript/04_marker-gene-volcano-plots.R
+++ code/09_manuscript/04_marker-gene-volcano-plots.R
@@ -1,5 +1,26 @@
 # load full data
+sce.fname <- "sce_DLPFC.Rdata"
+sce.dpath <- "DLPFC_snRNAseq/processed-data/sce"
+sce.fpath <- file.path(sce.dpath, sce.fname)
+sce <- get(load(sce.fpath))
 # load marker data
-# get plot data
-# make plot object
+markers.filename <- "list-scef_markers-k2-k3-k4_ro1-dlpfc.rda"
+sce.markers.path <- file.path("deconvo_method-paper", "outputs", 
+	"09_manuscript", markers.filename)
+lsce <- get(load(sce.markers.path))
+sce <- lsce[["k2"]]
+rm(lsce)
+# get volcano plot
+library(ggplot2); library(ggrepel)
+filepath=""
+dfp <- get(load(filepath))
+dfp$y.axis <- -1*log10(dfp$p.value)
+dfp$x.axis <- dfp$logFC
+dfp.subset <- dfp[dfp$p.value <= 1e-3,]
+newplot <- ggplot(dfp, aes(x = x.axis, y = y.axis)) + geom_point(alpha = 0.2) + 
+geom_point(data = dfp.subset, aes(x = x.axis, y = y.axis), color = "red") +
+geom_hline(yintercept = max(dfp.subset$y.axis), color = "black") +
+geom_vline(xintercept = 0, color = "black") +
+geom_label_repel(box.padding   = 0.35, point.padding = 0.8, segment.color = 'grey50')
 # save plot object
+ggsave(newplot)
