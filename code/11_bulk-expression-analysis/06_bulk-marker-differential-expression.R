46dfc31fbc08d976abe4ee0dd2962a7649ff8ab3
Sean Maden <metamaden@users.noreply.github.com>
2023-03-19 17:05:50 -0700

    Update 06_bulk-marker-differential-expression.R
    trying different deseq2 input formats
    adds format conversions

--- code/11_bulk-expression-analysis/06_bulk-marker-differential-expression.R
+++ code/11_bulk-expression-analysis/06_bulk-marker-differential-expression.R
@@ -9,6 +9,14 @@ source("deconvo_method-paper/code/11_bulk-expression-analysis/00_parameters.R")
 sapply(libv, library, character.only = T)
 rse.markers <- get(load(rse.k2markers.filepath))
 
+# perform differential expression analysis
+se.markers <- SummarizedExperiment(rse.markers)
+cd <- colData(rse.markers)[,condition.variable,drop=F]
+names(assays(se.markers)) <- "counts"
+des.markers <- DESeqDataSet(se.markers, design = cd)
+count.data <- as.matrix(assays(rse.markers)[[1]])
+des.markers <- DESeqDataSetFromMatrix(countData = count.data, 
+                                      colData = cd, design = design.matrix)
 # pairwise marker degs
 experiment.group.variable <- "experiment.group"
 experiment.group.vector <- colData(rsef[,experiment.group.variable])
