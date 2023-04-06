862c2c980ced5ee4ceccd70044ec26c565916841
Sean Maden <metamaden@users.noreply.github.com>
2023-04-06 12:35:03 -0700

    update permutation experiment code
    uses new lute functions for permutation experiments

--- code/15_sample-wise-signature-matrix-simulations/09-04_k2-simulate-missmatched-signature-pb_mrb.R
+++ code/15_sample-wise-signature-matrix-simulations/09-04_k2-simulate-missmatched-signature-pb_mrb.R
@@ -35,77 +35,10 @@ deconvolution.algorithm <- "nnls"
 summary.method  <- "mean"
 
 experiment <- deconvolution.experiment.permute.groups(
+  sce = sce, s = s,
   sample.id.variable = sample.id.variable, 
-  celltype.variable = celltype.variable, assay.name = assay.name, s = s,
+  celltype.variable = celltype.variable, 
+  assay.name = assay.name,
   deconvolution.algorithm = deconvolution.algorithm, 
-  summary.method = summary.method, verbose = TRUE)
-
-deconvolution.experiment.permute.groups <- function(verbose = FALSE, ...){
-  if(verbose){message("running permutation experiments...")}
-  group.id.vector <- sce[[sample.id.variable]]
-  unique.group.id.vector <- group.id.vector %>% unique() %>% as.character()
-  results.table.list <- lapply(unique.group.id.vector, function(group.id.z){
-    if(verbose){message("working on group id ", group.id.z, "...")}
-    if(verbose){message("getting main signature matrix...")}
-    filter.sce <- sce[[sample.id.variable]]==group.id.z
-    sce.group.z <- sce[,filter.sce]
-    z <- signature_matrix_from_sce(sce.group.z,
-                                   celltype.variable = celltype.variable,
-                                   summary.method = summary.method,
-                                   assay.name = assay.name)
-    
-    if(verbose){message("getting pseudobulks...")}
-    pb.filter <- !unique.group.id.vector==group.id.z
-    unique.group.id.pb <- unique.group.id.vector[pb.filter] %>% unique()
-    ypb.list <- lapply(unique.group.id.pb, function(group.id){
-      if(verbose){
-        message("working on pseudobulk for sample id ", group.id, "...")}
-      filter.group <- sce[[sample.id.variable]]==group.id
-      ypb_from_sce(sce = sce[,filter.group], assay.name = assay.name, 
-                   celltype.variable = celltype.variable, S = s)
-    })
-    ypb.table <- do.call(cbind, ypb.list) %>% as.data.frame()
-    colnames(ypb.table) <- unique.group.id.pb
-    
-    if(verbose){message("getting experiment series...")}
-    experiment <- deconvolution.experiment(sce = sce, 
-                                        y = ypb.table, s = s, z = z, 
-                                        assay.name = assay.name, 
-                                        sample.id.variable = sample.id.variable, 
-                                        experiment.labels = group.id.z,
-                              deconvolution.algorithm = deconvolution.algorithm,
-                              celltype.variable = celltype.variable)
-    
-    if(verbose){message("finished with group id, returning results table...")}
-    results.table.iteration <- experiment$results.table
-    results.table.iteration$group.id.signature <- group.id.z
-    results.table.iteration
-  })
-  results.table <- do.call(rbind, results.table.list) %>% as.data.frame()
-  # make plots
-  plots.list <- deconvolution.results.plots.permutations(results.table)
-  return(
-    list(
-      results.table = results.table, plots.list = plots.list))
-}
-
-deconvolution.results.plots.permutations <- function(results.table, verbose = FALSE){
-  if(verbose){"Making results plots for permutation experiments..."}
-  filter.results <- results.table$sample.id==results.table$group.id.signature
-  filter.types.vector <- c("matched", "miss-matched", "all")
-  lgg.permute <- lapply(filter.types.vector, function(filter.type){
-    if(filter.type=="matched"){
-      filter.final <- filter.results
-    } else if(filter.type=="miss-matched"){
-      filter.final <- !filter.results
-    } else{
-      filter.final <- seq(nrow(results.table))
-    }
-    results.filtered <- results.table[filter.final,]
-    results.filtered$sample.id <- results.filtered$group.id.signature
-    lgg.filter <- deconvolution.results.plots(results.filtered)
-    return(deconvolution.results.plots(results.filtered))
-  })
-  names(lgg.permute) <- filter.types.vector
-  return(lgg.permute)
-}
+  summary.method = summary.method, 
+  verbose = TRUE)
