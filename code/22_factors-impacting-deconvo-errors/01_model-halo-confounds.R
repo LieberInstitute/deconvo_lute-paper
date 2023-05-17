ef33f3c3bcc96c6f5b9c83243ba515d5f5d1e000
Sean Maden <metamaden@users.noreply.github.com>
2023-05-17 08:29:29 -0700

    adds nucleus area ancovas

--- code/22_factors-impacting-deconvo-errors/01_model-halo-confounds.R
+++ code/22_factors-impacting-deconvo-errors/01_model-halo-confounds.R
@@ -17,8 +17,55 @@ sapply(libv, library, character.only = TRUE)
 save.path <- here("deconvo_method-paper", "outputs", "20_snrnaseq-bulk-matched_training")
 
 #--------------
-# get halo data
+# prepare halo data
 #--------------
 # image reference
 halo.path <- "Human_DLPFC_Deconvolution/processed-data/03_HALO/halo_all.Rdata"
 halo.all <- get(load(halo.path))
+
+# filter other category
+halo.all <- halo.all[!halo.all$cell_type == "Other",]
+
+#------------------
+# get ancova models
+#------------------
+model.data <- halo.all[,c("Nucleus_Area", "cell_type", "BrNum", "Position", "Round", "Combo")]
+ancova1 <- anova(lm(Nucleus_Area ~. , data = model.data))
+
+# subsample on area
+summary(halo.all$Nucleus_Area)
+# Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
+# 5.006  17.800  29.913  32.750  42.274 199.999
+
+fract.res <- function(anova.result, res.index = 5){
+  anova.result$`Sum Sq`[res.index]/sum(anova.result$`Sum Sq`)}
+
+model.filter1 <- model.data[model.data$Nucleus_Area <= 30,]
+ancova.filter1 <- anova(lm(Nucleus_Area ~. , data = model.filter1))
+res.filter1 <- fract.res(ancova.filter1)
+
+model.filter2 <- model.data[model.data$Nucleus_Area <= 50,]
+ancova.filter2 <- anova(lm(Nucleus_Area ~. , data = model.filter1))
+res.filter2 <- fract.res(ancova.filter2)
+
+model.filter3 <- model.data[model.data$Nucleus_Area <= 50 & model.data$Nucleus_Area >= 30,]
+ancova.filter3 <- anova(lm(Nucleus_Area ~. , data = model.filter3))
+res.filter3 <- fract.res(ancova.filter3)
+
+model.filter4 <- model.data[model.data$Nucleus_Area <= 100 & model.data$Nucleus_Area >= 50,]
+ancova.filter4 <- anova(lm(Nucleus_Area ~. , data = model.filter4))
+res.filter4 <- fract.res(ancova.filter4)
+
+#-----------------------------
+# sample ids with fewest cells
+#-----------------------------
+# fewest total cells
+
+# fewest neurons
+
+# fewest glial
+
+#----------------------------------------------
+# subsample cells of varying amounts, by sample
+#----------------------------------------------
+
