ba8c5b66a83dcf0b8d54ef2350a019443c699254
Sean Maden <metamaden@users.noreply.github.com>
2023-05-22 10:29:10 -0700

    Update 04__multiassay-analysis-potential-confounds.R

--- code/22_factors-impacting-deconvo-errors/04__multiassay-analysis-potential-confounds.R
+++ code/22_factors-impacting-deconvo-errors/04__multiassay-analysis-potential-confounds.R
@@ -6,6 +6,9 @@
 #
 #
 
+libv <- c("dplyr", "ggplot2")
+sapply(libv, library, character.only = T)
+
 #---------------------------------
 # prepare processed confounds data
 #---------------------------------
@@ -18,16 +21,21 @@ sn.confounds <- get(load(
   "./deconvo_method-paper/outputs/22_factors-impacting-deconvo-errors/df-confounds-sn_dlpfc-train.rda"))
 
 # load marker expression
-bulk.marker.expr <- get(load("list-donormarker-bulkexpr_dlpfc-train.rda"))
-sn.marker.expr <- get(load("list-donormarker-snexpr_dlpfc-train.rda"))
+#bulk.marker.expr <- get(load("list-donormarker-bulkexpr_dlpfc-train.rda"))
+#sn.marker.expr <- get(load("list-donormarker-snexpr_dlpfc-train.rda"))
 
-#-----------------------
-#
-#-----------------------
+#---------------------
+# harmonize assay data
+#---------------------
+library(dplyr)
 
+sample.id.varname <- "sample.id"
 df.bulk <- cbind(bulk.confounds$total.counts, bulk.confounds$expressed.genes)
-df.bulk$sample.id.sn <- paste0(unlist(strsplit(df.bulk$sample.id, "_"))[2], "_",
-                               tolower(unlist(strsplit(df.bulk$sample.id, "_"))[3]))
+
+df.bulk[,sample.id.varname] <- paste0(
+  unlist(strsplit(df.bulk$sample.id, "_"))[2], "_",
+  tolower(unlist(strsplit(df.bulk$sample.id, "_"))[3]))
+
 for(sample.id in rownames(df.sn.confound)){
   for(cn in colnames(df.sn.confound)){
     filter.df.sn <- df.sn.confound$sample.id==sample.id
@@ -55,19 +63,33 @@ for(sample.id in df.halo.agg$sample.id){
   }
 }
 
-# save
+# format integrated data
 df.integrate <- df.bulk
+df.integrate$cell.compartment <- gsub(".*_", "", df.integrate$sample.id.1)
+df.integrate$library.prep <- gsub("_.*", "", df.integrate$sample.id.1)
+
+# save
 df.integrate.path <- paste0("./deconvo_method-paper/outputs/",
                             "22_factors-impacting-deconvo-errors/",
                             "df_integrate.rda")
 save(df.integrate, file = df.integrate.path)
 
-#--------------------
-# 
-#--------------------
+#----------------------
+# cross-assay summaries
+#----------------------
 df.integrate.path <- paste0("./deconvo_method-paper/outputs/",
                             "22_factors-impacting-deconvo-errors/",
                             "df_integrate.rda")
 df.integrate <- get(load(df.integrate.path))
+
+# plot summaries
+
+ggplot(df.integrate, aes(x = sample.id, y = expressed.genes)) + 
+  geom_violin(draw_quantiles = 0.5)
+
+ggplot(df.integrate, aes(x = halo.total.glial, y = expressed.genes)) + 
+  geom_point(alpha = 0.5) + geom_abline(slope = 1, intercept = 0)
+
+
 
 
