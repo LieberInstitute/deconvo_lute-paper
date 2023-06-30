548335d7b19fb7a43c99b1542e9adcd55952281c
Sean Maden <metamaden@users.noreply.github.com>
2023-06-30 11:31:58 -0700

    Update lute-paper_sosina2021-fig2b.R

--- code/23_replicate-sosina2021/lute-paper_sosina2021-fig2b.R
+++ code/23_replicate-sosina2021/lute-paper_sosina2021-fig2b.R
@@ -2,10 +2,9 @@
 
 # Author: Sean Maden
 #
-# Code to replicate Figure 2a from Sosina et al 2021 (DOI:10.12688/f1000research.50858.1)
+# Code to replicate Figure 2b from Sosina et al 2021 (DOI:10.12688/f1000research.50858.1)
 #
 #
-
 
 scripts.path <- "scripts"
 libv <- c("here", "SingleCellExperiment", "data.table", "dplyr", "lute", "ggplot2", "MuSiC")
@@ -72,11 +71,23 @@ y25 <- rse.bulk[top25.yz,]
 z50 <- zref[top50.yz,]
 z25 <- zref[top25.yz,]
 
-#------------------------
-# load cell scale factors
-#------------------------
+#------------------
+# cell size factors
+#------------------
+# source(file.path(scripts.path, "chunk2_prepare-cellsize-osm.R"))
+# scripts.path <- "scripts"
+# source(file.path(scripts.path, "chunk_prepare-cellsizes-osm.R"))
 
+cell_size_osm_fish_cellarea <- c("90.87", "122.96")
+cell_size_osm_fish_nrna_intensity <- c("180.46", "198.86")
 
+# make dataframes
+osm.nrna <- data.frame(celltypes = c(FALSE, TRUE), 
+                       cellsizes = as.numeric(cell_size_osm_fish_nrna_intensity))
+osm.cellarea <- data.frame(celltypes = c(FALSE, TRUE), 
+                           cellsizes = as.numeric(cell_size_osm_fish_cellarea))
+
+# source(file.path(scripts.path,"chunk_predict-plot-music-cellsize-osm.R"))
 #------------------
 # music predictions
 #------------------
@@ -87,27 +98,41 @@ sc.sce.music$Neuron <- ifelse(sc.sce.music$nucleusCellType == "Neuron",
 sc.sce.music$Sample <- gsub("\\..*", "", sc.sce.music$V1)
 
 # predict
-music_est <- music_prop(bulk.mtx = assays(y50)[["counts"]], 
-                        sc.sce = sc.sce.music, 
-                        clusters = "Neuron",
-                        samples = "Sample")
+music_est_nrna <- music_prop(bulk.mtx = assays(y50)[["counts"]], 
+                             sc.sce = sc.sce.music, 
+                             clusters = "Neuron",
+                             samples = "Sample",
+                             cell_size = osm.nrna)
+
+music_est_cellarea <- music_prop(bulk.mtx = assays(y50)[["counts"]], 
+                                 sc.sce = sc.sce.music, 
+                                 clusters = "Neuron",
+                                 samples = "Sample",
+                                 cell_size = osm.cellarea)
 
 # plot
-pred <- music_est$Est.prop.weighted  %>% as.data.frame()
-identical(rownames(pred), prop$samples)
-prop$music <- pred[,"TRUE"]
-dfp <- data.frame(true = prop[,2], music = pred$`TRUE`)
+pred.nrna <- music_est_nrna$Est.prop.weighted  %>% as.data.frame()
+pred.cellarea <- music_est_cellarea$Est.prop.weighted  %>% as.data.frame()
 
-ggpt.fig2a <- ggplot(dfp, aes(x = music, y = true)) + 
-  geom_point(alpha = 1, shape = "square", color = "forestgreen", size = 3) +
+identical(rownames(pred.nrna), prop$samples)
+identical(rownames(pred.cellarea), prop$samples)
+prop$music.osm.nrna <- pred.nrna[,"TRUE"]
+prop$music.osm.cellarea <- pred.cellarea[,"TRUE"]
+
+dfp <- data.frame(true = prop[,2], 
+                  music.osm.nrna = pred.nrna$`TRUE`, 
+                  music.osm.cellarea = pred.cellarea$`TRUE`)
+
+ggpt.fig2b.osm.nrna <- 
+  ggplot(dfp, aes(x = music.osm.nrna, y = true)) + 
+  geom_point(alpha = 1, col = "springgreen4", shape = 15) +
   geom_abline(slope = 1, intercept = 0) +
   xlim(0, 0.5) + ylim(0, 0.4) + theme_classic()
 
-jpeg("lute-replicate-sosina2021-fig2a.jpg", width = 5, height = 5, units = "in", res = 400)
-ggpt.fig2a
-dev.off()
+ggpt.fig2b.osm.cellarea <- 
+  ggplot(dfp, aes(x = music.osm.cellarea, y = true)) + 
+  geom_point(alpha = 1, col = "springgreen4", shape = 15) +
+  geom_abline(slope = 1, intercept = 0) +
+  xlim(0, 0.5) + ylim(0, 0.4) + theme_classic()
 
-#-----------------
-# save environment
-#-----------------
 save.image(file = file.path(env.path, "env_predict-music.RData"))
