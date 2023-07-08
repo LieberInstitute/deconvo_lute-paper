06172c6d1908072a7e3aea429bcb3bd5046c0927
Sean Maden <metamaden@users.noreply.github.com>
2023-07-08 13:51:52 -0700

    begins k3 scripts

---
+++ code/02_pseudobulk-predictions/01_only-k3_lute-pseudobulk-experiment_mrb-dlpfc-cohort2.R
@@ -0,0 +1,44 @@
+#!/usr/bin/env R
+
+#
+# Test independent pseudobulk from DLPFC cohort1 snRNAseq data.
+#
+
+source("deconvo_method-paper/code/02_pseudobulk-predictions/00_parameters-pseudobulk.R")
+sapply(libv, library, character.only = T)
+list.sce.markers <- get(load(sce.markers.list.path))
+sce <- list.sce.markers$k3
+
+# get experiment results tables
+dfp.tall <- get_ypb_experiment_series(sce, sample.id.variable = "Sample", 
+                                      celltype.variable = "k3", assay.name = "logcounts",
+                                      s.vector = c("glial" = 3, "Excit" = 10, "Inhib" ),
+                                      algorithm.name = "nnls", return.dimensions = "tall")
+
+dfp.wide <- get_ypb_experiment_series(sce, sample.id.variable = "Sample", 
+                                      celltype.variable = "k3", assay.name = "logcounts",
+                                      s.vector = c("glial" = 3, "neuron" = 10),
+                                      algorithm.name = "nnls", return.dimensions = "wide")
+
+# make new plots
+# plot proportions panel -- no scale
+ggplot(dfp.tall[dfp.tall$type=="noscale",], 
+       aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
+  xlab("True proportion") + ylab("Predicted proportion") +
+  xlim(0, 1) + ylim(0, 1) + ggtitle("Neuron")
+
+# plot proportions multipanel -- scale vs with scale
+ggplot(dfp.tall, aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
+  xlab("True proportion") + ylab("Predicted proportion") +
+  xlim(0, 1) + ylim(0, 1) + facet_wrap(~type) + ggtitle("Neuron")
+
+# plot jitterbox absolute errors -- scale vs with scale
+dfp.ae <- data.frame(abs.error.neuron.noscale = abs(dfp.wide$neuron.pred.noscale-dfp.wide$neuron.true.withscale),
+                     abs.error.neuron.scale = abs(dfp.wide$neuron.pred.withscale-dfp.wide$neuron.true.withscale))
+ggplot(dfp.tall, aes(x = type, y = abs.error.neuron)) + geom_jitter(alpha = 0.5) + 
+  geom_boxplot(draw_quantiles = 0.5, alpha = 0, color = "cyan") + theme_bw() + facet_wrap(~type)
+
