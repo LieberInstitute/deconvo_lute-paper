8199839655d4d2b3cdc7a2cf906568e144b1660c
Sean Maden <metamaden@users.noreply.github.com>
2023-07-08 13:46:09 -0700

    begins adding jitterbox error function

--- code/02_pseudobulk-predictions/01-01_only-k2_lute-pseudobulk-experiment_dlpfc-cohort1.R
+++ code/02_pseudobulk-predictions/01-01_only-k2_lute-pseudobulk-experiment_dlpfc-cohort1.R
@@ -21,8 +21,25 @@ dfp.wide <- get_ypb_experiment_series(sce.k2, sample.id.variable = "Sample",
                                       algorithm.name = "nnls", return.dimensions = "wide")
 
 # make new plots
-ggplot(dfp.k2[,grepl("noscale", colnames(dfp.k2))], aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+
+# plot proportions panel -- no scale
+ggplot(dfp.tall[dfp.tall$type=="noscale",], 
+       aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
   geom_abline(slope = 1, intercept = 0) + 
   geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
   xlab("True proportion") + ylab("Predicted proportion") +
-  xlim(0, 1) + ylim(0, 1)
+  xlim(0, 1) + ylim(0, 1) + ggtitle("Neuron")
+
+# plot proportions multipanel -- scale vs with scale
+ggplot(dfp.tall, aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
+  xlab("True proportion") + ylab("Predicted proportion") +
+  xlim(0, 1) + ylim(0, 1) + facet_wrap(~type) + ggtitle("Neuron")
+
+# plot jitterbox absolute errors -- scale vs with scale
+dfp.ae <- data.frame(abs.error.neuron.noscale = abs(dfp.wide$neuron.pred.noscale-dfp.wide$neuron.true.withscale),
+                     abs.error.neuron.scale = abs(dfp.wide$neuron.pred.withscale-dfp.wide$neuron.true.withscale))
+ggplot(dfp.tall, aes(x = type, y = abs.error.neuron)) + geom_jitter(alpha = 0.5) + 
+  geom_boxplot(draw_quantiles = 0.5, alpha = 0, color = "cyan") + theme_bw() + facet_wrap(~type)
+
