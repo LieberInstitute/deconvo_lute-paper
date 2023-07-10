ec508ee3c7b1ebcd37da4a9202aca3e00b0ce001
Sean Maden <metamaden@users.noreply.github.com>
2023-07-10 11:23:49 -0700

    adds paths to processed datasets for load into mae script

--- code/01_prepare-datasets/05_prepare-mae-integrated_dlpfc-cohort1.R
+++ code/01_prepare-datasets/05_prepare-mae-integrated_dlpfc-cohort1.R
@@ -11,3 +11,24 @@
 libv <- c("here", "nlme", "ggplot2", "gridExtra", "dplyr", "ggforce", "MultiAssayExperiment")
 sapply(libv, library, character.only = TRUE)
 
+#--------------------
+# load prepped assays
+#--------------------
+# load snrnaseq data
+list.snrnaseq.sce <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/list-scef_markers-k2-k3-k4_ro1-dlpfc.rda")
+# load bulk data
+rnascope.df <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/rse_k2-marker-expression_ro1-dlpfc.rda")
+# load rnascope image data
+bulk.rse <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/halo-outputs_updated.Rdata")
+
+#---------
+# make mae
+#---------
+sample.id.snrnaseq <- "Sample"
+sample.id.halo <- "Sample"
+
+# make mae on common indices
+
+
+
+
