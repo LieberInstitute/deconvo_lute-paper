652c0b9cb63bb663bc1cdc575e023db1e2a23d82
Sean Maden <metamaden@users.noreply.github.com>
2023-07-10 09:49:00 -0700

    Create 05_prepare-mae-integrated_dlpfc-cohort1.R

---
+++ code/01_prepare-datasets/05_prepare-mae-integrated_dlpfc-cohort1.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env R
+
+#
+# Makes a multi assay experiment object containing the data for integrated analysis. 
+# The new MAE object includes the following asssays:
+# * snRNAseq
+# * bulk RNAseq
+# * RNAscope image processing outputs from HALO
+#
