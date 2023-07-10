1e5d90dea5ef51e2962a11b993cb54408e7725a8
Sean Maden <metamaden@users.noreply.github.com>
2023-07-10 15:04:49 -0700

    update mae objects, adds map list to harmonize coldata
    begin following mae vignette instructions to map metadata between assays

--- code/01_prepare-datasets/05_prepare-mae-integrated_dlpfc-cohort1.R
+++ code/01_prepare-datasets/05_prepare-mae-integrated_dlpfc-cohort1.R
@@ -15,11 +15,12 @@ sapply(libv, library, character.only = TRUE)
 # load prepped assays
 #--------------------
 # load snrnaseq data
-list.snrnaseq.sce <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/list-scef_markers-k2-k3-k4_ro1-dlpfc.rda")
+load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/list-scef_markers-k2-k3-k4_ro1-dlpfc.rda")
+sce <- lscef[[1]]
 # load bulk data
-rnascope.df <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/rse_k2-marker-expression_ro1-dlpfc.rda")
+load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/rse-gene-filter.rda")
 # load rnascope image data
-bulk.rse <- load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/halo-outputs_updated.Rdata")
+load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/halo-outputs_updated.Rdata")
 
 #---------
 # make mae
@@ -29,7 +30,18 @@ sample.id.halo <- "Sample"
 sample.id.bulk <- "batch.id2"
 
 # make mae on common indices
+# make maplist
+sn.map <- data.frame(coldata = colnames(sce),
+                     sample.id = sce[[sample.id.snrnaseq]])
+bulk.map <- data.frame(coldata = colnames(rse.filter),
+                       sample.id = rse.filter[[sample.id.bulk]])
+image.map <- data.frame(coldata = halo.output.table[,sample.id.halo],
+                        sample.id = halo.output.table[,sample.id.halo])
+listmap <- list(sn.rnaseq = sn.map,
+                bulk.rnaseq = bulk.map,
+                rnascope.image = image.map)
 
-
+# make new sampleMap object
+listToMap(maplist)
 
 
