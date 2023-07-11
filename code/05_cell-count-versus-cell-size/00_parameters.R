54ccd80ae6c732bce1a4453b0a834edba0980d2f
Sean Maden <metamaden@users.noreply.github.com>
2023-07-11 10:55:07 -0700

    completes 1to1 harmonization function
    moves helper functions to parameters script
    sets harmonization code for aggregation

--- code/05_cell-count-versus-cell-size/00_parameters.R
+++ code/05_cell-count-versus-cell-size/00_parameters.R
@@ -10,3 +10,30 @@ save.path <- here("deconvo_method-paper", "outputs", "05_cell-count-versus-cell-
 # multi assay experiment path
 mae.filename <- "mae_final.rda"
 mae.path <- here("deconvo_method-paper", "outputs", "01_prepare-datasets", mae.filename)
+
+# helper functions
+append_k_columns <- function(df.input, df.ct = NULL, celltype.variable = "cell_type"){
+  df.ct <- data.frame(cell_type = c("Inhib", "Other", "Astro", "Endo", "Excit", "Oligo", "OPC", "Micro", "all"),
+                      k2 = c("neuron", "NA", "glial", "glial", "neuron", "glial", "glial", "glial", "NA"),
+                      k3 = c("Inhib", "NA", "glial", "glial", "Excit", "glial", "glial", "glial", "NA"),
+                      k4 = c("Inhib", "NA", "non_oligo_glial", "non_oligo_glial", "Excit", "Oligo", 
+                             "non_oligo_glial", "non_oligo_glial", "NA"))
+  celltype.vector <- df.input[,celltype.variable]
+  unique.cell.types <- unique(celltype.vector)
+  df.ct.filter <- df.ct[df.ct$cell_type %in% unique.cell.types,]
+  df.ct.new <- do.call(rbind, lapply(celltype.vector, function(cell.type.id){
+    df.ct.filter[df.ct.filter[,"cell_type"]==cell.type.id,,drop = F]
+  }))
+  cbind(df.input, df.ct.new)
+}
+
+# cell type harmonization table
+harmonize_celltype_tables_1to1 <- function(df.input1, df.input2, celltype.variable = "cell_type"){
+  # harmonize when there is one unique celltype level per row in each input
+  celltype.vector1 <- df.input1[,celltype.variable]
+  unique.cell.types1 <- unique(celltype.vector1)
+  do.call(rbind, lapply(unique.cell.types1, function(cell.type.id){
+    c(df.input1[df.input1[,celltype.variable]==cell.type.id,], 
+      df.input2[df.input2[,celltype.variable]==cell.type.id,])
+  })) %>% as.data.frame()
+}
--- code/05_cell-count-versus-cell-size/01_cell-sizes-versus-proportions_dlpfc-cohort1.R
+++ code/05_cell-count-versus-cell-size/01_cell-sizes-versus-proportions_dlpfc-cohort1.R
@@ -17,23 +17,6 @@ mae <- get(load(mae.filepath))
 mae.cd <- colData(mae.final)
 sample.id.vector <- mae.cd$sample.id[complete.cases(mae.final)]
 
-# cell type harmonization table
-df.ct <- data.frame(cell_type = c("Inhib", "Other", "Astro", "Endo", "Excit", "Oligo", "OPC", "Micro"),
-                    k2 = c("neuron", "NA", "glial", "glial", "neuron", "glial", "glial", "glial"),
-                    k3 = c("Inhib", "NA", "glial", "glial", "Excit", "glial", "glial", "glial"),
-                    k4 = c("Inhib", "NA", "non_oligo_glial", "non_oligo_glial", "Excit", "Oligo", 
-                           "non_oligo_glial", "non_oligo_glial"))
-
-append_k_columns <- function(df.input, df.ct, celltype.variable = "cell_type"){
-  celltype.vector <- df.input[,celltype.variable]
-  unique.cell.types <- unique(celltype.vector)
-  df.ct.filter <- df.ct[df.ct$cell_type %in% unique.cell.types,]
-  df.ct.new <- do.call(rbind, lapply(celltype.vector, function(cell.type.id){
-    df.ct.filter[df.ct.filter[,"cell_type"]==cell.type.id,,drop = F]
-  }))
-  cbind(df.input, df.ct.new)
-}
-
 # get cell size table
 df.cellsize <- do.call(rbind, lapply(sample.id.vector, function(sample.id){
   mae.iter <- mae[,mae.cd$sample.id==sample.id,]
@@ -41,24 +24,23 @@ df.cellsize <- do.call(rbind, lapply(sample.id.vector, function(sample.id){
   # gets cell size
   img.data <- mae.iter[[3]] %>% assays() %>% unlist() %>% t() %>% as.data.frame()
   img.data$cell_type <- mae.iter[[3]]$cell_type
-  df.iter <- aggregate(img.data[,"Nucleus_Area"], list("cell_type" = img.data$cell_type), FUN = "median")
-  colnames(df.iter)[2] <- "median_nucleus_area"
-  #df.iter$sample.id <- sample.id
+  df.img <- aggregate(img.data[,"Nucleus_Area"], list("cell_type" = img.data$cell_type), FUN = "median")
+  colnames(df.img)[2] <- "median_nucleus_area"
   
   # gets cell amount by type, from sce
   stat <- sumCountsAcrossCells(mae.iter[[2]], ids = colData(mae.iter[[2]])[,"cellType_broad_hc"])
   stat.med <- colMedians(assays(stat)[["sum"]])
-  df.stat <- data.frame(median_counts_snrnaseq = stat.med, cell_type = colnames(stat))
+  df.sn <- data.frame(median_counts_snrnaseq = stat.med, cell_type = colnames(stat))
   # gets total cell amount by type, from sce
   stat2 <- sumCountsAcrossCells(mae.iter[[2]], ids = rep("all", ncol(mae.iter[[2]])))
-  stat.med2 <- colMedians(assays(stat)[["sum"]])
-  df.stat.all <- data.frame(median_counts_snrnaseq = stat.med2, cell_type = colnames(stat2))
-  df.stat.final <- rbind(df.stat, df.stat.all)
-  #df.stat.final$sample.id <- sample.id
-  
-  df.return <- cbind(df.stat.final, df.iter)
-  df.return$sample.id <- sample.id
-  df.return
+  stat.med2 <- colMedians(assays(stat2)[["sum"]])
+  df.sn.all <- data.frame(median_counts_snrnaseq = stat.med2, cell_type = colnames(stat2))
+  df.sn.final <- rbind(df.sn, df.sn.all)
+ 
+  # harmonized image and sn data
+  df.harmonized <- harmonize_celltype_tables_1to1(df.sn.final, df.img)
+  df.harmonized <- append_k_columns(df.harmonized)
+  df.harmonized
 }))
 
 
