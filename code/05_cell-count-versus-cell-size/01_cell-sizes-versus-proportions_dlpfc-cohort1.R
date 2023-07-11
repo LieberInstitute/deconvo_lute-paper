ac09ca80782206c74681f4e363752aa8aacebe50
Sean Maden <metamaden@users.noreply.github.com>
2023-07-11 09:55:26 -0700

    gets median nucleus area

--- code/05_cell-count-versus-cell-size/01_cell-sizes-versus-proportions_dlpfc-cohort1.R
+++ code/05_cell-count-versus-cell-size/01_cell-sizes-versus-proportions_dlpfc-cohort1.R
@@ -1,3 +1,4 @@
+#!/usr/bin/env R
 
 #
 # Testing relationship between cell sizes and total cells.
@@ -16,10 +17,15 @@ mae <- get(load(mae.filepath))
 mae.cd <- colData(mae.final)
 sample.id.vector <- mae.cd$sample.id[complete.cases(mae.final)]
 
-# get cell size tables
+# get cell size table
 df.cellsize <- do.call(rbind, lapply(sample.id.vector, function(sample.id){
   mae.iter <- mae[,mae.cd$sample.id==sample.id,]
-  table(mae.iter[3])
+  img.data <- mae.iter[[3]] %>% assays() %>% unlist() %>% t() %>% as.data.frame()
+  img.data$cell_type <- mae.iter[[3]]$cell_type
+  df.iter <- aggregate(img.data[,"Nucleus_Area"], list("cell_type" = img.data$cell_type), FUN = "median")
+  colnames(df.iter)[2] <- "median_nucleus_area"
+  df.iter$sample.id <- sample.id
+  df.iter
 }))
 
 
