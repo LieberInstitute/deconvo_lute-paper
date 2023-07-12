143d146c38436188895e7cbbf04937478c3fe9fc
Sean Maden <metamaden@users.noreply.github.com>
2023-07-12 11:58:31 -0700

    adds multipanel scatterplots, begins cohort2 script

---
+++ code/02_pseudobulk-predictions/01_only-k2_algo-music_lute-pseudobulk-experiment_dlpfc-cohort1.R
@@ -0,0 +1,43 @@
+#!/usr/bin/env R
+
+#
+# Test independent pseudobulk from DLPFC cohort1 snRNAseq data.
+#
+
+source("deconvo_method-paper/code/02_pseudobulk-predictions/00_parameters-pseudobulk.R")
+sapply(libv, library, character.only = T)
+list.sce.markers <- get(load(sce.markers.list.path))
+sce.k2 <- list.sce.markers$k2
+
+# get experiment results tables
+dfp.tall <- get_ypb_experiment_series(sce.k2, sample.id.variable = "Sample", 
+                                 celltype.variable = "k2", assay.name = "logcounts",
+                                 s.vector = c("glial" = 3, "neuron" = 10),
+                                 algorithm.name = "nnls", return.dimensions = "tall",
+                                 deconvolution.algorithm = "bisque")
+
+dfp.wide <- get_ypb_experiment_series(sce.k2, sample.id.variable = "Sample", 
+                                      celltype.variable = "k2", assay.name = "logcounts",
+                                      s.vector = c("glial" = 3, "neuron" = 10),
+                                      algorithm.name = "nnls", return.dimensions = "wide",
+                                      deconvolution.algorithm = "bisque")
+
+# make new plots
+# plot proportions panel -- no scale
+ggplot(dfp.tall[dfp.tall$type=="noscale",], 
+       aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
+  xlab("True proportion") + ylab("Predicted proportion") +
+  xlim(0, 1) + ylim(0, 1) + ggtitle("Neuron")
+
+# plot proportions multipanel -- scale vs with scale
+ggplot(dfp.tall, aes(x = neuron.true, y = neuron.pred)) + geom_point() + 
+  geom_abline(slope = 1, intercept = 0) + 
+  geom_hline(yintercept = 0.5) + geom_vline(xintercept = 0.5) + theme_bw() +
+  xlab("True proportion") + ylab("Predicted proportion") +
+  xlim(0, 1) + ylim(0, 1) + facet_wrap(~type) + ggtitle("Neuron")
+
+# jitterbox -- jittered points and boxplots of absolute errors
+ggplot(dfp.tall, aes(x = type, y = neuron.abs.error)) + geom_jitter(alpha = 0.5) + 
+  geom_boxplot(color = "cyan", alpha = 0) + theme_bw() + ggtitle("Neuron")
