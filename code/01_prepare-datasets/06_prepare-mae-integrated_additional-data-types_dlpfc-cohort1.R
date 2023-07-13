dcbeb7dae2f8ded7e887b072859af1800ecbbb7b
Sean Maden <metamaden@users.noreply.github.com>
2023-07-13 09:47:43 -0700

    updates mae with rnascope klabel data, begins debugging df add

--- code/01_prepare-datasets/06_prepare-mae-integrated_additional-data-types_dlpfc-cohort1.R
+++ code/01_prepare-datasets/06_prepare-mae-integrated_additional-data-types_dlpfc-cohort1.R
@@ -21,21 +21,29 @@ load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/rse-gene-filter.
 # load rnascope image data
 load("~/GitHub/deconvo_method-paper/outputs/01_prepare-datasets/halo-outputs_updated.Rdata")
 
-#------------------
-# prep data for mae
-#------------------
 # common sample ids
 sample.id.snrnaseq <- "Sample"
 sample.id.halo <- "Sample"
 sample.id.bulk <- "batch.id2"
 
-# isolate snrnaseq sce data
+#------------------------------------
+# snrnaseq: isolate snrnaseq sce data
+#------------------------------------
 sce1 <- lscef[[1]]
 sce2 <- lscef[[2]]
 sce3 <- lscef[[3]]
 rm(lscef)
 
-# make sce with image data
+#--------------------------------------------
+# bulk: isolate bulk rnaseq marker expression
+#--------------------------------------------
+marker.id.vector <- unique(c(rownames(sce1), rownames(sce2), rownames(sce3)))
+rownames(rse.filter) <- rowData(rse.filter)$Symbol
+rse.filter <- rse.filter[rownames(rse.filter) %in% marker.id.vector,]
+
+#-----------------------------------
+# rnascope: make sce with image data
+#-----------------------------------
 img <- halo.output.table
 new.img.colnames <- paste0("cell", seq(nrow(img)))
 img.data.colnames <- c("Nucleus_Area", "AKT3_Copies", "Cell_Area", 
@@ -53,47 +61,116 @@ img.coldata <- DataFrame(img[,img.coldata.colnames])
 rownames(img.coldata) <- new.img.colnames
 colData(sce.img) <- img.coldata
 rm(halo.output.table)
-
 gc()
 
-#---------
-# make mae
-#---------
-# indicies for subsetting
-bulk.index <- seq(ncol(rse.filter))
-sn.index <- seq(ncol(sce1))
-img.index <- seq(ncol(sce.img))
+#--------------------------------------------------
+# df.rn: get additional rnascope data.frame objects
+#--------------------------------------------------
+# update sce.img with klabel categories
+# get true proportions from rnascope data
+rnascope <- sce.img
+cd <- colData(rnascope)
+cd$k2 <- cd$k3 <- cd$k4 <- "NA"
+# append k label categories
+cd$k2 <- ifelse(grepl("Excit|Inhib", cd$cell_type), "neuron",
+                ifelse(grepl("Endo|Oligo|Micro", cd$cell_type), "glial", "NA"))
+cd$k3 <- ifelse(grepl("Excit", cd$cell_type), "Excit",
+                ifelse(grepl("Inhib", cd$cell_type), "Inhib", 
+                       ifelse(grepl("Endo|Oligo|Micro", cd$cell_type), "glial", "NA")))
+cd$k4 <- ifelse(grepl("Excit", cd$cell_type), "Excit",
+                ifelse(grepl("Inhib", cd$cell_type), "Inhib", 
+                       ifelse(grepl("Oligo", cd$cell_type), "Oligo", 
+                              ifelse(grepl("Micro|Endo", cd$cell_type), "non_oligo_glial", "NA"))))
+# reassign coldata to sce.img
+colData(sce.img) <- cd
 
-# mae prep
+# get data.frames of cell sizes, counts, proportions by klabel category
+sample.id.vector <- unique(rnascope$Sample)
+# filter na values
+rnascope <- sce.img[,!sce.img$k2=="NA"]
+# get all kdata
+df.rnascope.kdata <- do.call(rbind, lapply(c("k2", "k3", "k4"), function(cell.type.variable){
+  do.call(rbind, lapply(sample.id.vector, function(sample.id){
+    rnf <- rnascope[,rnascope$Sample==sample.id]
+    # proportions
+    df.prop <- table(rnf[[cell.type.variable]], rnf$Sample) %>% prop.table()
+    # counts
+    df.count <- table(rnf[[cell.type.variable]], rnf$Sample)
+    # sizes
+    df.size <- aggregate(data.frame(area = assays(rnf)[["Nucleus_Area"]][1,]), 
+                         by = list(cell_type = rnf[[cell.type.variable]]), FUN = "median")
+    # combine and format output
+    df.iter <- cbind(df.prop, cbind(df.size, df.count))
+    df.iter <- df.iter[,c(1,2,3,5,8)]
+    colnames(df.iter) <- c("cell_type", "sample_id", "true_proportion", "cell_size", "cell_count")
+    df.iter$k.label <- cell.type.variable
+    df.iter
+  }))
+}))
+rownames(df.rnascope.kdata) <- paste0(df.rnascope.kdata$sample_id,";",
+                                      df.rnascope.kdata$cell_type, ";",
+                                      df.rnascope.kdata$k.label)
+
+
+#-------------
+# mae: coldata
+#-------------
 # coldata
-coldata <- DataFrame(data.frame(sample.id = unique(c(sn.sce.subset[[sample.id.snrnaseq]],
-                                                     bulk.rse.subset[[sample.id.bulk]],
-                                                     img.sce.subset[[sample.id.halo]]))))
-# get subsets
-bulk.rse.subset <- rse.filter[,bulk.index]
-sn.sce.subset <- sce1[,sn.index]
-img.sce.subset <- sce.img[,img.index]
+unique.sample.id.vector <- unique(
+  c(sce1[[sample.id.snrnaseq]], 
+    rse.filter[[sample.id.bulk]],
+    sce.img[[sample.id.halo]])
+)
+coldata <- DataFrame(
+  data.frame(
+    sample.id = unique.sample.id.vector))
+
+#------------------------
+# mae: make data mappings
+#------------------------
 # make maplist
-sn1.map <- data.frame(colname = colnames(sn.sce.subset), 
-                      primary = sn.sce.subset[[sample.id.snrnaseq]])
-bulk.map <- data.frame(colname = colnames(bulk.rse.subset), 
-                       primary = bulk.rse.subset[[sample.id.bulk]])
-image.map <- data.frame(colname = colnames(img.sce.subset), 
-                        primary = img.sce.subset[[sample.id.halo]])
-listmap <- list(sn1.rnaseq = sn1.map, 
-                bulk.rnaseq = bulk.map, 
-                rnascope.image = image.map)
+# snrnaseq
+sn1.map <- data.frame(colname = colnames(sce1), primary = sce1[[sample.id.snrnaseq]])
+sn2.map <- data.frame(colname = colnames(sce2), primary = sce2[[sample.id.snrnaseq]])
+sn3.map <- data.frame(colname = colnames(sce3), primary = sce3[[sample.id.snrnaseq]])
+# bulk
+bulk.map <- data.frame(colname = colnames(rse.filter), primary = rse.filter[[sample.id.bulk]])
+# rnascope data
+image.map <- data.frame(colname = colnames(sce.img), primary = sce.img[[sample.id.halo]])
+dfrn.map <- data.frame(colname = rownames(df.rnascope.kdata), primary = df.rnascope.kdata$sample_id)
+
+# make mappings list, then map df
+listmap <- list(sn1.rnaseq = sn1.map, # snrnaseq
+                sn2.rnaseq = sn2.map, 
+                sn3.rnaseq = sn3.map,
+                bulk.rnaseq = bulk.map, # bulk
+                rnascope.image = image.map, # rnascope
+                df.cellstat.rnascope = dfrn.map)
 dfmap <- listToMap(listmap) # make new sampleMap object
 rownames(dfmap) <- dfmap$primary
-object.list2 <- list(bulk.rnaseq = bulk.rse.subset, 
-                     sn1.rnaseq = sn.sce.subset, 
-                     rnascope.image = img.sce.subset)   
-experiment.list <- ExperimentList(object.list2)
+
+#---------------------
+# mae: get object list
+#---------------------
+object.list <- list(
+  sn1.rnaseq = sce1, 
+  sn2.rnaseq = sce2, 
+  sn3.rnaseq = sce3, 
+  bulk.rnaseq = rse.filter,
+  rnascope.image = sce.img,
+  df.cellstat.rnascope = df.rnascope.kdata)   
+experiment.list <- ExperimentList(object.list)
 rownames(coldata) <- coldata$sample.id
+
+#------------------------------
+# mae: prepare new mae datasets
+#------------------------------
 mae <- prepMultiAssay(ExperimentList = experiment.list, sampleMap = dfmap, colData = coldata)
 mae.final <- MultiAssayExperiment(mae$experiments, mae$colData, mae$sampleMap)
 
-# save
+#----------
+# mae: save
+#----------
 mae.final.filepath <- here("deconvo_method-paper", "outputs", "01_prepare-datasets", "mae_additional-data_final.rda")
 save(mae.final, file = mae.final.filepath)
 
