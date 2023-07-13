225139a589980cfa43bdcbe7da4cdf5dd1ff59bc
Sean Maden <metamaden@users.noreply.github.com>
2023-07-13 08:27:22 -0700

    adds matched experiments
    k2 script now binds together trials with shared reference and trials using within sample references

--- code/03_matched-bulk-predictions/01_k2-only_mae-matched-experiments_dlpfc-cohort1.R
+++ code/03_matched-bulk-predictions/01_k2-only_mae-matched-experiments_dlpfc-cohort1.R
@@ -12,13 +12,15 @@ mae.filepath <- here("deconvo_method-paper", "outputs", "01_prepare-datasets", "
 mae <- get(load(mae.filepath))
 # unpack mae
 rse.all <- mae[[1]]
+rownames(rse.all) <- rowData(rse.all)$Symbol
 # snrnaseq reference -- using same reference across experiments
 sce.iter <- mae[[2]]
 sce.iter <- logNormCounts(sce.iter)
+# experiment variables
+assay.name <- "logcounts"
+deconvolution.algorithm <- "nnls"
 
-#--------------
-# k2 experiment
-#--------------
+# get cell sizes
 # get cell size factor series
 # load cell size scale factors
 df.csf <- get_csf_reference()
@@ -35,105 +37,87 @@ list.s.pred <- list(s.set1 = c("glial" = 3, "neuron" = 10),
                     s.set3 = c("glial" = 1, "neuron" = 1),
                     s.set4 = s.set.osm.area, s.set5 = s.set.osm.mrna)
 
+#---------------------------------------------------
+# k2 experiment -- same reference across experiments
+#---------------------------------------------------
 # run k2 experiment
 celltype.variable <- "k2"
-sample.id.vector <- unique(colData(mae)[,1])
+sample.id.vector <- unique(colData(mae)[,1][complete.cases(mae)])
 sample.id <- sample.id.vector[1]
-df.s.k2 <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
+df.s.k2.shared <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
+  # format cell sizes
   s.set.name <- names(list.s.pred)[s.index]
-  assay.name <- "logcounts"
-  deconvolution.algorithm <- "nnls"
-  s.vector.pred <- list.s.pred[[s.index]] # c("glial" = 2, "neuron" = 3)
+  s.vector.pred <- list.s.pred[[s.index]]
   s.vector.pred <- order_svector(s.vector.pred)
-  rownames(rse.all) <- rowData(rse.all)$Symbol
-  filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
-  filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
-  rse.iter <- rse.all[filter.y.marker, filter.y.sample]
-  rse.iter <- logNormCounts(rse.iter)
-  y.iter <- assays(rse.iter)[[assay.name]]
-  prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
-                         celltype.variable = celltype.variable, s = s.vector.pred, 
-                         typemarker.algorithm = NULL, return.info = FALSE,
-                         deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
-    as.data.frame()
-  prop.pred.iter$s.set.label <- s.set.name
-  prop.pred.iter$s.set.values <- paste0(
-    names(s.vector.pred),"=",s.vector.pred,collapse=",")
-  prop.pred.iter$sample.id <- sample.id
-  prop.pred.iter$k.type <- celltype.variable
-  prop.pred.iter
+  # iterate on samples, returning predictions matrix
+  do.call(rbind, lapply(sample.id.vector, function(sample.id){
+    message(sample.id)
+    # filter bulk
+    filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
+    filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
+    rse.iter <- rse.all[filter.y.marker, filter.y.sample]
+    rse.iter <- logNormCounts(rse.iter)
+    y.iter <- assays(rse.iter)[[assay.name]]
+    # get predictions
+    prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
+                           celltype.variable = celltype.variable, s = s.vector.pred, 
+                           typemarker.algorithm = NULL, return.info = FALSE,
+                           deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
+      as.data.frame()
+    prop.pred.iter$s.set.label <- s.set.name
+    prop.pred.iter$s.set.values <- paste0(
+      names(s.vector.pred),"=",s.vector.pred,collapse=",")
+    prop.pred.iter$sample.id <- sample.id
+    prop.pred.iter$k.type <- celltype.variable
+    prop.pred.iter
+  }))
 }))
+df.s.k2.shared$experiment.type <- "shared.snrnaseq.reference"
 
-#--------------
-# k3 experiment
-#--------------
-# get cell sizes for k3 experiments from expression data
-# coerce to list for experiment
-list.s.pred <- list(s.set1 = c("glial" = 3, "Excit" = 10, "Inhib" = 10),
-                    s.set2 = c("glial" = 10, "Excit" = 3, "Inhib" = 10),
-                    s.set3 = c("glial" = 10, "Excit" = 10, "Inhib" = 3),
-                    s.set2 = c("glial" = 1, "Excit" = 1, "Inhib" = 1))
-
-# run k3 experiment
-celltype.variable <- "k3"
-sample.id.vector <- unique(colData(mae)[,1])
+#--------------------------------------------------------------------------
+# k2 experiment -- using within-sample matched reference across experiments
+#--------------------------------------------------------------------------
+# run k2 experiment
+celltype.variable <- "k2"
+sample.id.vector <- unique(colData(mae)[,1][complete.cases(mae)])
 sample.id <- sample.id.vector[1]
-df.s.k3 <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
+df.s.k2.within <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
+  # format cell sizes
   s.set.name <- names(list.s.pred)[s.index]
-  assay.name <- "logcounts"
-  deconvolution.algorithm <- "nnls"
-  s.vector.pred <- list.s.pred[[s.index]] # c("glial" = 2, "neuron" = 3)
+  s.vector.pred <- list.s.pred[[s.index]]
   s.vector.pred <- order_svector(s.vector.pred)
-  sce.iter <- mae[[2]]
-  sce.iter <- logNormCounts(sce.iter)
-  rownames(rse.all) <- rowData(rse.all)$Symbol
-  filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
-  filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
-  rse.iter <- rse.all[filter.y.marker, filter.y.sample]
-  rse.iter <- logNormCounts(rse.iter)
-  y.iter <- assays(rse.iter)[[assay.name]]
-  prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
-                         celltype.variable = celltype.variable, s = s.vector.pred, 
-                         typemarker.algorithm = NULL, return.info = FALSE,
-                         deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
-    as.data.frame()
-  prop.pred.iter$s.set.label <- s.set.name
-  prop.pred.iter$s.set.values <- paste0(
-    names(s.vector.pred),"=",s.vector.pred,collapse=",")
-  prop.pred.iter$sample.id <- sample.id
-  prop.pred.iter$k.type <- celltype.variable
-  prop.pred.iter
+  # iterate on samples, returning predictions matrix
+  do.call(rbind, lapply(sample.id.vector, function(sample.id){
+    message(sample.id)
+    # filter bulk
+    filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
+    filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
+    rse.iter <- rse.all[filter.y.marker, filter.y.sample]
+    rse.iter <- logNormCounts(rse.iter)
+    y.iter <- assays(rse.iter)[[assay.name]]
+    # filter snrnaseq
+    sce.iter.sample <- sce.iter[,sce.iter$Sample==sample.id]
+    # get predictions
+    prop.pred.iter <- lute(sce = sce.iter.sample, y = y.iter, assay.name = assay.name, 
+                           celltype.variable = celltype.variable, s = s.vector.pred, 
+                           typemarker.algorithm = NULL, return.info = FALSE,
+                           deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
+      as.data.frame()
+    prop.pred.iter$s.set.label <- s.set.name
+    prop.pred.iter$s.set.values <- paste0(
+      names(s.vector.pred),"=",s.vector.pred,collapse=",")
+    prop.pred.iter$sample.id <- sample.id
+    prop.pred.iter$k.type <- celltype.variable
+    prop.pred.iter
+  }))
 }))
+df.s.k2.within$experiment.type <- "within.sample.snrnaseq.reference"
 
-#--------------
-# k4 experiment
-#--------------
-celltype.variable <- "k4"
-sample.id.vector <- unique(colData(mae)[,1])
-sample.id <- sample.id.vector[1]
-df.s.k4 <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
-  s.set.name <- names(list.s.pred)[s.index]
-  assay.name <- "logcounts"
-  deconvolution.algorithm <- "nnls"
-  s.vector.pred <- list.s.pred[[s.index]] # c("glial" = 2, "neuron" = 3)
-  s.vector.pred <- order_svector(s.vector.pred)
-  sce.iter <- mae[[2]]
-  sce.iter <- logNormCounts(sce.iter)
-  rownames(rse.all) <- rowData(rse.all)$Symbol
-  filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
-  filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
-  rse.iter <- rse.all[filter.y.marker, filter.y.sample]
-  rse.iter <- logNormCounts(rse.iter)
-  y.iter <- assays(rse.iter)[[assay.name]]
-  prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
-                         celltype.variable = celltype.variable, s = s.vector.pred, 
-                         typemarker.algorithm = NULL, return.info = FALSE,
-                         deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
-    as.data.frame()
-  prop.pred.iter$s.set.label <- s.set.name
-  prop.pred.iter$s.set.values <- paste0(
-    names(s.vector.pred),"=",s.vector.pred,collapse=",")
-  prop.pred.iter$sample.id <- sample.id
-  prop.pred.iter$k.type <- celltype.variable
-  prop.pred.iter
-}))
+#---------------
+# format results
+#---------------
+df.k2 <- rbind(df.s.k2.shared, df.s.k2.within)
+
+
+
+
