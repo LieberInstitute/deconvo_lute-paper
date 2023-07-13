64bc405b34a0e49081b22ea0e18d1b5e216f2778
Sean Maden <metamaden@users.noreply.github.com>
2023-07-12 18:39:44 -0700

    adds s.factor list, adds loop for sample.id on mae for matched experiment

--- code/03_matched-bulk-predictions/01_mae-matched-experiments_dlpfc-cohort1.R
+++ code/03_matched-bulk-predictions/01_mae-matched-experiments_dlpfc-cohort1.R
@@ -16,7 +16,18 @@ mae <- get(load(mae.filepath))
 #-----------------------------
 # load cell size scale factors
 df.csf <- get_csf_reference()
-# get cell size scale factors from mae data
+df.csf <- df.csf[df.csf$tissue=="brain",]
+df.csf.area <- df.csf[grepl("mRNA", df.csf$scale.factor.type),]
+df.csf.mrna <- df.csf[df.csf$scale.factor.type=="cell area",]
+s.set.osm.area <- c("glial" = df.csf.area[df.csf.area$cell_type=="glial",]$scale.factor.value,
+                    "neuron" = df.csf.area[df.csf.area$cell_type=="neuron",]$scale.factor.value)
+s.set.osm.mrna <- c("glial" = df.csf.mrna[df.csf.mrna$cell_type=="glial",]$scale.factor.value,
+                    "neuron" = df.csf.mrna[df.csf.mrna$cell_type=="neuron",]$scale.factor.value)
+# coerce to list for experiment
+list.s.pred <- list(s.set1 = c("glial" = 3, "neuron" = 10),
+                    s.set2 = c("glial" = 10, "neuron" = 3),
+                    s.set3 = c("glial" = 1, "neuron" = 1),
+                    s.set4 = s.set.osm.area, s.set5 = s.set.osm.mrna)
 
 #--------------
 # k2 experiment
@@ -25,16 +36,37 @@ df.csf <- get_csf_reference()
 
 sample.id.vector <- unique(colData(mae)[,1])
 sample.id <- sample.id.vector[1]
+df.s <- do.call(rbind, lapply(seq(length(list.s.pred)), function(s.index){
+  s.set.name <- names(list.s.pred)[s.index]
+  assay.name <- "logcounts"
+  deconvolution.algorithm <- "nnls"
+  celltype.variable <- "k2"
+  s.vector.pred <- list.s.pred[[s.index]] # c("glial" = 2, "neuron" = 3)
+  s.vector.pred <- order_svector(s.vector.pred)
+  sce.iter <- mae[[2]]
+  sce.iter <- logNormCounts(sce.iter)
+  rownames(rse.all) <- rowData(rse.all)$Symbol
+  filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
+  filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
+  rse.iter <- rse.all[filter.y.marker, filter.y.sample]
+  rse.iter <- logNormCounts(rse.iter)
+  y.iter <- assays(rse.iter)[[assay.name]]
+  prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
+                         celltype.variable = celltype.variable, s = s.vector.pred, 
+                         typemarker.algorithm = NULL, return.info = FALSE,
+                         deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results %>%
+    as.data.frame()
+  prop.pred.iter$s.set.label <- s.set.name
+  prop.pred.iter$s.set.values <- paste0(
+    names(s.vector.pred),"=",s.vector.pred,collapse=",")
+  prop.pred.iter$sample.id <- sample.id
+  prop.pred.iter
+}))
 
-s.vector <- c("glial" = 2, "neuron" = 3)
-sce.iter <- mae[[2]]
 
-rownames(rse.all) <- rowData(rse.all)$Symbol
-filter.y.sample <- colData(rse.all)[,"batch.id2"]==sample.id
-filter.y.marker <- rownames(rse.all) %in% rownames(sce.iter)
-rse.iter <- rse.all[filter.y.marker, filter.y.sample]
 
-s.vector.pred <- order_svector(s.vector.pred)
+
+
 
 # get results for a single iteration of an experiment
 # use with get_ypb_experiment_series()
@@ -44,7 +76,7 @@ dfp <- do.call(rbind, lapply(unique.sample.id.vector, function(sample.id){
   sce.iter <- sce[,sce[[sample.id.variable]]==sample.id]
   y.iter <- mae[[1]] %>% as.matrix()
   prop.true.iter <- table(sce.iter[[celltype.variable]]) %>% prop.table() %>% as.matrix() %>% t()
-  prop.pred.iter <- lute(sce = sce.iter, y = ypb.iter, assay.name = assay.name, 
+  prop.pred.iter <- lute(sce = sce.iter, y = y.iter, assay.name = assay.name, 
                          celltype.variable = celltype.variable, s = s.vector.pred, 
                          typemarker.algorithm = NULL, return.info = FALSE,
                          deconvolution.algorithm = deconvolution.algorithm)$deconvolution.results
