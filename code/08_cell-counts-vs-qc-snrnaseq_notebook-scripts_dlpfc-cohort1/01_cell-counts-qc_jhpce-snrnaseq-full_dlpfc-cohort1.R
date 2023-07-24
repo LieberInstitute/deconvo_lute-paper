c3c997a0e03109ae5fa440801f4e791cd3398723
Sean Maden <metamaden@users.noreply.github.com>
2023-07-24 10:34:37 -0700

    adds new notebooks
    adds notebook for cell counts vs qc comparison
    adds notebook for revised nmarkers vs bias comparison in cohort2

---
+++ code/08_cell-counts-vs-qc-snrnaseq_notebook-scripts_dlpfc-cohort1/01_cell-counts-qc_jhpce-snrnaseq-full_dlpfc-cohort1.R
@@ -0,0 +1,6 @@
+snrnaseq.filename <- "list-scef_markers-k2-k3-k4_ro1-dlpfc.rda"
+snrnaseq.path <- file.path(base.path, snrnaseq.filename)
+snrnaseq <- get(load(snrnaseq.path))
+snrnaseq <- get(load())
+
+sn.qc.k2 <- scuttle::perCellQCMetrics(lscef$k2)
