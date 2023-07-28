ec01b2fb73b80977dd80b9d43a46bc14d690fe71
Sean Maden <metamaden@users.noreply.github.com>
2023-07-28 16:46:32 -0700

    k2 rpkm z norm
    completes k2 z rpkm norm using library sizes from full data

--- code/01_prepare-datasets/08-00_get-qc-libsize-snrnaseq_dlpfc-cohort1.R
+++ code/01_prepare-datasets/08-00_get-qc-libsize-snrnaseq_dlpfc-cohort1.R
@@ -1,6 +1,8 @@
 library(scran)
 sce.path <- "/dcs04/lieber/lcolladotor/deconvolution_LIBD4030/DLPFC_snRNAseq/processed-data/sce/sce_DLPFC.Rdata"
-save.path <- "/users/smaden/sce-dlpfc-cohort1_qc_metrics.rda"
+save.path <- "/users/smaden/total-counts-library-sizes_sce-dlpfc-cohort1.rda"
 sce <- get(load(sce.path))
-metrics <- perCellQCMetrics(sce)
+# metrics <- perCellQCMetrics(sce)
+metrics <- colSums(counts(sce))
+names(metrics) <- colnames(sce)
 save(metrics, file = save.path)
--- code/01_prepare-datasets/08_rpkm-sce-calculate.R
+++ code/01_prepare-datasets/08_rpkm-sce-calculate.R
@@ -5,24 +5,44 @@ sce <- mae[["sn1.rnaseq"]]
 rse <- mae[["bulk.rnaseq"]]; rse <- rse[rownames(sce),]
 sce.counts <- assays(sce)[["counts"]]
 # load library sizes from complete snrnaseq object
-
+sce.libsize.path <- "./deconvo_method-paper/outputs/01_prepare-datasets/total-counts-library-sizes_sce-dlpfc-cohort1.rda"
+sce.libsize <- get(load(sce.libsize.path))
 # set params
 marker.lengths <- rowData(rse)$Length
 library.sizes <- rep(1000, ncol(sce.counts))
 
 # try with full sce object
 sce.input <- DGEList(counts = sce.counts, 
-                     lib.sizes = colSums(sce.counts)+1,
+                     lib.sizes = as.numeric(sce.libsize),
                      genes = data.frame(Length=rowData(rse)$Length))
-sce.input <- calcNormFactors(sce.input)
+#sce.input <- calcNormFactors(sce.input)
 sce.rpkm <- rpkm(y = sce.input, log = F)
 # returns error
 #Error in if (median(f75) < 1e-20) { : 
 #    missing value where TRUE/FALSE needed
 
-# try with reference dataset
+#-----------------------------------
+# try with z reference dataset -- k2
+#-----------------------------------
+# library size by k2 type
+libsize.vector <- sapply(unique(sce[["k2"]]), function(cell.type){
+  celltype.filter <- sce[["k2"]]==cell.type
+  mean(sce.libsize[celltype.filter])})
 sce.z.counts <- lute:::.get_z_from_sce(sce, "counts", "k2")
-rpkm.input <- DGEList(counts = sce.z.counts, 
-                     lib.sizes = colSums(sce.z.counts),
-                     genes = data.frame(Length=marker.lengths))
+rpkm.input <- DGEList(counts = sce.z.counts, lib.sizes = libsize.vector,
+                      genes = data.frame(Length=marker.lengths))
 sce.z.rpkm <- rpkm(y = rpkm.input, log = F)
+# save
+sce.z.rpkm.path <- file.path("./deconvo_method-paper/outputs/01_prepare-datasets/sce-z-rpkm-k2_dlpfc-cohort1.rda")
+save(sce.z.rpkm, file = sce.z.rpkm.path)
+
+# check bulk rpkm
+# assays(mae[["bulk.rpkm.rnaseq"]])[[1]]
+
+#-----------------------------------
+# try with z reference dataset -- k3
+#-----------------------------------
+
+#-----------------------------------
+# try with z reference dataset -- k4
+#-----------------------------------
