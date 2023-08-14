5e29874108f8c8e0753fb55255a4340900de7ee6
Sean Maden <metamaden@users.noreply.github.com>
2023-08-14 12:23:03 -0700

    subsets mae on validation sample ids

--- code/01_prepare-datasets/09-01_get_sampleid_mae_train-validate.R
+++ code/01_prepare-datasets/09-01_get_sampleid_mae_train-validate.R
@@ -30,7 +30,7 @@ length(sce.train.sample.id)
 ybulk.train.sample.id.all <- unique(gsub("_.*" ,"",mae[["bulk.rnaseq"]]$BrNum))
 ybulk.train.sample.id <- ybulk.train.sample.id.all[ybulk.train.sample.id.all %in% sce.train.sample.id]
 ybulk.train.sample.id
-# [1] "Br2720" "Br6432" "Br6471" "Br2743" "Br3942" "Br6423" "Br6522" "Br8325" "Br8492" "Br8667"
+# [1] "Br2720" "Br6471" "Br2743" "Br3942" "Br6423" "Br8325" "Br8492"
 length(ybulk.train.sample.id)
 # [1] 7
 
@@ -38,3 +38,19 @@ length(ybulk.train.sample.id)
 ybulk.test.validate.sample.id <- ybulk.train.sample.id.all[!ybulk.train.sample.id.all %in% sce.train.sample.id]
 # ybulk.test.validate.sample.id
 # [1] "Br6432" "Br6522" "Br8667"
+
+# save mae validate subset
+# test/validate subset
+train.string <- paste0(ybulk.train.sample.id.all, collapse = "|")
+train.mae.filter <- grepl(train.string, mae$sample.id)
+mae.validate <- mae[,train.mae.filter,]
+# save
+mae.validate.name <- "mae-validate_with-rpkm_cohort1.rda"
+mae.validate.path <- file.path("deconvo_method-paper", "outputs", "01_prepare-datasets", "mae.name")
+save(mae.validate, file = mae.validate.path)
+
+
+
+
+
+
