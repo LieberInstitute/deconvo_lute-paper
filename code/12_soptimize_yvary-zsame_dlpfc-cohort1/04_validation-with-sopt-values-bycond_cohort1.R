420fdc33fdbe2e75bd0abb0de1da3c49d1d45ad3
Sean Maden <metamaden@users.noreply.github.com>
2023-08-14 12:28:13 -0700

    adds parameters with helper functions

--- code/12_soptimize_yvary-zsame_dlpfc-cohort1/04_validation-with-sopt-values-bycond_cohort1.R
+++ code/12_soptimize_yvary-zsame_dlpfc-cohort1/04_validation-with-sopt-values-bycond_cohort1.R
@@ -5,8 +5,32 @@
 # Validate sopt utility on new held-out bulk samples.
 #
 
-libv <- c("lute")
+libv <- c("lute", "MultiAssayExperiment")
 sapply(libv, library, character.only = T)
 
+#-----
 # load
+#-----
 # validation data
+validation.sample.id <- c("Br6432", "Br6522", "Br8667")
+# get all mae
+mae.path <- file.path("deconvo_method-paper", "outputs", "01_prepare-datasets", "mae_with-rpkm_additional-data_final.rda")
+mae <- get(load(mae.path))
+# get sce marker data
+sce <- mae[["sn1.rnaseq"]] # training marker expression
+# subset mae
+cd.mae <- colData(mae)
+filter.validate.mae <- grepl(paste0(validation.sample.id,collapse = "|"),cd.mae$sample.id)
+table(filter.validate.mae)
+mae <- mae[,filter.validate.mae,]
+
+
+
+
+
+
+
+
+
+
+
