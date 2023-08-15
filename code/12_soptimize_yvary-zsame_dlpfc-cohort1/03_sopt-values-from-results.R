7be26ad8d97f0862c92d0af6138a8c027879baef
Sean Maden <metamaden@users.noreply.github.com>
2023-08-14 17:32:05 -0700

    updates the df.min summaries of s.train, updates new variable names

--- code/12_soptimize_yvary-zsame_dlpfc-cohort1/03_sopt-values-from-results.R
+++ code/12_soptimize_yvary-zsame_dlpfc-cohort1/03_sopt-values-from-results.R
@@ -16,22 +16,23 @@ load.path <- file.path("deconvo_method-paper", "outputs",
 df.res <- get(load(load.path))
 
 # get unique labels
-labels.vector <- unique(df.res$sample.labels)
+labels.vector <- unique(df.res$sample.label)
 
 #---------------------
 # get optimal s values
 #---------------------
 # gets the minima
 df.min <- do.call(rbind, lapply(labels.vector, function(label.iter){
-  df.iter <- df.res[df.res$sample.labels==label.iter,]
-  df.iter[df.iter$error.neuron == min(df.iter$error.neuron)[1],,drop=F]
+  df.iter <- df.res[df.res$sample.label==label.iter,]
+  df.min.iter <- df.iter[df.iter$error.neuron == min(df.iter$error.neuron),,drop=F]
+  df.min.iter[1,]
 }))
 df.min$min.error.neuron <- df.min$error.neuron
 
 # get list of optimal s values by labels
 list.sopt <- lapply(labels.vector, function(label.iter){
-  df.iter <- df.min[df.min$sample.labels==label.iter,]
-	c("glial" = df.iter[,"glial"], "neuron" = df.iter[,"neuron"])
+  df.iter <- df.min[df.min$sample.label==label.iter,]
+	c("glial" = df.iter[,"s.glial"], "neuron" = df.iter[,"s.neuron"])
 })
 names(list.sopt) <- labels.vector
 
@@ -39,7 +40,7 @@ names(list.sopt) <- labels.vector
 # plot summaries
 #---------------
 
-dfp <- rbind(data.frame(s.train = df.min$glial), data.frame(s.train = df.min$neuron))
+dfp <- rbind(data.frame(s.train = df.min$s.glial), data.frame(s.train = df.min$s.neuron))
 dfp$cell.type <- c(rep("glial", nrow(df.min)), rep("neuron", nrow(df.min)))
 
 # violin
@@ -50,9 +51,9 @@ ggplot(dfp, aes(x = cell.type, y = s.train)) +
   geom_jitter(alpha = 0.5) + geom_boxplot(color = "cyan", alpha = 0)
 
 # scatter
-ggplot(df.min, aes(x = glial, y = neuron)) + geom_point(alpha = 0.5) +
+ggplot(df.min, aes(x = s.glial, y = s.neuron)) + geom_point(alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0)
-ggplot(df.min, aes(x = glial, y = neuron)) + geom_point(alpha = 0.5) +
+ggplot(df.min, aes(x = s.glial, y = s.neuron)) + geom_point(alpha = 0.5) +
   geom_abline(slope = 1, intercept = 0) + facet_wrap(~cell.compartment)
 
 #------------------
