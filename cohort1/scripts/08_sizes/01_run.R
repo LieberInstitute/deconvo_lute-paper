c993697301147585236ee4fc1966319d39113f78
Sean Maden <metamaden@users.noreply.github.com>
2023-10-03 13:22:10 -0700

    adds notebooks

--- cohort1/scripts/08_sizes/01_run.R
+++ cohort1/scripts/08_sizes/01_run.R
@@ -1,5 +1,6 @@
 #!/usr/bin/env R
 
+# Author: Sean Maden
 #
 # Get cell type proportions
 #
@@ -10,14 +11,15 @@ sapply(libv, library, character.only = TRUE)
 folder.name <- "08_sizes"
 source(file.path("./scripts/",folder.name,"/00_size.R"))
 
+#------
 # load
+#------
 mae <- get(load("./outputs/01_mae/mae_allsamples.rda"))
 sn <- mae[["snrnaseq.k2.all"]]
 sample.id.variable <- unique(sn[["Sample"]])
 mae <- mae[,colData(mae)$sample.id %in% sample.id.variable,]
 # sopt cell size factors
 sopt <- get(load("./outputs/06_estimate/train_result.rda"))
-
 
 #----------------
 # get cell sizes
@@ -40,14 +42,10 @@ dfp.tall$ratio.neuron.glial <- dfp.tall$neuron/dfp.tall$glials
 dfp.tall$ratio.centered.glial.neuron <- dfp.tall$centerd.glial/dfp.tall$centered.neuron
 dfp.tall$ratio.centered.neuron.glial <- dfp.tall$centered.neuron/dfp.tall$centered.glial
 
-
-
-
 #------
 # save 
 #------
 
 # save env
 rm(mae)
-save.image(file.path("./env/",folder.name,"/01_sizes_script.RData"))
-
+save.image(file.path("./env/",folder.name,"/01_run_script.RData"))
