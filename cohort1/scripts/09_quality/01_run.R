9ec5527201a14967d52a43d7318f37e9d93dbcef
Sean Maden <metamaden@users.noreply.github.com>
2023-10-05 09:09:03 -0700

    adding quality plots

--- cohort1/scripts/09_quality/01_run.R
+++ cohort1/scripts/09_quality/01_run.R
@@ -32,10 +32,27 @@ df.conf <- conf_from_halodataobject(halo_all)[[1]]
 
 df.conf.freq <- conf_frequencies(df.conf)
 
-map_halo_conf_df <- function(df, df.conf, sample.id.variable = "sample.id"){
-  
-}
+df.conf.wide <- df.conf.freq$df.combo.wide
+dfp.tall$has.all.slides <- 
+  dfp.tall$is.high.and.low <- 
+  dfp.tall$is.high.and.middle <-
+  dfp.tall$is.low.and.middle <-
+  "NA"
+dfp.tall$is.high.consensus <- 
+  dfp.tall$is.low.consensus <- 
+  dfp.tall$is.middle.consensus <-
+  "NA"
 
+for(sample.id in df.conf.wide$sample.id.Circle){
+  df.conf.wide.iter <- df.conf.wide[df.conf.wide$sample.id.Circle==sample.id,]
+  dfp.tall$has.all.slides <- df.conf.wide.iter$has.all.slides
+  dfp.tall$is.high.and.low <- df.conf.wide.iter$is.high.and.low
+  dfp.tall$is.high.and.middle <- df.conf.wide.iter$is.high.and.middle
+  dfp.tall$is.low.and.middle <- df.conf.wide.iter$is.low.and.middle
+  dfp.tall$is.high.consensus <- df.conf.wide.iter$is.high.consensus
+  dfp.tall$is.low.consensus <- df.conf.wide.iter$is.low.consensus
+  dfp.tall$is.middle.consensus <- df.conf.wide.iter$is.middle.consensus
+}
 
 #---------
 # save
