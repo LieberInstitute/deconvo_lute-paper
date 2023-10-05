a7924d6ed3dc0dd8d8f61d0227837e8bd544a6ff
Sean Maden <metamaden@users.noreply.github.com>
2023-10-05 09:21:45 -0700

    debug append

--- cohort1/scripts/09_quality/01_run.R
+++ cohort1/scripts/09_quality/01_run.R
@@ -32,27 +32,63 @@ df.conf <- conf_from_halodataobject(halo_all)[[1]]
 
 df.conf.freq <- conf_frequencies(df.conf)
 
-df.conf.wide <- df.conf.freq$df.combo.wide
-dfp.tall$has.all.slides <- 
-  dfp.tall$is.high.and.low <- 
-  dfp.tall$is.high.and.middle <-
-  dfp.tall$is.low.and.middle <-
-  "NA"
-dfp.tall$is.high.consensus <- 
-  dfp.tall$is.low.consensus <- 
-  dfp.tall$is.middle.consensus <-
-  "NA"
+list.dfp.tall <- list(
+  
+  dfp.tall = dfp.tall,
+  dfp.tall.high = dfp.tall.high,
+  dfp.tall.low = dfp.tall.low
+  
+)
 
-for(sample.id in df.conf.wide$sample.id.Circle){
-  df.conf.wide.iter <- df.conf.wide[df.conf.wide$sample.id.Circle==sample.id,]
-  dfp.tall$has.all.slides <- df.conf.wide.iter$has.all.slides
-  dfp.tall$is.high.and.low <- df.conf.wide.iter$is.high.and.low
-  dfp.tall$is.high.and.middle <- df.conf.wide.iter$is.high.and.middle
-  dfp.tall$is.low.and.middle <- df.conf.wide.iter$is.low.and.middle
-  dfp.tall$is.high.consensus <- df.conf.wide.iter$is.high.consensus
-  dfp.tall$is.low.consensus <- df.conf.wide.iter$is.low.consensus
-  dfp.tall$is.middle.consensus <- df.conf.wide.iter$is.middle.consensus
-}
+df.conf.wide <- df.conf.freq$df.combo.wide
+
+list.dfp.tall.append.quality <- lapply(list.dfp.tall, function(item){
+  dfp.tall.iter <- item
+  
+  dfp.tall.iter$has.all.slides <- 
+    dfp.tall.iter$is.high.and.low <- 
+    dfp.tall.iter$is.high.and.middle <-
+    dfp.tall.iter$is.low.and.middle <-
+    "NA"
+  dfp.tall.iter$is.high.consensus <- 
+    dfp.tall.iter$is.low.consensus <- 
+    dfp.tall.iter$is.middle.consensus <-
+    "NA"
+  
+  for(sample.id in df.conf.wide$sample.id.Circle){
+    
+    
+    message(sample.id)
+    df.conf.wide.iter <- 
+      df.conf.wide[df.conf.wide$sample.id.Circle==sample.id,,drop = F]
+    
+    
+    if(sample.id %in% dfp.tall.iter$sample.id){
+      iter.filter.condition <- dfp.tall.iter$sample.id==sample.id
+      
+      dfp.tall.iter[iter.filter.condition,]$has.all.slides <- 
+        df.conf.wide.iter$has.all.slides
+      dfp.tall.iter[iter.filter.condition,]$is.high.and.low <- 
+        df.conf.wide.iter$is.high.and.low
+      dfp.tall.iter[iter.filter.condition,]$is.high.and.middle <- 
+        df.conf.wide.iter$is.high.and.middle
+      dfp.tall.iter[iter.filter.condition,]$is.low.and.middle <- 
+        df.conf.wide.iter$is.low.and.middle
+      dfp.tall.iter[iter.filter.condition,]$is.high.consensus <- 
+        df.conf.wide.iter$is.high.consensus
+      dfp.tall.iter[iter.filter.condition,]$is.low.consensus <- 
+        df.conf.wide.iter$is.low.consensus
+      dfp.tall.iter[iter.filter.condition,]$is.middle.consensus <- 
+        df.conf.wide.iter$is.middle.consensus
+    }
+    
+  }
+  
+  return(dfp.tall.iter)
+  
+})
+
+names(list.dfp.tall.append.quality) <- names(list.dfp.tall)
 
 #---------
 # save
