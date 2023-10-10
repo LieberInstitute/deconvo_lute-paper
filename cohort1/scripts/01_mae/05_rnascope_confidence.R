4e93428c1c6150e96f53ae17aec8f07f52e9b4bf
Sean Maden <metamaden@users.noreply.github.com>
2023-10-09 17:55:04 -0700

    samples samples qc df

--- cohort1/scripts/01_mae/05_rnascope_confidence.R
+++ cohort1/scripts/01_mae/05_rnascope_confidence.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env R
 
-# 
+# Author: Sean Maden
+#
+# Gets summaries and confidences by sample.id.
+#
 #
 #
 
@@ -13,16 +16,37 @@ combo.id.variable <- "SAMPLE_ID"
 confidence.variable <- "Confidence"
 
 cd.id <- colData(mae)
+cd.id$confidence.circle <- cd.id$confidence.star <- "NA"
+
 sce.img <- mae[["sce.img"]]
 cd.rnascope <- colData(sce.img)
-cd$confidence.circle <- cd$confidence.star <- "NA"
+cd.rnascope$combo <- gsub(".*_", "", cd.rnascope$SAMPLE_ID)
+table(cd.rnascope$combo)
 
-sce.img$combo <- gsub(".*_", "", sce.img[[combo.id.variable]]) 
+cell.sizes <- metadata(mae[["snrnaseq.k2.all"]])[["cell.sizes"]]
+cell.sizes <- cell.sizes[cell.sizes$ktype=="k2",]
 
-for(sample.id in cd$sample.id){
-  cd.id[cd.id]$confidence <- colData(sce.img)[sce.img$combo=="STAR",confidence.variable][1]
+cell.proportions <- metadata(mae[["snrnaseq.k2.all"]])[["list.df.true.k2"]]
+cd.id$size.sn.glial <- cd.id$size.sn.neuron <- "NA"
+cd.id$proportion.sn.glial <- cd.id$proportion.sn.neuron <- "NA"
+
+for(sample.id in cd.id$sample.id){
+  message(sample.id)
+  filter.sample.rnascope <- cd.rnascope$Sample==sample.id
+  cd.rnascope.iter <- cd.rnascope[filter.sample.rnascope,]
+  cd.id[cd.id$sample.id==sample.id,]$confidence.circle <- cd.rnascope.iter[cd.rnascope.iter$combo=="CIRCLE",]$Confidence[1]
+  cd.id[cd.id$sample.id==sample.id,]$confidence.star <- cd.rnascope.iter[cd.rnascope.iter$combo=="STAR",]$Confidence[1]
   
+  cell.sizes.iter <- cell.sizes[cell.sizes$sample.id==sample.id,]
+  filter.glial <- cell.sizes.iter[,2]=="glial"
+  filter.neuron <- cell.sizes.iter[,2]=="neuron"
+  cd.id[cd.id$sample.id==sample.id,]$size.sn.glial <- cell.sizes.iter[filter.glial,]$size[1]
+  cd.id[cd.id$sample.id==sample.id,]$size.sn.neuron <- cell.sizes.iter[filter.neuron,]$size[1]
+  
+  if(sample.id %in% names(cell.proportions)){
+    cd.id[cd.id$sample.id==sample.id,]$proportion.sn.glial <- cell.proportions[[sample.id]][["glial"]]
+    cd.id[cd.id$sample.id==sample.id,]$proportion.sn.neuron <- cell.proportions[[sample.id]][["neuron"]]
+  }
 }
 
-
-
+save(cd.id, file = "./outputs/01_mae/sample_qc_df.rda")
