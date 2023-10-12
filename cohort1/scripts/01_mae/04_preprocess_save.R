e0dacec0bc0205299304bf6f2c0a3db7309be77d
Sean Maden <metamaden@users.noreply.github.com>
2023-10-12 08:49:32 -0700

    adds mae analysis output tests

--- cohort1/scripts/01_mae/04_preprocess_save.R
+++ cohort1/scripts/01_mae/04_preprocess_save.R
@@ -1,54 +1,80 @@
 #!/usr/bin/env R
 
-#
-# Preprocess MultiAssayExperiment
-#
-#
-#
+# Author: Sean Maden
+# 
+# Preprocess MultiAssayExperiment by applying filters and saving MAE set for analysis.
 #
 #
-
+#
+#
+#
 
 filter.rnascope.confidence <- "Low"
 
 #-----
 # load
 #-----
+# mae input
 mae.in.path <- "./outputs/01_mae/mae_allsamples_append.rda"
-mae <- mae.all <- get(load(mae.in.path))
-dim(colData(mae))
+mae <- get(load(mae.in.path))
+# test mae input
+num.samples.mae.input <- 22
+length(unique(colData(mae)$sample.id))==num.samples.mae.input
 
 # rnascope confidence annotations
-cd.id <- get(load("./outputs/01_mae/sample_qc_df.rda"))
-samples.to.remove <- 
-  cd.id$confidence.star == "Low" |
-  cd.id$confidence.circle == "Low"
-sample.vector.keep <- cd.id[!samples.to.remove,]$sample.id
+cd.id <- get(
+  load(
+    "./outputs/01_mae/sample_qc_df.rda"))
+samples.to.remove <- unique(cd.id[cd.id$remove.low==TRUE,]$sample.id)
+
+# samples to keep
+sample.vector.keep <- colData(mae)$sample.id
+sample.vector.keep <- sample.vector.keep[!sample.vector.keep %in% samples.to.remove]
+
+# test filtered samples
+num.samples.keep <- 19
+num.samples.filter <- 3
+length(samples.to.remove)==num.samples.filter
+length(sample.vector.keep)==num.samples.keep
 
 
 
 
 
 
-#---------------------------------------------
-# 1. filter on rnascope confidence annotations
-#---------------------------------------------
 
-# filter sce.img
-sce.img <- mae[["sce.img"]]
-dim(sce.img)
-filter.sce <- colData(sce.img)$Sample %in% sample.vector.keep
-sce.img <- sce.img[,filter.sce]
-dim(sce.img)
-mae[["sce.img"]] <- sce.img
+#---------------------------------
+# 1. defines the sample ids filter
+#---------------------------------
+total.source.ids.to.keep <- 13
 
-# filter cell.sizes
-cell.sizes <- mae[["cell.sizes"]]
-dim(cell.sizes)
-filter.cell.sizes <- cell.sizes["sample_id",] %in% sample.vector.keep
-cell.sizes <- cell.sizes[,filter.cell.sizes]
-dim(cell.sizes)
-mae[["cell.sizes"]] <- cell.sizes
+# sample id vector to keep
+sample.vector.keep <- unique(
+  na.omit(cd.id[!cd.id$sample.id %in% samples.to.remove,]$sample.id))
+
+sample.vector.keep <- intersect(
+  unique(mae[[1]]$Sample),
+  unique(sample.vector.keep)
+) # unique with k2 sce samples
+
+#sample.vector.keep <- unique(
+#  unique(mae[[1]]$Sample),
+#  sample.vector.keep
+#) # unique with k2 sce samples
+
+length(sample.vector.keep)==total.source.ids.to.keep
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 #--------------------------------------
@@ -59,11 +85,13 @@ mae.old <- mae
 sample.map.mae <- mae@sampleMap
 
 overlapping.samples.3.platforms <- intersect(
+  
   intersect(
     sample.map.mae[sample.map.mae$assay=="snrnaseq.k2.all",]$primary,
     sample.map.mae[sample.map.mae$assay=="cell.sizes",]$primary
   ),
   sample.map.mae[sample.map.mae$assay=="bulk.rnaseq",]$primary
+  
 )
 
 #filter.samples.3platforms <- 
@@ -75,7 +103,38 @@ sample.mae.map.filter.final <-
 
 mae <- mae[,sample.mae.map.filter.final,]
 
+# tests filtered mae
+mae.final.samples.count <- 11
 dim(colData(mae))
+
+names.test.mae1 <- c("snrnaseq.k2.all")
+for(name in names.test.mae1){
+  message(name)
+  message(length(unique(mae[[name]]$Sample))==mae.final.samples.count)
+}
+
+names.test.mae2 <- c("snrnaseq.k3.all", 
+                    "snrnaseq.k4.all",
+                    "bulk.rnaseq", 
+                    "bulk.rpkm.rnaseq",
+                    "cell.sizes", 
+                    "sce.img",
+                    "bulk.pb.k2", "bulk.pb.k3", "bulk.pb.k4")
+for(name2 in names.test.mae2){
+  message(name2)
+  message(
+    length(unique(mae[[name2]]$Sample))==mae.final.samples.count)
+}
+
+names.test.mae3 <- c("cell.sizes")
+for(name in names.test.mae3){
+  message(name)
+  message(length(unique(mae[[name]]["sample_id",]))==mae.final.samples.count)
+}
+
+
+
+
 
 
 
