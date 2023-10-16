8a80350e7fecce0de17c15bae3966bab7975bd50
Sean Maden <metamaden@users.noreply.github.com>
2023-10-16 11:53:04 -0700

    adds param script, adds filter type to summaries dfs

---
+++ cohort1/scripts/02_summarize_mae/00_param.R
@@ -0,0 +1,103 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+# Functions for MAE summaries.
+#
+
+
+summaries_df_list <- function(dfmap, filter.type.label){
+  
+  # get summaries by data set type
+  df.all <- do.call(cbind, 
+                    lapply(variables.vector, 
+                           function(variable.iter){
+                             dfmap$variable.iter <- dfmap[,variable.iter]
+                             df.iter <- dfmap %>% 
+                               as.data.frame() %>%
+                               distinct(assay, variable.iter) %>%
+                               group_by(assay) %>%
+                               summarize(n()) %>%
+                               as.data.frame()
+                             colnames(df.iter) <- c("assay", variable.iter)
+                             df.iter
+                           })) %>% as.data.frame()
+  df.all <- df.all[,c(1,2,4,6)]
+  df.train <- do.call(cbind, 
+                      lapply(variables.vector, 
+                             function(variable.iter){
+                               dfmap$variable.iter <- dfmap[,variable.iter]
+                               df.iter <- dfmap %>% 
+                                 as.data.frame() %>%
+                                 filter(primary %in% sample.id.train) %>%
+                                 distinct(assay, variable.iter) %>%
+                                 group_by(assay) %>%
+                                 summarize(n()) %>%
+                                 as.data.frame()
+                               colnames(df.iter) <- c("assay", variable.iter)
+                               df.iter
+                             })) %>% as.data.frame()
+  df.train <- df.train[,c(1,2,4,6)]
+  df.validate <- do.call(cbind, 
+                         lapply(variables.vector, 
+                                function(variable.iter){
+                                  dfmap$variable.iter <- dfmap[,variable.iter]
+                                  df.iter <- dfmap %>% 
+                                    as.data.frame() %>%
+                                    filter(primary %in% sample.id.validate) %>%
+                                    distinct(assay, variable.iter) %>%
+                                    group_by(assay) %>%
+                                    summarize(n()) %>%
+                                    as.data.frame()
+                                  colnames(df.iter) <- c("assay", variable.iter)
+                                  df.iter
+                                })) %>% as.data.frame()
+  df.validate <- df.validate[,c(1,2,4,6)]
+  
+  # make wide data.frame
+  df.all.wide <- df.all; df.train.wide <- df.train; df.validate.wide <- df.validate
+  colnames(df.all.wide)[2:4] <- paste0(colnames(df.all.wide)[2:4],".all")
+  colnames(df.train.wide)[2:4] <- paste0(colnames(df.train.wide)[2:4],".train")
+  colnames(df.validate.wide)[2:4] <- paste0(colnames(df.validate.wide)[2:4],".validate")
+  
+  df.wide <- cbind(df.all.wide, 
+                   cbind(df.train.wide[,c(2:4)], df.validate.wide[,c(2:4)])) %>% 
+    as.data.frame()
+  
+  # make df tall tables
+  df.all$data.type <- "all"
+  df.train$data.type <- "train"
+  df.validate$data.type <- "validate"
+  df.tall <- rbind(df.all,
+                   rbind(df.train, df.validate)) %>% as.data.frame()
+  
+  # append formatted assay name and platform name
+  assays.keep <- c("snrnaseq.k2.all", "cell.sizes", "bulk.rnaseq")
+  df.tall <- df.tall[df.tall[,1] %in% assays.keep,]
+  df.wide <- df.wide[df.wide[,1] %in% assays.keep,]
+  df.tall$platform.name <- df.tall$assay.type <- 
+    df.wide$platform.name <- df.wide$assay.type <- "NA"
+  
+  assay.name.iter <- assays.keep[1]
+  df.wide[df.wide$assay==assay.name.iter,]$assay.type <- "snRNAseq"
+  df.wide[df.wide$assay==assay.name.iter,]$platform.name <- "10X Chromium"
+  df.tall[df.tall$assay==assay.name.iter,]$assay.type <- "snRNAseq"
+  df.tall[df.tall$assay==assay.name.iter,]$platform.name <- "10X Chromium"
+  
+  assay.name.iter <- assays.keep[2]
+  df.wide[df.wide$assay==assay.name.iter,]$assay.type <- "fluorescent in situ hybridization"
+  df.wide[df.wide$assay==assay.name.iter,]$platform.name <- "RNAscope"
+  df.tall[df.tall$assay==assay.name.iter,]$assay.type <- "fluorescent in situ hybridization"
+  df.tall[df.tall$assay==assay.name.iter,]$platform.name <- "RNAscope"
+  
+  assay.name.iter <- assays.keep[3]
+  df.wide[df.wide$assay==assay.name.iter,]$assay.type <- "bulk RNAseq"
+  df.wide[df.wide$assay==assay.name.iter,]$platform.name <- "Illumina HiSeq"
+  df.tall[df.tall$assay==assay.name.iter,]$assay.type <- "bulk RNAseq"
+  df.tall[df.tall$assay==assay.name.iter,]$platform.name <- "Illumina HiSeq"
+  
+  df.tall$filter.type <- filter.type.label
+  df.wide$filter.type <- filter.type.label
+  
+  return(list(wide = df.wide, tall = df.tall))
+}
--- cohort1/scripts/02_summarize_mae/01_data_summaries_table.R
+++ cohort1/scripts/02_summarize_mae/01_data_summaries_table.R
@@ -19,12 +19,7 @@ list.sample.id <- get(load("./outputs/00_preprocess/list_snrnaseq_sampleid.rda")
 sample.id.train <- list.sample.id[["train"]]
 sample.id.validate <- list.sample.id[["validation"]]
 
-
-
-
-
-
-
+source("./scripts/02_summarize_mae/00_param.R")
 
 
 #----------------------------
@@ -39,121 +34,14 @@ dfmap$region <- gsub(".*_", "", dfmap$primary)
 dfmap$brnum <- gsub("_.*", "", dfmap$primary)
 variables.vector <- c("primary", "region", "brnum")
 
-
-
-
-
-# get summaries by data set type
-df.all <- do.call(cbind, 
-                      lapply(variables.vector, 
-                             function(variable.iter){
-  dfmap$variable.iter <- dfmap[,variable.iter]
-  df.iter <- dfmap %>% 
-    as.data.frame() %>%
-    distinct(assay, variable.iter) %>%
-    group_by(assay) %>%
-    summarize(n()) %>%
-    as.data.frame()
-  colnames(df.iter) <- c("assay", variable.iter)
-  df.iter
-})) %>% as.data.frame()
-df.all <- df.all[,c(1,2,4,6)]
-df.train <- do.call(cbind, 
-                    lapply(variables.vector, 
-                           function(variable.iter){
-                             dfmap$variable.iter <- dfmap[,variable.iter]
-                             df.iter <- dfmap %>% 
-                               as.data.frame() %>%
-                               filter(primary %in% sample.id.train) %>%
-                               distinct(assay, variable.iter) %>%
-                               group_by(assay) %>%
-                               summarize(n()) %>%
-                               as.data.frame()
-                             colnames(df.iter) <- c("assay", variable.iter)
-                             df.iter
-                           })) %>% as.data.frame()
-df.train <- df.train[,c(1,2,4,6)]
-df.validate <- do.call(cbind, 
-                    lapply(variables.vector, 
-                           function(variable.iter){
-                             dfmap$variable.iter <- dfmap[,variable.iter]
-                             df.iter <- dfmap %>% 
-                               as.data.frame() %>%
-                               filter(primary %in% sample.id.validate) %>%
-                               distinct(assay, variable.iter) %>%
-                               group_by(assay) %>%
-                               summarize(n()) %>%
-                               as.data.frame()
-                             colnames(df.iter) <- c("assay", variable.iter)
-                             df.iter
-                           })) %>% as.data.frame()
-df.validate <- df.validate[,c(1,2,4,6)]
-
-
-# make wide data.frame
-df.all.wide <- df.all; df.train.wide <- df.train; df.validate.wide <- df.validate
-colnames(df.all.wide)[2:4] <- paste0(colnames(df.all.wide)[2:4],".all")
-colnames(df.train.wide)[2:4] <- paste0(colnames(df.train.wide)[2:4],".train")
-colnames(df.validate.wide)[2:4] <- paste0(colnames(df.validate.wide)[2:4],".validate")
-
-df.wide <- cbind(df.all.wide, 
-                 cbind(df.train.wide[,c(2:4)], df.validate.wide[,c(2:4)])) %>% 
-  as.data.frame()
-
-
-df.train$data.type <- "train"
-df.validate$data.type <- "validate"
-df.summary$data.type <- "all"
-
-
-# PLATFORM_TYPE X SAMPLE_SOURCE_ID_COUNT
-
-# make tall data.frame
-new.table <- rbind(df.samples.map.all,
-                   rbind(
-                     df.samples.map.train,
-                     df.samples.map.validate
-                   ))
-colnames(new.table) <- c("platform", "unique_sample_source_count", "dataset_type")
-new.table <- new.table[,c(3,1,2)]
-
-
-# append formatted assay name and platform name
-new.table$platform.name <- 
-  new.table$assay.type <- "NA"
-platform.names.iter <- c(
-  "snrnaseq.k2.all", "snrnaseq.k3.all", "snrnaseq.k4.all",
-  "bulk.pb.k2", "bulk.pb.k3", "bulk.pb.k4"
-)
-new.table[new.table$platform %in% platform.names.iter,]$assay.type <- "snRNAseq"
-new.table[new.table$platform %in% platform.names.iter,]$platform.name <- "10X Chromium"
-platform.names.iter <- c(
-  "bulk.rnaseq", "bulk.rpkm.rnaseq"
-)
-new.table[new.table$platform %in% platform.names.iter,]$assay.type <- "bulk RNAseq"
-new.table[new.table$platform %in% platform.names.iter,]$platform.name <- "Illumina HiSeq"
-platform.names.iter <- c(
-  "cell.sizes", "sce.img"
-)
-new.table[new.table$platform %in% platform.names.iter,]$assay.type <- "fluorescent in situ hybridization"
-new.table[new.table$platform %in% platform.names.iter,]$platform.name <- "RNAscope"
-
-
-
+list.summaries.prefilter <- get_summaries_df_list(dfmap, label = "prefilter")
 
 # save
 save(new.table, file = "./outputs/01_mae/table2_platforms_prefilter.rda")
 write.csv(new.table, file = "./outputs/01_mae/table2_platforms_prefilter.csv", row.names = FALSE)
 
-new.table.prefilter <- new.table
-
-
-
-
-
-
-
-
+df.wide.prefilter <- df.wide
+df.tall.prefilter <- df.tall
 
 
 
