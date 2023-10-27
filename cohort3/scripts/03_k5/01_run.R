fb8a0f62b6f811e2926e9ee25606b3d78e6c450f
Sean Maden <metamaden@users.noreply.github.com>
2023-10-27 06:27:41 -0700

    adds mappings

--- cohort3/scripts/03_k5/01_run.R
+++ cohort3/scripts/03_k5/01_run.R
@@ -6,26 +6,74 @@
 #
 #
 
+library(tidyr)
+
 #-----
 # load
 #-----
+
 load("./env/02_abisseq/01_abisseq_script.RData")
 source("./source/lute_experiments.R")
 
 #------------------
 # map k5 cell types
 #------------------
-cellTypes <- c("T", "B", "Dendrocyte", "Plasma", "Monocyte", "Neutrophil", "NK")
-cellTypesRemove <- c("")
-cellSizes <- c()
+
+vectorCellTypeMap <- c("T", "B", "Dendrocyte", "Plasma", "Monocyte", "Neutrophil", "NK")
+vectorCellTypeStart <- colnames(zref)
+
+mappingsTable <- cellLabelMappings(
+  vectorCellTypeMap, vectorCellTypeStart, returnType = "list")[[1]]
+
+zrefMapped <- cellLabelMappings(
+  vectorCellTypeMap, vectorCellTypeStart, zref, returnType = "df")
+
+# tests
+#
+#
+#
+# test label Monocyte
+mappedLabel <- "Monocyte"
+# test gene FXYD6, label Monocyte
+geneName <- "FXYD6"
+zrefMapped[geneName,mappedLabel]==rowMeans(
+  zref[,which(colnames(zref) %in% mappingsTable[mappingsTable[,2]==mappedLabel,1])]
+)[geneName]
+# test gene NRG1, label Monocyte
+geneName <- "NRG1"
+zrefMapped[geneName,mappedLabel]==rowMeans(
+  zref[,which(colnames(zref) %in% mappingsTable[mappingsTable[,2]==mappedLabel,1])]
+)[geneName]
+
+# test label Plasma
+mappedLabel <- "Plasma"
+# test gene FXYD6, label Plasma
+geneName <- "FXYD6"
+zrefMapped[geneName,mappedLabel]==
+  zref[,which(colnames(zref) %in% 
+                mappingsTable[mappingsTable[,2]==mappedLabel,1])][
+                  which(rownames(zref)==geneName)]
+# test gene NRG1, label Plasma
+geneName <- "NRG1"
+zrefMapped[geneName,mappedLabel]==
+  zref[,which(colnames(zref) %in% 
+                mappingsTable[mappingsTable[,2]==mappedLabel,1])][
+                  which(rownames(zref)==geneName)]
+
+
+
+
+
+
+
 
 #---------------
 # run experiment
 #---------------
 # get experiment results
 experimentList <- newExperimentList(
-  singleCellExperiment=singleCellExperimentDarmanis,
-  referenceExpression=NULL,
+  singleCellExperiment=NULL,
+  referenceExpression=zref.new,
   bulkExpression=bulkExpressionDarmanis,
   cellScaleFactors=cellScaleFactors,
   experimentType=experimentType,
---
+++ cohort3/source/cell_mappings_helpers.R
@@ -0,0 +1,75 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+#
+#
+#
+#
+#
+
+cellLabelMappings <- function(
+    vectorCellTypeMap, vectorCellTypeStart = NULL, dfToMap = NULL, 
+    returnType = c("list", "df"), summaryOperation = c("mean", "median")){
+  #
+  #
+  #
+  #
+  # uses regex to map labels
+  # returnType : whether to return list or single df (mapped values)
+  # summaryOperation : how values were summarized
+  #
+  #
+  
+  # get mappings
+  if(is(vectorCellTypeStart, "NULL")){
+    mappingsTable <- data.frame(celltype1 = colnames(dfToMap))
+  } else{
+    mappingsTable <- data.frame(celltype1 = vectorCellTypeStart)
+  }
+  
+  mappingsTable$celltype2 <- "NA"
+  for(type in vectorCellTypeMap){
+    message(type)
+    filterCelltype1 <- 
+      which(grepl(paste0("^", type, ".*"), mappingsTable$celltype1))
+    if(length(filterCelltype1)>0){
+      mappingsTable[filterCelltype1,]$celltype2 <- type
+    }
+  }
+  
+  # map terms to reference
+  dfMapped <- "NULL"
+  if(is(dfToMap, "NULL")){
+    message("Skipping mapping summaries")
+  } else{
+    dfMapped <- dfToMap
+    colnames(dfMapped) <- unlist(lapply(colnames(dfMapped),function(cname){
+      mappingsTable[mappingsTable[,1]==cname,2]
+    }))
+    vectorNewTypes <- unique(mappingsTable[,2])
+    dfMapped <- do.call(cbind, lapply(vectorNewTypes, function(type){
+      filterType <- which(
+        colnames(dfToMap) %in% mappingsTable[mappingsTable[,2]==type,1])
+      if(length(filterType)>1){
+        rowMeans(dfToMap[,filterType])
+      } else{
+        dfToMap[,filterType]
+      }
+    }))
+    colnames(dfMapped) <- vectorNewTypes
+  }
+  
+  # parse return options
+  if(returnType == "list"){
+    returnObject <- list(
+      mappingsTable = mappingsTable,
+      dfMapped = dfMapped
+    )
+  } else if(returnType == "df" & is(dfMapped, "NULL")){
+    returnObject <- mappingsTable
+  } else{
+    returnObject <- dfMapped
+  }
+  return(returnObject)
+}
