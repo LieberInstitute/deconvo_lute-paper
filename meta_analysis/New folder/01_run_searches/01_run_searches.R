4f7530c9c9cbfadacc9f358afb3df9862f0c7a18
Sean Maden <metamaden@users.noreply.github.com>
2023-10-31 10:21:25 -0700

    adds meta_analysis

---
+++ meta_analysis/New folder/01_run_searches/01_run_searches.R
@@ -0,0 +1,13 @@
+#!/usr/bin/env R
+
+# Author: Sean Maden
+#
+# Searches on markers for cell types (in tissue contexts).
+#
+
+# load
+
+load("./data/search_results.csv")
+
+# save
+save.imagee("./env/01_run_searches.RData")
