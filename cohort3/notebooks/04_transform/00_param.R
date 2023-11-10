98c8b8c45fbd0d8d018c2239c4cd346ff2447719
Sean Maden <metamaden@users.noreply.github.com>
2023-11-10 06:47:17 -0800

    sets chunk include to false, adds mapTable function for abis

--- cohort3/notebooks/04_transform/00_param.R
+++ cohort3/notebooks/04_transform/00_param.R
@@ -37,7 +37,7 @@ annotationHeatmapList <- function(
   identical(rownames(heatmapTall), markerTable$marker)
   
   leftAnnotation<-rowAnnotation(
-    markerTypeLabel=markerTable$type,
+    markerTypeLabel=markerTable[,"cellType"],
     col=list(markerTypeLabel=columnLabelVector)
   )
   
@@ -56,3 +56,34 @@ annotationHeatmapList <- function(
     )
   )
 }
+
+mapTableImmuneCells <- function(whichTable="abis17"){
+  if(whichTable=="abis17"){
+    mapTable <- data.frame(
+      cellType = colnames(heatmapTall),
+      color=c(
+        "forestgreen", # Monocytes.C
+        "tan", # NK
+        "dodgerblue", # T.CD8.Memory
+        "gold4", # T.CD4.Naive
+        "gold4", # T.CD8.Naive
+        "brown", # B.Naive
+        "yellow", # T.CD4.Memory
+        "blue4", # MAIT
+        "purple", # T.gd.Vd2
+        "gold2", # Neutrophils.LD
+        "purple", # T.gd.non.Vd2
+        "darkorange1", # "Basophils.LD
+        "forestgreen", # Monocytes.NC.I
+        "darkgoldenrod4", # B.Memory
+        "darkgreen", # mDCs
+        "orange", # pDCs
+        "gold" # Plasmablasts
+      )
+    )
+  } else{
+    stop("Error, table not found.")
+  }
+  return(mapTable)
+}
+
