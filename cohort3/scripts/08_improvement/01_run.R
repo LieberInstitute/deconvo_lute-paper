296f75a4de5155d9351f537bc85f30a0b3c80678
Sean Maden <metamaden@users.noreply.github.com>
2023-11-20 10:20:20 -0800

    cleans up folder, builds new notebook

--- cohort3/scripts/08_improvement/01_run.R
+++ cohort3/scripts/08_improvement/01_run.R
@@ -126,125 +126,36 @@ dfPlotTranspose <- t(df.plot.tall.s13) %>% as.data.frame()
 
 # append proportions
 dfProportions <- experimentData[["p.true"]]
+# check samples overlap
+length(intersect(rownames(dfProportions), dfPlotTall$sample.id))
+samplesKeep <- intersect(rownames(dfProportions), unique(dfPlotTall$sample.id))
+# format as fractions
+dfProportions <- apply(dfProportions,2,function(ci){ci/sum(ci)}) %>% 
+  as.data.frame()
 
 # get very tall plot of sample X proportion
 dfPlotTallTall <- reshape2::melt(dfPlotTall)
-for(type in colnames(dfProportions)){
+colnames(dfPlotTallTall) <- 
+  c("sample.id", "condition", "cellType", "proportion")
+trueProportionVarname <- "trueProportion"
+dfPlotTallTall[,ncol(dfPlotTallTall)+1] <- "NA"
+colnames(dfPlotTallTall)[ncol(dfPlotTallTall)] <- trueProportionVarname
+for(cellType in colnames(dfProportions)){
   for(sample.id in unique(dfPlotTallTall$sample.id)){
-    filterTall <- 
-      dfPlotTall[
-        dfPlotTall[,"sample.id.format"]==sample.id, type]$true.proportions <- 
-      dfProportions[sample.id,type]
+    filterTallTall <- 
+      dfPlotTallTall[,"sample.id"]==sample.id & 
+      dfPlotTallTall[,"cellType"]==cellType
+    dfPlotTallTall[filterTallTall, trueProportionVarname] <- 
+      dfProportions[sample.id, cellType]
   }
 }
+# filter samples
+filterSamples <- dfPlotTallTall$sample.id %in% samplesKeep
+dfPlotTallTall <- dfPlotTallTall[filterSamples,]
+dfPlotTallTall$trueProportion <- as.numeric(dfPlotTallTall$trueProportion)
 
-
-# append to df.plot.tall.s13
-dfPlotTall$true.proportions <- "NA"
-dfPlotTall$sample.id.format <- gsub("_.*", "", dfPlotTall$sample.id)
-for(type in colnames(df.proportions)){
-  for(sample.id in unique(dfPlotTall$sample.id.format)){
-    filterTall <- 
-      dfPlotTall[
-        dfPlotTall[,"sample.id.format"]==sample.id, type]$true.proportions <- 
-      dfProportions[sample.id,type]
-  }
-}
-
-
-
-
-#------------------------------
-# aggregate results proportions
-#------------------------------
-# get means on overlapping ids
-df.plot.tall.mean <- 
-  df.plot.tall.s13 %>% group_by(type) %>% 
-  summarise(across(everything(), mean)) %>% t() %>% as.data.frame()
-colnames(df.plot.tall.mean) <- df.plot.tall.mean[1,]
-df.plot.tall.mean <- df.plot.tall.mean[seq(2,nrow(df.plot.tall.mean)),]
-head(df.plot.tall.mean)
-
-# get sds
-df.plot.tall.sd <- df.plot.tall.s13 %>% 
-  group_by(type) %>% summarise(across(everything(), sd)) %>%
-  t() %>% as.data.frame()
-colnames(df.plot.tall.sd) <- df.plot.tall.sd[1,]
-df.plot.tall.sd <- df.plot.tall.sd[seq(2,nrow(df.plot.tall.sd)),]
-head(df.plot.tall.sd)
-
-# combine
-colnames(df.plot.tall.sd) <- paste0("sd.", colnames(df.plot.tall.sd))
-colnames(df.plot.tall.mean) <- paste0("mean.", colnames(df.plot.tall.mean))
-df.tall <- as.data.frame(cbind(df.plot.tall.mean, df.plot.tall.sd))
-
-# append true
-df.tall$true.prop.mean.flow.cyto <- 
-  df.tall$true.prop.sd.flow.cyto <- 
-  df.tall$true.prop.mean.mrna.yield <- 
-  df.tall$true.prop.sd.mrna.yield <-
-  df.tall$s.cell.size <- "NA"
-for(type in df.map$p.true.id){
-  filter.tall <- gsub("\\.", " ", rownames(df.tall)) == type
-  df.tall[filter.tall,"true.prop.mean.flow.cyto"] <- 
-    df.proportions[df.proportions[,1]==type,"flow.cyto.mean"]
-  df.tall[filter.tall,"true.prop.sd.flow.cyto"] <- 
-    df.proportions[df.proportions[,1]==type,"flow.cyto.sd"]
-  df.tall[filter.tall,"true.prop.mean.mrna.yield"] <- 
-    df.proportions[df.proportions[,1]==type,"mrna.yield.mean"]
-  df.tall[filter.tall,"true.prop.sd.mrna.yield"] <- 
-    df.proportions[df.proportions[,1]==type,"mrna.yield.sd"]
-  df.tall[filter.tall,"s.cell.size"] <- 
-    s.vector[gsub("\\.", " ", names(s.vector))==type]
-}
-df.tall <- df.tall[!rownames(df.tall)=="sample.id",]
-
-# format columns as numeric
-for(c in seq(ncol(df.tall))){df.tall[,c] <- as.numeric(df.tall[,c])}
-df.tall$cell.type <- rownames(df.tall)
-# format cell size
-df.tall$s.cell.size.log <- log(df.tall$s.cell.size)
-
-# get df.wide
-df.wide <- rbind(
-  data.frame(mean = df.tall$mean.unscaled, sd = df.tall$sd.unscaled, 
-             type = rep("unscaled", nrow(df.tall)), cell.type = rownames(df.tall),
-             true.prop.mean.flow.cyto = df.tall$true.prop.mean.flow.cyto,
-             true.prop.mean.mrna.yield = df.tall$true.prop.mean.mrna.yield,
-             true.prop.sd.flow.cyto = df.tall$true.prop.sd.flow.cyto,
-             true.prop.sd.mrna.yield = df.tall$true.prop.sd.mrna.yield),
-  data.frame(mean = df.tall$mean.scaled, sd = df.tall$sd.scaled, 
-             type = rep("scaled", nrow(df.tall)), cell.type = rownames(df.tall),
-             true.prop.mean.flow.cyto = df.tall$true.prop.mean.flow.cyto,
-             true.prop.mean.mrna.yield = df.tall$true.prop.mean.mrna.yield,
-             true.prop.sd.flow.cyto = df.tall$true.prop.sd.flow.cyto,
-             true.prop.sd.mrna.yield = df.tall$true.prop.sd.mrna.yield)
-)
-df.wide <- as.data.frame(df.wide)
-df.wide <- df.wide[!df.wide$cell.type=="sample.id",]
-
-# format columns as numeric
-for(c in c(1,2,5,6,7,8)){df.wide[,c] <- as.numeric(df.wide[,c])}
-
-# append bias and error
-df.wide$true.prop.mean.flow.cyto.format <- 
-  df.wide$true.prop.mean.flow.cyto/sum(na.omit(df.wide$true.prop.mean.flow.cyto))
-df.wide$bias.flow.cyto <- df.wide$true.prop.mean.flow.cyto.format-
-  df.wide$mean
-df.wide$error.flow.cyto <- abs(df.wide$bias.flow.cyto)
-
-
-
-
-
-
-
-#-----
-# save
-#-----
-save.image("./env/02_abisseq/01_abisseq_script.RData")
-
-
-
-
+#-----------------
+# save environment
+#-----------------
+save.image("./env/08_improvement/01_run_script.RData")
 
