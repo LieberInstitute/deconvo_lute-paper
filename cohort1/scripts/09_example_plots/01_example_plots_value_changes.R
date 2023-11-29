dfc03e324d796cc66344b7826f778f65cc7ea32e
Sean Maden <metamaden@users.noreply.github.com>
2023-11-28 16:07:32 -0800

    saves env, saves three example multipanel plots

---
+++ cohort1/scripts/09_example_plots/01_example_plots_value_changes.R
@@ -0,0 +1,75 @@
+#!/usr/bin/env R
+
+libv <- c("ggplot2", "reshape2")
+sapply(libv, library, character.only = T)
+
+# Author: Sean Maden
+#
+# Plot to show direction change in expression and prediction with scale factor change.
+#
+#
+#
+#
+#
+
+source("./scripts/09_example_plots/00_param.R")
+
+#--------------------------------------------------
+
+# multiple panels -- marker expression start is 0.5
+
+#--------------------------------------------------
+# get multiple panels
+# view facet of multi panel plots
+
+# marker expression start is 0.5
+listMultiPlot05 <- multiPanelPlots(markerExpressionStart = 0.5)
+barplotStart05 <- 
+  listMultiPlot05$resultsList$Decrease$result$valuesList$ggBarplotStart
+grid.arrange(barplotStart, listMultiPlot05$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+
+# marker expression start is 1
+listMultiPlot1 <- multiPanelPlots(markerExpressionStart = 1)
+barplotStart1 <- 
+  listMultiPlot1$resultsList$Decrease$result$valuesList$ggBarplotStart
+grid.arrange(barplotStart, listMultiPlot1$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+
+# marker expression start is 2
+listMultiPlot2 <- multiPanelPlots(markerExpressionStart = 2)
+barplotStart2 <- 
+  listMultiPlot2$resultsList$Decrease$result$valuesList$ggBarplotStart
+grid.arrange(barplotStart, listMultiPlot2$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+
+#--------------------------
+
+# save multipanel plots
+
+#--------------------------
+# save
+jpeg(paste0("./figures/09_example_plots/",
+            "multipanel_example_barplots_marker05.jpg"), width = 11, 
+     height = 4, units = "in", res = 400)
+grid.arrange(barplotStart05, listMultiPlot05$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+dev.off()
+
+jpeg(paste0("./figures/09_example_plots/",
+            "multipanel_example_barplots_marker1.jpg"), width = 11, 
+     height = 4, units = "in", res = 400)
+grid.arrange(barplotStart1, listMultiPlot1$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+dev.off()
+
+jpeg(paste0("./figures/09_example_plots/",
+            "multipanel_example_barplots_marker2.jpg"), width = 11, 
+     height = 4, units = "in", res = 400)
+grid.arrange(barplotStart2, listMultiPlot2$ggMulti, nrow = 1,
+             layout_matrix = matrix(c(1,2,2,2),nrow=1))
+dev.off()
+
+# save image
+save.image(
+  "./env/09_example_plots/01_example_plots_value_changes_script.RData")
