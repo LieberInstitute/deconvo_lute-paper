fa3a0c3b40728f8d81edac58cee2f4edfc877e34
Sean Maden <metamaden@users.noreply.github.com>
2023-12-29 15:11:46 -0800

    adds snrnaseq k4 nuclei summary

--- cohort1/scripts/01_mae/04_summary_total_nuclei.R
+++ cohort1/scripts/01_mae/04_summary_total_nuclei.R
@@ -1,12 +1,39 @@
-unique.samples <- unique(sc$Sample)
-mean(unlist(lapply(unique.samples, function(sample.id){
-  ncol(sc[,sc$Sample==sample.id])
-})))
+#!/usr/bin/env R
 
-median(unlist(lapply(unique.samples, function(sample.id){
-  ncol(sc[,sc$Sample==sample.id])
-})))
+# Author: Sean Maden
+#
+# Summaries of nuclei counts and proportions.
+#
+#
 
-sd(unlist(lapply(unique.samples, function(sample.id){
-  ncol(sc[,sc$Sample==sample.id])
-})))
+libv <- c("SingleCellExperiment", "MultiAssayExperiment")
+sapply(libv, library, character.only = TRUE)
+
+# load
+load("outputs/01_mae/mae_analysis.rda")
+
+# snrnaseq
+# k4 summaries
+sce <- mae[["snrnaseq.k4.all"]]
+cd <- colData(sce)
+dfs <- table(cd$k4, cd$Sample)
+dfs <- dfs[!rownames(dfs)=="Ambiguous",] # filter
+dfp <- apply(dfs, 2, function(ci){ci/sum(ci)})
+apply(dfp, 1, function(ri){min(ri)})
+apply(dfp, 1, function(ri){max(ri)})
+apply(dfp, 1, function(ri){mean(ri)})
+apply(dfp, 1, function(ri){median(ri)})
+
+# total nuclei
+#unique.samples <- unique(sc$Sample)
+#mean(unlist(lapply(unique.samples, function(sample.id){
+#  ncol(sc[,sc$Sample==sample.id])
+#})))
+
+#median(unlist(lapply(unique.samples, function(sample.id){
+#  ncol(sc[,sc$Sample==sample.id])
+#})))
+
+#sd(unlist(lapply(unique.samples, function(sample.id){
+#  ncol(sc[,sc$Sample==sample.id])
+#})))
