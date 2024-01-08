562bf4dee7ecc8da90267196e80a125dbfde83ea
Sean Maden <metamaden@users.noreply.github.com>
2024-01-08 09:56:30 -0800

    adds script to combine cohort1 and cohort2 rmse

--- software/scripts/03_rmseTable/01_save.R
+++ software/scripts/03_rmseTable/01_save.R
@@ -1,12 +1,16 @@
 # load
+# setwd("..") # set path to deconvo_lute-paper
 supplementTable1 <- 
-  get(load("./outputs/01_pseudobulk/rmse_supplementTable.rda"))
+  get(load("./cohort1/outputs/02_pseudobulk/rmse_supplementTable.rda"))
 supplementTable2 <- 
-  get(load("./outputs/02_pseudobulk/rmse_supplementTable.rda"))
+  get(load("./cohort2/outputs/01_pseudobulk/rmse_supplementTable.rda"))
+supplementTable3 <- 
+  get(load("./cohort2/outputs/01_pseudobulk/rmse_supplementTable.rda"))
+
 # bind
 
 tableOut <- 
   rbind(supplementTable1, supplementTable2, supplementTable3) |> as.data.frame()
 
 # save
-write.csv("./out/03_rmse/rmse_supplementTable.csv")
+write.csv("./software/out/03_rmse/rmse_supplementTable.csv")
