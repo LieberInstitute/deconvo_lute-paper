5e39bfa7dd0f4a10e42465598a37fb79fa4e4842
Sean Maden <metamaden@users.noreply.github.com>
2024-03-21 10:39:30 -0700

    remove train/test label, remove table s8 subset

--- huuki-meyers-et-al-2023_dlpfc-cohort1/scripts/02_summarize_mae/01_data_summaries_table.R
+++ huuki-meyers-et-al-2023_dlpfc-cohort1/scripts/02_summarize_mae/01_data_summaries_table.R
@@ -20,17 +20,5 @@ path.data.post.filter <- "./outputs/01_mae/mae_analysis_append.rda"
 mae.all <- get(load(path.data.pre.filter))
 mae.filter <- get(load(path.data.post.filter))
 
-list.sample.id <- get(load("./outputs/00_preprocess/list_snrnaseq_sampleid.rda"))
-sample.id.train <- list.sample.id[["train"]]
-sample.id.validate <- list.sample.id[["validation"]]
-
-# summaries
-list.summaries.prefilter <- summaries_df_list(mae.all, filter.type.label = "prefilter")
-list.summaries.postfilter <- summaries_df_list(mae.filter, filter.type.label = "postfilter")
-
-# write summary tables
-write.csv(list.summaries.prefilter[["wide"]], 
-          file = "./outputs/02_summarize_mae/TABLE_S8_SUBSET.csv", row.names = FALSE)
-
 # save image
 save.image(file = './env/02_summarize_mae/01_data_summaries_script.RData')
